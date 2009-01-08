91ba6510f147dd6a6856ef9bc43109efb6c2b54c
Mike Dirolf <mike@10gen.com>
2009-01-08 14:33:37 -0500

    add client tests to test script

--- run_tests.py
+++ run_tests.py
@@ -5,16 +5,19 @@ import unittest
 import bson
 import objectid
 import dbref
+import mongo
 
 def main():
     bson_suite = unittest.TestLoader().loadTestsFromModule(bson)
     objectid_suite = unittest.TestLoader().loadTestsFromModule(objectid)
     dbref_suite = unittest.TestLoader().loadTestsFromModule(dbref)
+    mongo_suite = unittest.TestLoader().loadTestsFromModule(mongo)
     alltests = unittest.TestSuite([bson_suite, objectid_suite, dbref_suite])
 
     unittest.TextTestRunner().run(objectid_suite)
     unittest.TextTestRunner().run(dbref_suite)
     unittest.TextTestRunner().run(bson_suite)
+    unittest.TextTestRunner().run(mongo_suite)
 
 if __name__ == "__main__":
     main()
