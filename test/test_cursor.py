ea219cbaeb330a854717a8044f974e04286e1ed9
Mike Dirolf <mike@10gen.com>
2009-01-21 14:20:40 -0500

    move limit, skip, count, and sort tests to test_cursor

--- test/test_cursor.py
+++ test/test_cursor.py
@@ -1,8 +1,11 @@
 """Test the cursor module."""
 import unittest
+import types
+import random
 
+from errors import InvalidOperation
 from cursor import Cursor
-from database import Database
+from database import Database, ASCENDING, DESCENDING
 from test_connection import get_connection
 
 class TestCursor(unittest.TestCase):
@@ -16,6 +19,172 @@ class TestCursor(unittest.TestCase):
             break
         self.assertEqual(a.explain(), explanation)
         self.assertTrue("cursor" in explanation)
+
+    def test_limit(self):
+        db = self.db
+
+        self.assertRaises(TypeError, db.test.find().limit, None)
+        self.assertRaises(TypeError, db.test.find().limit, "hello")
+        self.assertRaises(TypeError, db.test.find().limit, 5.5)
+
+        db.test.remove({})
+        for i in range(100):
+            db.test.save({"x": i})
+
+        count = 0
+        for _ in db.test.find():
+            count += 1
+        self.assertEqual(count, 100)
+
+        count = 0
+        for _ in db.test.find().limit(20):
+            count += 1
+        self.assertEqual(count, 20)
+
+        count = 0
+        for _ in db.test.find().limit(99):
+            count += 1
+        self.assertEqual(count, 99)
+
+        count = 0
+        for _ in db.test.find().limit(1):
+            count += 1
+        self.assertEqual(count, 1)
+
+        count = 0
+        for _ in db.test.find().limit(0):
+            count += 1
+        self.assertEqual(count, 100)
+
+        count = 0
+        for _ in db.test.find().limit(0).limit(50).limit(10):
+            count += 1
+        self.assertEqual(count, 10)
+
+        a = db.test.find()
+        a.limit(10)
+        for _ in a:
+            break
+        self.assertRaises(InvalidOperation, a.limit, 5)
+
+    def test_skip(self):
+        db = self.db
+
+        self.assertRaises(TypeError, db.test.find().skip, None)
+        self.assertRaises(TypeError, db.test.find().skip, "hello")
+        self.assertRaises(TypeError, db.test.find().skip, 5.5)
+
+        db.test.remove({})
+        for i in range(100):
+            db.test.save({"x": i})
+
+        for i in db.test.find():
+            self.assertEqual(i["x"], 0)
+            break
+
+        for i in db.test.find().skip(20):
+            self.assertEqual(i["x"], 20)
+            break
+
+        for i in db.test.find().skip(99):
+            self.assertEqual(i["x"], 99)
+            break
+
+        for i in db.test.find().skip(1):
+            self.assertEqual(i["x"], 1)
+            break
+
+        for i in db.test.find().skip(0):
+            self.assertEqual(i["x"], 0)
+            break
+
+        for i in db.test.find().skip(0).skip(50).skip(10):
+            self.assertEqual(i["x"], 10)
+            break
+
+        for i in db.test.find().skip(1000):
+            self.fail()
+
+        a = db.test.find()
+        a.skip(10)
+        for _ in a:
+            break
+        self.assertRaises(InvalidOperation, a.skip, 5)
+
+    def test_sort(self):
+        db = self.db
+
+        self.assertRaises(TypeError, db.test.find().sort, 5)
+        self.assertRaises(TypeError, db.test.find().sort, "hello")
+        self.assertRaises(ValueError, db.test.find().sort, [])
+        self.assertRaises(TypeError, db.test.find().sort, [], ASCENDING)
+        self.assertRaises(TypeError, db.test.find().sort, [("hello", DESCENDING)], DESCENDING)
+        self.assertRaises(TypeError, db.test.find().sort, "hello", "world")
+
+        db.test.remove({})
+
+        unsort = range(10)
+        random.shuffle(unsort)
+
+        for i in unsort:
+            db.test.save({"x": i})
+
+        asc = [i["x"] for i in db.test.find().sort("x", ASCENDING)]
+        self.assertEqual(asc, range(10))
+        asc = [i["x"] for i in db.test.find().sort([("x", ASCENDING)])]
+        self.assertEqual(asc, range(10))
+
+        expect = range(10)
+        expect.reverse()
+        desc = [i["x"] for i in db.test.find().sort("x", DESCENDING)]
+        self.assertEqual(desc, expect)
+        desc = [i["x"] for i in db.test.find().sort([("x", DESCENDING)])]
+        self.assertEqual(desc, expect)
+        desc = [i["x"] for i in db.test.find().sort("x", ASCENDING).sort("x", DESCENDING)]
+        self.assertEqual(desc, expect)
+
+        expected = [(1, 5), (2, 5), (0, 3), (7, 3), (9, 2), (2, 1), (3, 1)]
+        shuffled = list(expected)
+        random.shuffle(shuffled)
+
+        db.test.remove({})
+        for (a, b) in shuffled:
+            db.test.save({"a": a, "b": b})
+
+        result = [(i["a"], i["b"]) for i in db.test.find().sort([("b", DESCENDING),
+                                                                 ("a", ASCENDING)])]
+        self.assertEqual(result, expected)
+
+        a = db.test.find()
+        a.sort("x", ASCENDING)
+        for _ in a:
+            break
+        self.assertRaises(InvalidOperation, a.sort, "x", ASCENDING)
+
+    def test_count(self):
+        db = self.db
+        db.test.remove({})
+
+        self.assertEqual(0, db.test.find().count())
+
+        for i in range(10):
+            db.test.save({"x": i})
+
+        self.assertEqual(10, db.test.find().count())
+        self.assertTrue(isinstance(db.test.find().count(), types.IntType))
+        self.assertEqual(10, db.test.find().limit(5).count())
+        self.assertEqual(10, db.test.find().skip(5).count())
+
+        self.assertEqual(1, db.test.find({"x": 1}).count())
+        self.assertEqual(5, db.test.find({"x": {"$lt": 5}}).count())
+
+        a = db.test.find()
+        b = a.count()
+        for _ in a:
+            break
+        self.assertRaises(InvalidOperation, a.count)
+
+        self.assertEqual(0, db.test.acollectionthatdoesntexist.find().count())
 
 if __name__ == "__main__":
     unittest.main()
--- test/test_database.py
+++ test/test_database.py
@@ -178,8 +178,8 @@ class TestDatabase(unittest.TestCase):
         self.assertRaises(TypeError, db.test.remove, [])
 
         one = db.test.save({"x": 1})
-        two = db.test.save({"x": 2})
-        three = db.test.save({"x": 3})
+        db.test.save({"x": 2})
+        db.test.save({"x": 3})
         length = 0
         for _ in db.test.find():
             length += 1
@@ -196,8 +196,8 @@ class TestDatabase(unittest.TestCase):
         self.assertEqual(db.test.find_one(), None)
 
         one = db.test.save({"x": 1})
-        two = db.test.save({"x": 2})
-        three = db.test.save({"x": 3})
+        db.test.save({"x": 2})
+        db.test.save({"x": 3})
 
         self.assertTrue(db.test.find_one({"x": 2}))
         db.test.remove({"x": 2})
@@ -224,172 +224,6 @@ class TestDatabase(unittest.TestCase):
         for _ in xrange(62):
             for _ in db.test.find():
                 break
-
-    def test_limit(self):
-        db = self.connection.test
-
-        self.assertRaises(TypeError, db.test.find().limit, None)
-        self.assertRaises(TypeError, db.test.find().limit, "hello")
-        self.assertRaises(TypeError, db.test.find().limit, 5.5)
-
-        db.test.remove({})
-        for i in range(100):
-            db.test.save({"x": i})
-
-        count = 0
-        for _ in db.test.find():
-            count += 1
-        self.assertEqual(count, 100)
-
-        count = 0
-        for _ in db.test.find().limit(20):
-            count += 1
-        self.assertEqual(count, 20)
-
-        count = 0
-        for _ in db.test.find().limit(99):
-            count += 1
-        self.assertEqual(count, 99)
-
-        count = 0
-        for _ in db.test.find().limit(1):
-            count += 1
-        self.assertEqual(count, 1)
-
-        count = 0
-        for _ in db.test.find().limit(0):
-            count += 1
-        self.assertEqual(count, 100)
-
-        count = 0
-        for _ in db.test.find().limit(0).limit(50).limit(10):
-            count += 1
-        self.assertEqual(count, 10)
-
-        a = db.test.find()
-        a.limit(10)
-        for _ in a:
-            break
-        self.assertRaises(InvalidOperation, a.limit, 5)
-
-    def test_skip(self):
-        db = self.connection.test
-
-        self.assertRaises(TypeError, db.test.find().skip, None)
-        self.assertRaises(TypeError, db.test.find().skip, "hello")
-        self.assertRaises(TypeError, db.test.find().skip, 5.5)
-
-        db.test.remove({})
-        for i in range(100):
-            db.test.save({"x": i})
-
-        for i in db.test.find():
-            self.assertEqual(i["x"], 0)
-            break
-
-        for i in db.test.find().skip(20):
-            self.assertEqual(i["x"], 20)
-            break
-
-        for i in db.test.find().skip(99):
-            self.assertEqual(i["x"], 99)
-            break
-
-        for i in db.test.find().skip(1):
-            self.assertEqual(i["x"], 1)
-            break
-
-        for i in db.test.find().skip(0):
-            self.assertEqual(i["x"], 0)
-            break
-
-        for i in db.test.find().skip(0).skip(50).skip(10):
-            self.assertEqual(i["x"], 10)
-            break
-
-        for i in db.test.find().skip(1000):
-            self.fail()
-
-        a = db.test.find()
-        a.skip(10)
-        for _ in a:
-            break
-        self.assertRaises(InvalidOperation, a.skip, 5)
-
-    def test_sort(self):
-        db = self.connection.test
-
-        self.assertRaises(TypeError, db.test.find().sort, 5)
-        self.assertRaises(TypeError, db.test.find().sort, "hello")
-        self.assertRaises(ValueError, db.test.find().sort, [])
-        self.assertRaises(TypeError, db.test.find().sort, [], ASCENDING)
-        self.assertRaises(TypeError, db.test.find().sort, [("hello", DESCENDING)], DESCENDING)
-        self.assertRaises(TypeError, db.test.find().sort, "hello", "world")
-
-        db.test.remove({})
-
-        unsort = range(10)
-        random.shuffle(unsort)
-
-        for i in unsort:
-            db.test.save({"x": i})
-
-        asc = [i["x"] for i in db.test.find().sort("x", ASCENDING)]
-        self.assertEqual(asc, range(10))
-        asc = [i["x"] for i in db.test.find().sort([("x", ASCENDING)])]
-        self.assertEqual(asc, range(10))
-
-        expect = range(10)
-        expect.reverse()
-        desc = [i["x"] for i in db.test.find().sort("x", DESCENDING)]
-        self.assertEqual(desc, expect)
-        desc = [i["x"] for i in db.test.find().sort([("x", DESCENDING)])]
-        self.assertEqual(desc, expect)
-        desc = [i["x"] for i in db.test.find().sort("x", ASCENDING).sort("x", DESCENDING)]
-        self.assertEqual(desc, expect)
-
-        expected = [(1, 5), (2, 5), (0, 3), (7, 3), (9, 2), (2, 1), (3, 1)]
-        shuffled = list(expected)
-        random.shuffle(shuffled)
-
-        db.test.remove({})
-        for (a, b) in shuffled:
-            db.test.save({"a": a, "b": b})
-
-        result = [(i["a"], i["b"]) for i in db.test.find().sort([("b", DESCENDING),
-                                                                 ("a", ASCENDING)])]
-        self.assertEqual(result, expected)
-
-        a = db.test.find()
-        a.sort("x", ASCENDING)
-        for _ in a:
-            break
-        self.assertRaises(InvalidOperation, a.sort, "x", ASCENDING)
-
-    def test_count(self):
-        db = self.connection.test
-        db.test.remove({})
-
-        self.assertEqual(0, db.test.find().count())
-
-        for i in range(10):
-            db.test.save({"x": i})
-
-        self.assertEqual(10, db.test.find().count())
-        self.assertTrue(isinstance(db.test.find().count(), types.IntType))
-        self.assertEqual(10, db.test.find().limit(5).count())
-        self.assertEqual(10, db.test.find().skip(5).count())
-
-        self.assertEqual(1, db.test.find({"x": 1}).count())
-        self.assertEqual(5, db.test.find({"x": {"$lt": 5}}).count())
-
-        a = db.test.find()
-        b = a.count()
-        for _ in a:
-            break
-        self.assertRaises(InvalidOperation, a.count)
-
-        self.assertEqual(0, db.test.acollectionthatdoesntexist.find().count())
 
 if __name__ == "__main__":
     unittest.main()
