ec01929e50efbc887140f1ca98615182dadfaeda
Mike Dirolf <mike@10gen.com>
2009-01-22 14:30:41 -0500

    starting on a setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup
+
+setup(
+    name="pymongo",
+    version="0.1",
+    description="Driver for the Mongo database <http://www.mongodb.org>",
+    long_description="""\
+The pymongo package is a driver for Mongo, a high performance, document-based
+database. See http://www.mongodb.org and http://www.10gen.com for more
+information on Mongo.
+""",
+    author="10gen",
+    author_email="mike@10gen.com",
+    url="http://github.com/mongodb/mongo-python-driver",
+    packages=["pymongo"],
+    install_requires=['elementtree'])
