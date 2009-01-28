296cbed9cafc9076469ff46abf1555ccbc8f20fd
Mike Dirolf <mike@10gen.com>
2009-01-28 16:36:20 -0500

    optimizing bson module - remove logging and automatic validation

--- pymongo/bson.py
+++ pymongo/bson.py
@@ -62,64 +62,53 @@ def _make_c_string(string):
     return string + "\x00"
 
 def _validate_number(data):
-    _logger.debug("validating number")
     assert len(data) >= 8
     return data[8:]
 
 def _validate_string(data):
-    _logger.debug("validating string")
     (length, data) = _get_int(data)
     assert len(data) >= length
     assert data[length - 1] == "\x00"
     return data[length:]
 
 def _validate_object(data):
-    _logger.debug("validating object")
     return _validate_document(data, None)
 
 _valid_array_name = re.compile("^\d+$")
 def _validate_array(data):
-    _logger.debug("validating array")
     return _validate_document(data, _valid_array_name)
 
 def _validate_binary(data):
-    _logger.debug("validating binary")
     (length, data) = _get_int(data)
     # + 1 for the subtype byte
     assert len(data) >= length + 1
     return data[length + 1:]
 
 def _validate_undefined(data):
-    _logger.debug("validating undefined")
     return data
 
 _OID_SIZE = 12
 def _validate_oid(data):
-    _logger.debug("validating oid")
     assert len(data) >= _OID_SIZE
     return data[_OID_SIZE:]
 
 def _validate_boolean(data):
-    _logger.debug("validating boolean")
     assert len(data) >= 1
     return data[1:]
 
 _DATE_SIZE = 8
 def _validate_date(data):
-    _logger.debug("validating date")
     assert len(data) >= _DATE_SIZE
     return data[_DATE_SIZE:]
 
 _validate_null = _validate_undefined
 
 def _validate_regex(data):
-    _logger.debug("validating regex")
     (regex, data) = _get_c_string(data)
     (options, data) = _get_c_string(data)
     return data
 
 def _validate_ref(data):
-    _logger.debug("validating ref")
     data = _validate_string(data)
     return _validate_oid(data)
 
@@ -127,7 +116,6 @@ _validate_code = _validate_string
 
 # still not sure what is actually stored here, but i know how big it is...
 def _validate_code_w_scope(data):
-    _logger.debug("validating code w/ scope")
     (length, data) = _get_int(data)
     assert len(data) >= length + 1
     return data[length + 1:]
@@ -135,7 +123,6 @@ def _validate_code_w_scope(data):
 _validate_symbol = _validate_string
 
 def _validate_number_int(data):
-    _logger.debug("validating int")
     assert len(data) >= 4
     return data[4:]
 
@@ -194,19 +181,15 @@ def _validate_document(data, valid_name=None):
     return data[obj_size:]
 
 def _get_number(data):
-    _logger.debug("unpacking number")
     return (struct.unpack("<d", data[:8])[0], data[8:])
 
 def _get_string(data):
-    _logger.debug("unpacking string")
     return _get_c_string(data[4:])
 
 def _get_object(data):
-    _logger.debug("unpacking object")
     return _document_to_dict(data)
 
 def _get_array(data):
-    _logger.debug("unpacking array")
     (obj, data) = _get_object(data)
     result = []
     i = 0
@@ -219,7 +202,6 @@ def _get_array(data):
     return (result, data)
 
 def _get_binary(data):
-    _logger.debug("unpacking binary")
     (length, data) = _get_int(data)
     subtype = data[0]
     data = data[1:]
@@ -231,25 +213,20 @@ def _get_binary(data):
     return (Binary(data[:length2]), data[length2:])
 
 def _get_oid(data):
-    _logger.debug("unpacking oid")
     oid = _shuffle_oid(data[:12])
     return (ObjectId(oid), data[12:])
 
 def _get_boolean(data):
-    _logger.debug("unpacking boolean")
     return (data[0] == "\x01", data[1:])
 
 def _get_date(data):
-    _logger.debug("unpacking date")
     seconds = float(struct.unpack("<q", data[:8])[0]) / 1000.0
     return (datetime.datetime.utcfromtimestamp(seconds), data[8:])
 
 def _get_null(data):
-    _logger.debug("unpacking null")
     return (None, data)
 
 def _get_regex(data):
-    _logger.debug("unpacking regex")
     (pattern, data) = _get_c_string(data)
     (bson_flags, data) = _get_c_string(data)
     flags = 0
@@ -268,7 +245,6 @@ def _get_regex(data):
     return (re.compile(pattern, flags), data)
 
 def _get_ref(data):
-    _logger.debug("unpacking ref")
     (collection, data) = _get_c_string(data[4:])
     (oid, data) = _get_oid(data)
     return (DBRef(collection, oid), data)
@@ -322,42 +298,32 @@ def _shuffle_oid(data):
 _RE_TYPE = type(_valid_array_name)
 def _value_to_bson(value):
     if isinstance(value, types.FloatType):
-        _logger.debug("packing number")
         return ("\x01", struct.pack("<d", value))
     if is_binary(value):
-        _logger.debug("packing binary using subtype '\x02'")
         length = len(value)
         return ("\x05",
                 _int_to_bson(length + 4) + "\x02" + _int_to_bson(length) + value)
     if isinstance(value, types.StringTypes):
-        _logger.debug("packing string")
         cstring = _make_c_string(unicode(value))
         length = _int_to_bson(len(cstring))
         return ("\x02", length + cstring)
     if isinstance(value, (types.DictType, SON)):
-        _logger.debug("packing object")
         return ("\x03", BSON.from_dict(value))
     if isinstance(value, types.ListType):
-        _logger.debug("packing array")
         as_dict = SON(zip([str(i) for i in range(len(value))], value))
         return ("\x04", BSON.from_dict(as_dict))
     if isinstance(value, ObjectId):
-        _logger.debug("packing oid")
         return ("\x07", _shuffle_oid(str(value)))
     if isinstance(value, types.BooleanType):
-        _logger.debug("packing boolean")
         if value:
             return ("\x08", "\x01")
         return ("\x08", "\x00")
     if isinstance(value, datetime.datetime):
-        _logger.debug("packing date")
         millis = int(calendar.timegm(value.timetuple()) * 1000 + value.microsecond / 1000)
         return ("\x09", _int_64_to_bson(millis))
     if isinstance(value, types.NoneType):
-        _logger.debug("packing null")
         return ("\x0A", "")
     if isinstance(value, _RE_TYPE):
-        _logger.debug("packing regex")
         pattern = value.pattern
         flags = ""
         if value.flags & re.IGNORECASE:
@@ -374,16 +340,13 @@ def _value_to_bson(value):
             flags += "x"
         return ("\x0B", _make_c_string(pattern) + _make_c_string(flags))
     if isinstance(value, DBRef):
-        _logger.debug("packing ref")
         ns = _make_c_string(value.collection())
         return ("\x0C", _int_to_bson(len(ns)) + ns + _shuffle_oid(str(value.id())))
     if isinstance(value, types.IntType):
-        _logger.debug("packing int")
         return ("\x10", _int_to_bson(value))
     raise InvalidDocument("cannot convert value of type %s to bson" % type(value))
 
 def _where_value_to_bson(value):
-    _logger.debug("packing code")
     if not isinstance(value, types.StringTypes):
         raise TypeError("$where value must be an instance of (str, unicode)")
     cstring = _make_c_string(value)
@@ -439,15 +402,11 @@ class BSON(str):
     def __new__(cls, bson):
         """Initialize a new BSON object with some data.
 
-        The data given must be a string instance and represent a valid BSON
-        object, otherwise an TypeError or InvalidBSON exception is raised.
+        Raises TypeError if `bson` is not an instance of str.
 
         :Parameters:
           - `bson`: the initial data
         """
-        if not is_valid(bson):
-            raise InvalidBSON()
-
         return str.__new__(cls, bson)
 
     @classmethod
