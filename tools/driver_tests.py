6aba7c9cdbb57d61a1a5e031c8793e9e8147700e
Mike Dirolf <mike@10gen.com>
2009-02-05 10:35:24 -0500

    support for the new driver tests

--- tools/driver_tests.py
+++ tools/driver_tests.py
@@ -23,37 +23,7 @@ import datetime
 sys.path[0:0] = [os.path.join(os.getcwd(), "..")]
 from pymongo.connection import Connection
 from pymongo.errors import CollectionInvalid
-from pymongo import OFF, SLOW_ONLY, ALL
-
-def test1(db, out):
-    for i in range(100):
-        db.part1.insert({"x": i})
-
-def remove(db, out):
-    db.remove1.remove({})
-    db.remove2.remove({"a": 3})
-
-def find(db, out):
-    db.test.insert({"a": 2})
-
-def capped(db, out):
-    collection = db.create_collection("capped1", {"capped": True, "size": 500})
-    collection.insert({"x": 1})
-    collection.insert({"x": 2})
-
-    # TODO ignoring $nExtents for now
-    collection2 = db.create_collection("capped2", {"capped": True, "size": 1000})
-    str = ""
-    for _ in range(100):
-        collection2.insert({"dashes": str})
-        str += "-"
-
-def count1(db, out):
-    print >>out, db.test1.find().count()
-    print >>out, db.test2.find().count()
-    print >>out, db.test3.find({"i": "a"}).count()
-    print >>out, db.test3.find({"i": 3}).count()
-    print >>out, db.test3.find({"i": {"$gte": 67}}).count()
+from pymongo import OFF, SLOW_ONLY, ALL, ASCENDING
 
 def admin(db, out):
     tester = db.tester
@@ -83,6 +53,39 @@ def admin(db, out):
     db.set_profiling_level(OFF)
     print >>out, levels[db.profiling_level()]
 
+def capped(db, out):
+    collection = db.create_collection("capped1", {"capped": True, "size": 500})
+    collection.insert({"x": 1})
+    collection.insert({"x": 2})
+
+    # TODO ignoring $nExtents for now
+    collection2 = db.create_collection("capped2", {"capped": True, "size": 1000})
+    str = ""
+    for _ in range(100):
+        collection2.insert({"dashes": str})
+        str += "-"
+
+def count1(db, out):
+    print >>out, db.test1.find().count()
+    print >>out, db.test2.find().count()
+    print >>out, db.test3.find({"i": "a"}).count()
+    print >>out, db.test3.find({"i": 3}).count()
+    print >>out, db.test3.find({"i": {"$gte": 67}}).count()
+
+def dberror(db, out):
+    db.reset_error_history()
+    print >>out, db.error() is None
+    print >>out, db.previous_error() is None
+    db._command({"forceerror": 1}, check=False)
+    print >>out, db.error() is None
+    print >>out, db.previous_error() is None
+    db.foo.find_one()
+    print >>out, db.error() is None
+    print >>out, db.previous_error() is None
+    print >>out, db.previous_error()["nPrev"]
+    db.reset_error_history()
+    print >>out, db.previous_error() is None
+
 def dbs(db, out):
     db.dbs_1.save({"foo": "bar"})
     db.dbs_2.save({"psi": "phi"})
@@ -93,6 +96,56 @@ def dbs(db, out):
     db.create_collection("dbs_3")
     for name in [n for n in sorted(db.collection_names()) if n.startswith("dbs")]:
         print >>out, name
+
+def find(db, out):
+    db.test.insert({"a": 2})
+
+def find1(db, out):
+    for doc in db.c.find({"x": 1}).sort("y", ASCENDING).skip(20).limit(10):
+        print >>out, doc["z"]
+
+def indices(db, out):
+    db.x.drop_index([("field1", ASCENDING)])
+    for name in db.x.index_information().keys():
+        print >>out, name
+    db.y.create_index([("a", ASCENDING),
+                       ("b", ASCENDING),
+                       ("c", ASCENDING)])
+    db.y.create_index("d", ASCENDING)
+    for name in sorted(db.y.index_information().keys()):
+        print >>out, name
+
+def remove(db, out):
+    db.remove1.remove({})
+    db.remove2.remove({"a": 3})
+
+def stress1(db, out):
+    for i in range(50000):
+        db.stress1.save({"name": "asdf" + str(i),
+                         "date": datetime.datetime.now(),
+                         "id": i,
+                         "blah": "lksjhasoh1298a1shasoidiohaskjasiouashoasasiugoas" +
+                         "lksjhasoh1298a1shasoidiohaskjasiouashoasasiugoas" +
+                         "lksjhasoh1298a1shasoidiohaskjasiouashoasasiugoas" +
+                         "lksjhasoh1298a1shasoidiohaskjasiouashoasasiugoas" +
+                         "lksjhasoh1298a1shasoidiohaskjasiouashoasasiugoas" +
+                         "lksjhasoh1298a1shasoidiohaskjasiouashoasasiugoas",
+                         "subarray": []})
+    for i in range(10000):
+        x = db.stress1.find_one({"id": i})
+        x["subarray"] = "foo" + str(i)
+        db.stress1.save(x)
+
+    db.stress1.create_index("date", ASCENDING)
+
+def test1(db, out):
+    for i in range(100):
+        db.part1.insert({"x": i})
+
+def update(db, out):
+    db.foo.update({"x": 1}, {"x": 1, "y": 2})
+    db.foo.update({"x": 2}, {"x": 1, "y": 7})
+    db.foo.update({"x": 3}, {"x": 4, "y": 1}, upsert=True)
 
 def main(test, out_file):
     db = Connection().driver_test_framework
