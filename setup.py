9109d8ca5a8c798bf1ce0d6f76a6a2bad8c62288
Mike Dirolf <mike@10gen.com>
2009-02-05 09:08:26 -0500

    bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ finally:
 
 setup(
     name="pymongo",
-    version="0.3.1pre",
+    version="0.4pre",
     description="Driver for the Mongo database <http://www.mongodb.org>",
     long_description=readme_content,
     author="10gen",
