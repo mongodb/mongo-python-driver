7560be2ddbc16bb04ee44a4b75a661262f2c4eb5
Mike Dirolf <mike@10gen.com>
2009-02-12 10:29:24 -0500

    a couple of tests

--- test/test_pooling.py
+++ test/test_pooling.py
@@ -25,9 +25,16 @@ class TestPooling(unittest.TestCase):
         self.host = os.environ.get("DB_IP", "localhost")
         self.port = int(os.environ.get("DB_PORT", 27017))
         default_connection = Connection(self.host, self.port)
+        no_auto_connection = Connection(self.host, self.port,
+                                              {"auto_start_request": False})
         pooled_connection = Connection(self.host, self.port, {"pool_size": 10})
+        no_auto_pooled_connection = Connection(self.host, self.port,
+                                               {"pool_size": 10,
+                                                "auto_start_request": False})
         self.default_db = default_connection["pymongo_test"]
         self.pooled_db = pooled_connection["pymongo_test"]
+        self.no_auto_db = no_auto_connection["pymongo_test"]
+        self.no_auto_pooled_db = no_auto_pooled_connection["pymongo_test"]
 
     def test_exceptions(self):
         self.assertRaises(TypeError, Connection, self.host, self.port,
@@ -41,10 +48,83 @@ class TestPooling(unittest.TestCase):
         self.assertRaises(ValueError, Connection, self.host, self.port,
                           {"pool_size": 0})
 
+    # NOTE this test is non-deterministic
+    def test_end_request(self):
+        count = 0
+        for _ in range(100):
+            self.default_db.test.remove({})
+            self.default_db.test.insert({})
+            if not self.default_db.test.find_one():
+                count += 1
+        self.assertEqual(0, count)
+
+        count = 0
+        for _ in range(100):
+            self.default_db.test.remove({})
+            self.default_db.test.insert({})
+            self.default_db.connection().end_request()
+            if not self.default_db.test.find_one():
+                count += 1
+        self.assertEqual(0, count)
+
+        count = 0
+        for _ in range(100):
+            self.pooled_db.test.remove({})
+            self.pooled_db.test.insert({})
+            if not self.pooled_db.test.find_one():
+                count += 1
+        self.assertEqual(0, count)
+
+        count = 0
+        for _ in range(3000):
+            self.pooled_db.test.remove({})
+            self.pooled_db.test.insert({})
+            self.pooled_db.connection().end_request()
+            if not self.pooled_db.test.find_one():
+                count += 1
+        self.assertNotEqual(0, count)
+
+    # NOTE this test is non-deterministic
+    def test_no_auto_start_request(self):
+        count = 0
+        for _ in range(100):
+            self.no_auto_db.test.remove({})
+            self.no_auto_db.test.insert({})
+            if not self.no_auto_db.test.find_one():
+                count += 1
+        self.assertEqual(0, count)
+
+        count = 0
+        for _ in range(3000):
+            self.no_auto_pooled_db.test.remove({})
+            self.no_auto_pooled_db.test.insert({})
+            if not self.no_auto_pooled_db.test.find_one():
+                count += 1
+        self.assertNotEqual(0, count)
+
+        count = 0
+        for _ in range(100):
+            self.no_auto_db.connection().start_request()
+            self.no_auto_db.test.remove({})
+            self.no_auto_db.test.insert({})
+            if not self.no_auto_db.test.find_one():
+                count += 1
+            self.no_auto_db.connection().end_request()
+        self.assertEqual(0, count)
+
+        count = 0
+        for _ in range(100):
+            self.no_auto_pooled_db.connection().start_request()
+            self.no_auto_pooled_db.test.remove({})
+            self.no_auto_pooled_db.test.insert({})
+            if not self.no_auto_pooled_db.test.find_one():
+                count += 1
+            self.no_auto_pooled_db.connection().end_request()
+        self.assertEqual(0, count)
+
+
 # TODO more tests for this!
 # test auth support
-# test request support
-# test pools of size 1 and large pools
 # test multithreaded stuff
 
 if __name__ == "__main__":
