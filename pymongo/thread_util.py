cf308e3b09efe2a7397d6d4a86aed8636584911e
Mike Dirolf <mike@10gen.com>
2009-05-28 14:18:28 -0400

    TimeoutableLock

---
+++ pymongo/thread_util.py
@@ -0,0 +1,98 @@
+# Copyright 2009 10gen, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Some utilities for dealing with threading."""
+
+import threading
+import time
+
+class TimeoutableLock(object):
+    """Lock implementation that allows blocking acquires to timeout.
+    """
+    def __init__(self):
+        self.__unlocked = threading.Event()
+        self.__unlocked.set()
+        self.__lock = threading.Lock()
+
+    def acquire(self, blocking=True, timeout=None):
+        """Acquire the lock, blocking or non-blocking.
+
+        When invoked without arguments, block until the lock is unlocked, then
+        set it to locked, and return True.
+
+        When invoked with the `blocking` argument set to True, do the same thing
+        as when called without arguments, and return True.
+
+        When invoked with the `blocking` argument set to False, do not block. If
+        a call without an argument would block, return False immediately;
+        otherwise do the same thing as when called without arguments, and return
+        True.
+
+        If `blocking` is True and `timeout` is not None then `timeout` specifies
+        a timeout in seconds. If the lock cannot be acquired within the time
+        limit return False. If `blocking` is False then `timeout` is ignored.
+
+        :Parameters:
+          - `blocking` (optional): perform a blocking acquire
+          - `timeout` (optional): add a time limit to a blocking acquire
+        """
+        did_acquire = False
+
+        self.__lock.acquire()
+
+        if self.__unlocked.is_set():
+            self.__unlocked.clear()
+            did_acquire = True
+        elif blocking:
+            if timeout is not None:
+                start_blocking = time.time()
+            while True:
+                self.__lock.release()
+
+                if timeout is not None:
+                    self.__unlocked.wait(start_blocking + timeout - time.time())
+                else:
+                    self.__unlocked.wait()
+
+                self.__lock.acquire()
+
+                if self.__unlocked.is_set():
+                    self.__unlocked.clear()
+                    did_acquire = True
+                    break
+                elif timeout is not None and time.time() > start_blocking + timeout:
+                    break
+
+        self.__lock.release()
+        return did_acquire
+
+    def release(self):
+        """Release the lock.
+
+        When the lock is locked, reset it to unlocked and return. If any other
+        threads are blocked waiting for the lock to become unlocked, allow
+        exactly one of them to proceed.
+
+        Do not call this method when the lock is unlocked - a RuntimeError will
+        be raised.
+
+        There is no return value.
+        """
+        self.__lock.acquire()
+        if self.__unlocked.is_set():
+            self.__lock.release()
+            raise RuntimeError("trying to release an unlocked TimeoutableLock")
+
+        self.__unlocked.set()
+        self.__lock.release()
---
+++ test/test_thread_util.py
@@ -0,0 +1,111 @@
+# Copyright 2009 10gen, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Test some utilities for threading."""
+
+import unittest
+import threading
+import time
+
+from pymongo.thread_util import TimeoutableLock
+
+class AcquireWithTimeout(threading.Thread):
+    def __init__(self, lock, timeout):
+        threading.Thread.__init__(self)
+        self.__lock = lock
+        self.__timeout = timeout
+
+    def run(self):
+        assert self.__lock.acquire(timeout=self.__timeout)
+
+class AcquireHoldRelease(threading.Thread):
+    def __init__(self, lock, hold_time, timeout, counter):
+        threading.Thread.__init__(self)
+        self.__lock = lock
+        self.__hold_time = hold_time
+        self.__timeout = timeout
+        self.__counter = counter
+
+    def run(self):
+        if not self.__lock.acquire(timeout=self.__timeout):
+            self.__counter.assert_count += 1
+            return
+        time.sleep(self.__hold_time)
+        self.__lock.release()
+
+class TestTimeoutableLock(unittest.TestCase):
+    def test_basic(self):
+        lock = TimeoutableLock()
+
+        self.assert_(lock.acquire())
+        self.failIf(lock.acquire(False))
+
+        lock.release()
+        self.assert_(lock.acquire())
+
+        lock.release()
+        self.assertRaises(RuntimeError, lock.release)
+
+    def test_timeout(self):
+        lock = TimeoutableLock()
+        self.assert_(lock.acquire())
+        before = time.time()
+        self.failIf(lock.acquire(timeout=0.25))
+        self.assertAlmostEqual(0.25, time.time() - before, 2)
+
+    def test_blocking_acquire(self):
+        lock = TimeoutableLock()
+        self.assert_(lock.acquire())
+        t = AcquireWithTimeout(lock, None)
+        t.start()
+        lock.release()
+        t.join()
+        self.failIf(lock.acquire(False))
+
+    def test_acquire_early(self):
+        lock = TimeoutableLock()
+        self.assert_(lock.acquire())
+        t = AcquireWithTimeout(lock, 10)
+
+        before = time.time()
+        t.start()
+        lock.release()
+        t.join()
+        self.assert_(time.time() - before < 1)
+        self.failIf(lock.acquire(False))
+
+    def test_multiple_threads(self):
+        lock = TimeoutableLock()
+
+        class Foo:
+            pass
+        counter = Foo()
+        counter.assert_count = 0
+
+        def start_and_join(thread_count):
+            threads = [AcquireHoldRelease(lock, 0.1, 0.3, counter) for _ in range(thread_count)]
+            for thread in threads:
+                thread.start()
+            for thread in threads:
+                thread.join()
+
+        start_and_join(3)
+        self.failIf(counter.assert_count)
+        start_and_join(4)
+        self.assertEqual(counter.assert_count, 1)
+        start_and_join(3)
+        self.assertEqual(counter.assert_count, 1)
+
+if __name__ == "__main__":
+    unittest.main()
