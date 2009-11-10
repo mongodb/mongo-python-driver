f6a3696c910e15a0e2246ae057ad40ec2e42c8db
Mike Dirolf <mike@10gen.com>
2009-11-10 17:28:00 -0500

    helpers for encoding / decoding documents to and from JSON

---
+++ pymongo/json_util.py
@@ -0,0 +1,56 @@
+# Copyright 2009 10gen, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tools for using the json (or simplejson) module with MongoDB documents.
+
+This module provides two methods: `object_hook` and `default`. These names are
+pretty terrible, but match the names used in Python's `json library
+<http://docs.python.org/library/json.html>`_. They allow for specialized
+encoding and decoding of MongoDB documents into `Mongo Extended JSON
+<http://www.mongodb.org/display/DOCS/Mongo+Extended+JSON>`_'s *Strict* mode.
+This lets you encode / decode MongoDB documents to JSON even when they use
+special PyMongo types.
+
+Example usage (serialization):
+>>> json.dumps(..., default=json_util.default)
+
+Example usage (deserialization):
+>>> json.loads(..., object_hook=json_util.object_hook)
+
+Currently this only handles special encoding and decoding for ObjectId and
+DBRef instancs.
+"""
+
+import base64
+
+from objectid import ObjectId
+from dbref import DBRef
+
+# TODO support other types, like Binary, Code, datetime & regex
+# Binary and Code are tricky because they subclass str so json thinks it can
+# handle them. Not sure what the proper way to get around this is...
+
+def object_hook(dct):
+    if "$oid" in dct:
+        return ObjectId(str(dct["$oid"]))
+    if "$ref" in dct:
+        return DBRef(dct["$ref"], dct["$id"], dct.get("$db", None))
+    return dct
+
+def default(obj):
+    if isinstance(obj, ObjectId):
+        return {"$oid": str(obj)}
+    if isinstance(obj, DBRef):
+        return obj.as_doc()
+    raise TypeError("%r is not JSON serializable" % obj)
---
+++ test/test_json_util.py
@@ -0,0 +1,61 @@
+# Copyright 2009 10gen, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Test some utilities for working with JSON and PyMongo."""
+
+import unittest
+import sys
+json_lib = True
+try:
+    import json
+except ImportError:
+    try:
+        import simplejson as json
+    except ImportError:
+        json_lib = False
+
+from nose.plugins.skip import SkipTest
+
+sys.path[0:0] = [""]
+
+from pymongo.json_util import default, object_hook
+from pymongo.objectid import ObjectId
+from pymongo.dbref import DBRef
+
+class TestJsonUtil(unittest.TestCase):
+
+    def setUp(self):
+        if not json_lib:
+            raise SkipTest()
+
+    def round_trip(self, doc):
+        def round_tripped(doc):
+            return json.loads(json.dumps(doc, default=default),
+                              object_hook=object_hook)
+
+        self.assertEqual(doc, round_tripped(doc))
+
+    def test_basic(self):
+        self.round_trip({"hello": "world"})
+
+    def test_objectid(self):
+        self.round_trip({"id": ObjectId()})
+
+    def test_dbref(self):
+        self.round_trip({"ref": DBRef("foo", 5)})
+        self.round_trip({"ref": DBRef("foo", 5, "db")})
+
+
+if __name__ == "__main__":
+    unittest.main()
