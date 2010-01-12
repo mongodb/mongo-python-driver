2307a7852b1132104d689e3c58152aefdbd08a66
Mike Dirolf <mike@10gen.com>
2010-01-12 16:52:08 -0500

    minor: doc

--- pymongo/errors.py
+++ pymongo/errors.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Exceptions raised by the Mongo driver."""
+"""Exceptions raised by PyMongo."""
 
 class PyMongoError(Exception):
     """Base class for all PyMongo exceptions.
