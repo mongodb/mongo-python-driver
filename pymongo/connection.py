c3cb6b94dbd4f000cefc782a045f7c9b077e783e
Mike Dirolf <mike@10gen.com>
2010-05-09 10:50:32 -0400

    oops, accidentally commited this along with previou changes

--- pymongo/connection.py
+++ pymongo/connection.py
@@ -1,27 +1,28 @@
 # Copyright 2009-2010 10gen, Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License.  You
+# may obtain a copy of the License at
 #
 # http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.  See the License for the specific language governing
+# permissions and limitations under the License.
 
 """Tools for connecting to MongoDB.
 
-To connect to a single instance of MongoDB use :class:`Connection`. To connect
-to a replica pair use :meth:`~Connection.paired`.
+To connect to a single instance of MongoDB use :class:`Connection`. To
+connect to a replica pair use :meth:`~Connection.paired`.
 
-.. seealso:: Module :mod:`~pymongo.master_slave_connection` for connecting to
-   master-slave clusters.
+.. seealso:: Module :mod:`~pymongo.master_slave_connection` for
+   connecting to master-slave clusters.
 
 To get a :class:`~pymongo.database.Database` instance from a
-:class:`Connection` use either dictionary-style or attribute-style access:
+:class:`Connection` use either dictionary-style or attribute-style
+access:
 
 .. doctest::
 
@@ -74,9 +75,6 @@ class Pool(threading.local):
         self.socket_factory = socket_factory
         if not hasattr(self, "sockets"):
             self.sockets = []
-
-    def __del__(self):
-        self.return_socket()
 
     def socket(self):
         # we store the pid here to avoid issues with fork /
