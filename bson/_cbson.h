9c5975ab205b2ab7883009c4f28902a10041a3cd
Mike Dirolf <mike@10gen.com>
2010-09-16 13:12:05 -0400

    use unsigned for PY_SSIZE_T if we have to define it ourselves

--- bson/_cbson.h
+++ bson/_cbson.h
@@ -18,12 +18,13 @@
 #define _CBSON_H
 
 #include <Python.h>
+#include <stdio.h>
 #include "buffer.h"
 
 #if PY_VERSION_HEX < 0x02050000 && !defined(PY_SSIZE_T_MIN)
-typedef int Py_ssize_t;
-#define PY_SSIZE_T_MAX INT_MAX
-#define PY_SSIZE_T_MIN INT_MIN
+typedef unsigned int Py_ssize_t;
+#define PY_SSIZE_T_MAX UINT_MAX
+#define PY_SSIZE_T_MIN 0
 #endif
 
 int buffer_write_bytes(buffer_t buffer, const char* data, int size);
