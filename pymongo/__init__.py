d0c54416bad2746ecb11b12e23f28334ef9e4444
behackett <bernie@10gen.com>
2011-03-30 13:53:25 -0700

    BUMP 1.10

--- pymongo/__init__.py
+++ pymongo/__init__.py
@@ -39,7 +39,7 @@ ALL = 2
 """Profile all operations."""
 
 # Remember to change in setup.py as well!
-version = "1.9+"
+version = "1.10"
 """Current version of PyMongo."""
 
 Connection = PyMongo_Connection
