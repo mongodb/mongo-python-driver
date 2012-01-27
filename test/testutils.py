00f7026bfafed3b2232840ade5453a71b273e45a
A. Jesse Jiryu Davis <jesse@10gen.com>
2012-01-27 14:05:54 -0500

    Check more reliably if server has auth enabled before running authentication tests

---
+++ test/testutils.py
@@ -0,0 +1,27 @@
+# Copyright 2012 10gen, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Utilities for testing pymongo
+"""
+
+def delay(sec):
+    return '''function() {
+        var d = new Date((new Date()).getTime() + %s * 1000);
+        while (d > (new Date())) { }; return true;
+    }''' % sec
+
+def server_started_with_auth(connection):
+    command_line = connection.admin.command('getCmdLineOpts')
+    assert command_line['ok'] == 1, "getCmdLineOpts() failed"
+    return '--auth'  in command_line['argv']
