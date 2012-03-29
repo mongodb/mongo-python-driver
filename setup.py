96b133cb0cb2e3af44677006d05a1fe660c474c3
behackett <bernie@10gen.com>
2012-03-28 19:10:54 -0700

    Python 3 setup.py changes PYTHON-84
    1. Switch from setuptools to distribute
    2. Make setup.py PY3 compatible.
    3. Use 2to3 for PY3 builds.
    4. Make "test" and "nosetests" work in PY3
    5. Only write setup.cfg when needed.
    6. git ignore setup.cfg.
    
    Traditional setuptools does not support PY3.

--- setup.py
+++ setup.py
@@ -1,25 +1,37 @@
-#!/usr/bin/env python
-
-import sys
+import glob
 import os
-try:
-    import subprocess
-    has_subprocess = True
-except:
-    has_subprocess = False
-import shutil
+import subprocess
+import sys
+import warnings
 
-from ez_setup import use_setuptools
-use_setuptools()
-from setuptools import setup
-from setuptools import Feature
+# Hack to silence atexit traceback in newer python versions.
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+try:
+    from ConfigParser import SafeConfigParser
+except ImportError:
+    # PY3
+    from configparser import SafeConfigParser
+
+# Don't force people to install distribute unless
+# we have to.
+try:
+    from setuptools import setup, Feature
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, Feature
+
 from distutils.cmd import Command
 from distutils.command.build_ext import build_ext
 from distutils.errors import CCompilerError
 from distutils.errors import DistutilsPlatformError, DistutilsExecError
 from distutils.core import Extension
 
-from pymongo import version
+version = "2.1.1+"
 
 f = open("README.rst")
 try:
@@ -29,6 +41,34 @@ try:
         readme_content = ""
 finally:
     f.close()
+
+PY3 = sys.version_info[0] == 3
+
+nose_config_options = {
+    'with-xunit': '1',    # Write out nosetests.xml for CI.
+    'py3where': 'build',  # Tell nose where to find tests under PY3.
+}
+
+def write_nose_config():
+    """Write out setup.cfg. Since py3where has to be set
+    for tests to run correctly in Python 3 we create this
+    on the fly.
+    """
+    config = SafeConfigParser()
+    config.add_section('nosetests')
+    for opt, val in nose_config_options.items():
+        config.set('nosetests', opt, val)
+    try:
+        cf = open('setup.cfg', 'w')
+        config.write(cf)
+    finally:
+        cf.close()
+
+
+def should_run_tests():
+    if "test" in sys.argv or "nosetests" in sys.argv:
+        return True
+    return False
 
 
 class doc(Command):
@@ -54,27 +94,19 @@ class doc(Command):
             path = "doc/_build/%s" % version
             mode = "html"
 
-            # shutil.rmtree("doc/_build", ignore_errors=True)
             try:
                 os.makedirs(path)
             except:
                 pass
 
-        if has_subprocess:
-            status = subprocess.call(["sphinx-build", "-E", "-b", mode, "doc", path])
+        status = subprocess.call(["sphinx-build", "-E",
+                                  "-b", mode, "doc", path])
 
-            if status:
-                raise RuntimeError("documentation step '%s' failed" % mode)
+        if status:
+            raise RuntimeError("documentation step '%s' failed" % (mode,))
 
-            print ""
-            print "Documentation step '%s' performed, results here:" % mode
-            print "   %s/" % path
-        else:
-            print """
-`setup.py doc` is not supported for this version of Python.
-
-Please ask in the user forums for help.
-"""
+        sys.stdout.write("\nDocumentation step '%s' performed, results here:\n"
+                         "   %s/\n" % (mode, path))
 
 
 if sys.platform == 'win32' and sys.version_info > (2, 6):
@@ -113,27 +145,46 @@ Debian/Ubuntu: python-dev
     def run(self):
         try:
             build_ext.run(self)
-        except DistutilsPlatformError, e:
-            print e
-            print self.warning_message % ("Extension modules",
-                                          "There was an issue with your "
-                                          "platform configuration "
-                                          "- see above.")
+        except DistutilsPlatformError:
+            e = sys.exc_info()[1]
+            sys.stdout.write('%s\n' % str(e))
+            warnings.warn(self.warning_message % ("Extension modules",
+                                                  "There was an issue with "
+                                                  "your platform configuration"
+                                                  " - see above."))
+
+    def set_nose_options(self):
+        # Under python 3 we need to tell nose where to find the
+        # proper tests. if we built the C extensions this will be
+        # someplace like build/lib.<os>-<arch>-<python version>
+        if PY3:
+            ver = '.'.join(map(str, sys.version_info[:2]))
+            lib_dirs = glob.glob('build/lib*' + ver)
+            if lib_dirs:
+                nose_config_options['py3where'] = lib_dirs[0]
+        write_nose_config()
 
     def build_extension(self, ext):
+        name = ext.name
         if sys.version_info[:3] >= (2, 4, 0):
             try:
                 build_ext.build_extension(self, ext)
-            except build_errors, e:
-                print e
-                print self.warning_message % ("The %s extension "
-                                              "module" % ext.name,
-                                              "Above is the ouput showing how "
-                                              "the compilation failed.")
+                if should_run_tests():
+                    self.set_nose_options()
+            except build_errors:
+                e = sys.exc_info()[1]
+                sys.stdout.write('%s\n' % str(e))
+                warnings.warn(self.warning_message % ("The %s extension "
+                                                      "module" % (name,),
+                                                      "Above is the ouput "
+                                                      "showing how the "
+                                                      "compilation failed."))
         else:
-            print self.warning_message % ("The %s extension module" % ext.name,
-                                          "Please use Python >= 2.4 to take "
-                                          "advantage of the extension.")
+            warnings.warn(self.warning_message % ("The %s extension "
+                                                  "module" % (name,),
+                                                  "Please use Python >= 2.4 "
+                                                  "to take advantage of the "
+                                                  "extension."))
 
 c_ext = Feature(
     "optional C extensions",
@@ -147,35 +198,57 @@ c_ext = Feature(
                  Extension('pymongo._cmessage',
                            include_dirs=['bson'],
                            sources=['pymongo/_cmessagemodule.c',
-                                    'bson/_cbsonmodule.c',
-                                    'bson/time64.c',
-                                    'bson/buffer.c',
-                                    'bson/encoding_helpers.c'])])
+                                    'bson/buffer.c'])])
 
 if "--no_ext" in sys.argv:
     sys.argv = [x for x in sys.argv if x != "--no_ext"]
     features = {}
+    # If we are testing with C extensions write_nose_config
+    # will be called elsewhere.
+    if should_run_tests():
+        write_nose_config()
 elif (sys.platform.startswith("java") or
       sys.platform == "cli" or
       "PyPy" in sys.version):
-    print """
-*****************************************************
-The optional C extensions are currently not supported
-by this python implementation.
-*****************************************************
-"""
+    sys.stdout.write("""
+*****************************************************\n
+The optional C extensions are currently not supported\n
+by this python implementation.\n
+*****************************************************\n
+""")
     features = {}
 elif sys.byteorder == "big":
-    print """
-*****************************************************
-The optional C extensions are currently not supported
-on big endian platforms and will not be built.
-Performance may be degraded.
-*****************************************************
-"""
+    sys.stdout.write("""
+*****************************************************\n
+The optional C extensions are currently not supported\n
+on big endian platforms and will not be built.\n
+Performance may be degraded.\n
+*****************************************************\n
+""")
     features = {}
 else:
     features = {"c-ext": c_ext}
+
+extra_opts = {
+    "packages": ["bson", "pymongo", "gridfs"],
+    "test_suite": "nose.collector"
+}
+if PY3:
+    extra_opts["use_2to3"] = True
+    if should_run_tests():
+        # Distribute isn't smart enough to copy the
+        # tests and run 2to3 on them. We don't want to
+        # install the test suite so only do this if we
+        # are testing.
+        # https://bitbucket.org/tarek/distribute/issue/233
+        extra_opts["packages"].append("test")
+        # Hack to make "python3.x setup.py nosetests" work in python 3
+        # otherwise it won't run 2to3 before running the tests.
+        if "nosetests" in sys.argv:
+            sys.argv.remove("nosetests")
+            sys.argv.append("test")
+            # All "nosetests" does is import and run nose.main.
+            extra_opts["test_suite"] = "nose.main"
 
 setup(
     name="pymongo",
@@ -188,12 +261,10 @@ setup(
     maintainer_email="bernie@10gen.com",
     url="http://github.com/mongodb/mongo-python-driver",
     keywords=["mongo", "mongodb", "pymongo", "gridfs", "bson"],
-    packages=["bson", "pymongo", "gridfs"],
     install_requires=[],
     features=features,
     license="Apache License, Version 2.0",
-    tests_require=['nose'],
-    test_suite="nose.collector",
+    tests_require=["nose"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
@@ -201,7 +272,17 @@ setup(
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
         "Operating System :: POSIX",
-        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.4",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: Jython",
+        "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Database"],
     cmdclass={"build_ext": custom_build_ext,
-              "doc": doc})
+              "doc": doc},
+    **extra_opts
+)
