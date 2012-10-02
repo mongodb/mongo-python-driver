733fafec8406580be5a7b6f5a0d6c21f0f508b0d
behackett <bernie@10gen.com>
2012-10-02 16:21:26 -0700

    Note Python 3.3 support.

--- setup.py
+++ setup.py
@@ -302,6 +302,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.1",
         "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: Jython",
         "Programming Language :: Python :: Implementation :: PyPy",
