1db08de711fef1d33c6c7c400984423ec7909f9d
behackett <bernie@10gen.com>
2013-09-17 09:11:01 -0400

    Bump setuptools, hopefully for the last time.

--- ez_setup.py
+++ ez_setup.py
@@ -29,7 +29,7 @@ try:
 except ImportError:
     USER_SITE = None
 
-DEFAULT_VERSION = "1.1.4"
+DEFAULT_VERSION = "1.1.5"
 DEFAULT_URL = "https://pypi.python.org/packages/source/s/setuptools/"
 
 def _python_cmd(*args):
