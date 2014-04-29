7d55d7707296ac816e146b127f3053ca9eab7cf3
Bernie Hackett <bernie@10gen.com>
2014-04-29 11:40:26 -0700

    Version -> +

--- pymongo/__init__.py
+++ pymongo/__init__.py
@@ -67,7 +67,7 @@ SLOW_ONLY = 1
 ALL = 2
 """Profile all operations."""
 
-version_tuple = (2, 7)
+version_tuple = (2, 7, '+')
 
 def get_version_string():
     if isinstance(version_tuple[-1], basestring):
