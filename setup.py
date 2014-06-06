6fbb4c5307f4039c39ab8e608f60ffae97951874
behackett <bernie@10gen.com>
2014-06-06 14:47:41 -0700

    Version -> +

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ from distutils.errors import CCompilerError
 from distutils.errors import DistutilsPlatformError, DistutilsExecError
 from distutils.core import Extension
 
-version = "2.7.1"
+version = "2.7.1+"
 
 f = open("README.rst")
 try:
