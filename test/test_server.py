cfcc6019226c41f324f2c6e7d9014ec7a6cf2468
A. Jesse Jiryu Davis <jesse@mongodb.com>
2014-09-24 19:03:41 -0400

    Fix test_server.py's docstring.

--- test/test_server.py
+++ test/test_server.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Test the server_description module."""
+"""Test the server module."""
 
 import sys
 
