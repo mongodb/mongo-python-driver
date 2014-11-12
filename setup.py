21ef41346ae1966e4330481074e4b48d43db6803
Bernie Hackett <bernie@10gen.com>
2014-11-12 09:55:38 -0800

    BUMP 2.8rc0

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ from distutils.errors import CCompilerError
 from distutils.errors import DistutilsPlatformError, DistutilsExecError
 from distutils.core import Extension
 
-version = "2.7.2+"
+version = "2.8rc0"
 
 f = open("README.rst")
 try:
