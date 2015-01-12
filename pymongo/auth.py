a2551b96f2ccf9fba864f524353767d2c4496eb1
Bernie Hackett <bernie@10gen.com>
2015-01-12 12:09:19 -0800

    PYTHON-818 - Work around issues with unicode_literals and pbkdf2_hmac.

--- pymongo/auth.py
+++ pymongo/auth.py
@@ -14,6 +14,9 @@
 
 """Authentication helpers."""
 
+# TODO: We may not want to do this. It causes issues
+# with passing the string 'sha1' to hashlib.pbkdf2_hmac
+# in cpython 2.x >= 2.8 and pypy >= 2.4.x.
 from __future__ import unicode_literals
 
 import hmac
@@ -101,7 +104,7 @@ try:
     from backports.pbkdf2 import pbkdf2_hmac
 
     def _hi(data, salt, iterations):
-        return pbkdf2_hmac('sha1', data, salt, iterations)
+        return pbkdf2_hmac(str('sha1'), data, salt, iterations)
 
 except ImportError:
     try:
@@ -109,7 +112,7 @@ except ImportError:
         from hashlib import pbkdf2_hmac
 
         def _hi(data, salt, iterations):
-            return pbkdf2_hmac('sha1', data, salt, iterations)
+            return pbkdf2_hmac(str('sha1'), data, salt, iterations)
 
     except ImportError:
 
