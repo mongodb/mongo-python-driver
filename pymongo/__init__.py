18328a909545ece6e1cd7e172e28271a59e367d5
Bernie Hackett <bernie@10gen.com>
2015-04-07 15:05:22 -0700

    BUMP 3.0

--- pymongo/__init__.py
+++ pymongo/__init__.py
@@ -70,7 +70,7 @@ SLOW_ONLY = 1
 ALL = 2
 """Profile all operations."""
 
-version_tuple = (3, 0, "rc1.post0")
+version_tuple = (3, 0)
 
 def get_version_string():
     if isinstance(version_tuple[-1], str):
