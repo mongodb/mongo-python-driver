43347f61f1b7428f52d8f585f363bfc742c533b5
Bernie Hackett <bernie@10gen.com>
2015-04-13 11:22:45 -0700

    Start work on 2.8.1.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ from distutils.errors import CCompilerError
 from distutils.errors import DistutilsPlatformError, DistutilsExecError
 from distutils.core import Extension
 
-version = "2.8"
+version = "2.8.1.dev0"
 
 f = open("README.rst")
 try:
