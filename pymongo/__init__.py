056d1a01f4c9c6a5d6a1147c937a4906adfd317f
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-05-19 13:34:38 -0400

    Version -> +

--- pymongo/__init__.py
+++ pymongo/__init__.py
@@ -70,7 +70,7 @@ SLOW_ONLY = 1
 ALL = 2
 """Profile all operations."""
 
-version_tuple = (3, 0, 2)
+version_tuple = (3, 0, 2, "+")
 
 def get_version_string():
     if isinstance(version_tuple[-1], str):
