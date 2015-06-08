1c806791a9cbc61192e1b08c8c2f45ed18e6d0b8
Jason R. Coombs <jaraco@jaraco.com>
2015-06-08 09:44:51 -0400

    PYTHON-946 - Allow fields to be any sequence.

--- pymongo/helpers.py
+++ pymongo/helpers.py
@@ -270,7 +270,7 @@ def _check_write_command_response(results):
 
 
 def _fields_list_to_dict(fields, option_name):
-    """Takes a list of field names and returns a matching dictionary.
+    """Takes a sequence of field names and returns a matching dictionary.
 
     ["a", "b"] becomes {"a": 1, "b": 1}
 
@@ -280,15 +280,13 @@ def _fields_list_to_dict(fields, option_name):
     """
     if isinstance(fields, collections.Mapping):
         return fields
-    elif isinstance(fields, list):
-        as_dict = {}
-        for field in fields:
-            if not isinstance(field, string_type):
-                raise TypeError("%s must be a list of key names, each an "
-                                "instance of %s" % (option_name,
-                                                    string_type.__name__))
-            as_dict[field] = 1
-        return as_dict
-    else:
-        raise TypeError("%s must be a mapping or "
-                        "list of key names" % (option_name,))
+
+    if isinstance(fields, collections.Sequence):
+        if not all(isinstance(field, string_type) for field in fields):
+            raise TypeError("%s must be a list of key names, each an "
+                            "instance of %s" % (option_name,
+                                                string_type.__name__))
+        return dict.fromkeys(fields, 1)
+
+    raise TypeError("%s must be a mapping or "
+                    "list of key names" % (option_name,))
