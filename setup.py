07b0f78585a92511c8c390b5eb30242f51313b96
Bernie Hackett <bernie@10gen.com>
2016-03-02 15:18:11 -0500

    Start work on 3.2.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from distutils.errors import CCompilerError, DistutilsOptionError
 from distutils.errors import DistutilsPlatformError, DistutilsExecError
 from distutils.core import Extension
 
-version = "3.2.1"
+version = "3.2.2.dev0"
 
 f = open("README.rst")
 try:
