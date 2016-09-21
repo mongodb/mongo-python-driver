100a069a6387b0622adf91a895c9d92628fb74ad
Bernie Hackett <bernie@10gen.com>
2016-09-21 11:15:01 -0700

    Continue work on 3.4

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from distutils.errors import CCompilerError, DistutilsOptionError
 from distutils.errors import DistutilsPlatformError, DistutilsExecError
 from distutils.core import Extension
 
-version = "3.4rc0"
+version = "3.4rc1.dev0"
 
 f = open("README.rst")
 try:
