bb43928f14e67a9aabfea3126729943afa565dda
Shane Harvey <shane.harvey@mongodb.com>
2017-01-25 16:24:33 -0800

    PYTHON-1225 Test Python toolchain against Ubuntu 16.04 (x86_64)
    MongoDB versions < 3.2 do not come with SSL support on Ubuntu 16.04 (x86_64).

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -6,33 +6,35 @@ set -o errexit  # Exit the script with error if any of the commands fail
 #       AUTH                    Set to enable authentication. Defaults to "noauth"
 #       SSL                     Set to enable SSL. Defaults to "nossl"
 #       MONGODB_URI             Set the suggested connection MONGODB_URI (including credentials and topology info)
-#       MARCH                   Machine Architecture. Defaults to lowercase uname -m
+#       PYTHON_BINARY           The Python version to use. Defaults to whatever is available
 
 
 AUTH=${AUTH:-noauth}
 SSL=${SSL:-nossl}
 MONGODB_URI=${MONGODB_URI:-}
-
-OS=$(uname -s | tr '[:upper:]' '[:lower:]')
-[ -z "$MARCH" ] && MARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
+PYTHON_BINARY=${PYTHON_BINARY:-}
 
 
 if [ "$AUTH" != "noauth" ]; then
-  export DB_USER="bob"
-  export DB_PASSWORD="pwd123"
+    export DB_USER="bob"
+    export DB_PASSWORD="pwd123"
 fi
 
 if [ "$SSL" != "nossl" ]; then
-   export CLIENT_PEM="$DRIVERS_TOOLS/.evergreen/x509gen/client.pem"
-   export CA_PEM="$DRIVERS_TOOLS/.evergreen/x509gen/ca.pem"
+    export CLIENT_PEM="$DRIVERS_TOOLS/.evergreen/x509gen/client.pem"
+    export CA_PEM="$DRIVERS_TOOLS/.evergreen/x509gen/ca.pem"
 fi
 
-PYTHON=$(command -v python || command -v python3)
-
-if [ "$PYTHON" == "" ]; then
-    echo "Cannot test without python or python3 installed!"
-    exit 1
+if [ -z "$PYTHON_BINARY" ]; then
+    PYTHON=$(command -v python || command -v python3)
+    if [ -z "$PYTHON" ]; then
+        echo "Cannot test without python or python3 installed!"
+        exit 1
+    fi
+else
+    PYTHON="$PYTHON_BINARY"
 fi
+
 
 echo "Running $AUTH tests over $SSL with python $PYTHON, connecting to $MONGODB_URI"
 $PYTHON -c 'import sys; print(sys.version)'
