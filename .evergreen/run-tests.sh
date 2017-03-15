14df77835a2b6f2c1d53ffae90e7fb120c3402c4
Bernie Hackett <bernie@10gen.com>
2017-03-15 13:55:56 -0700

    Various CI script fixes

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -7,6 +7,8 @@ set -o errexit  # Exit the script with error if any of the commands fail
 #       SSL                     Set to enable SSL. Defaults to "nossl"
 #       MONGODB_URI             Set the suggested connection MONGODB_URI (including credentials and topology info)
 #       PYTHON_BINARY           The Python version to use. Defaults to whatever is available
+#       GREEN_FRAMEWORK         The green framwork to test with, if any.
+#       C_EXTENSIONS            Pass --no_ext to setup.py, or not.
 
 
 AUTH=${AUTH:-noauth}
@@ -37,6 +39,13 @@ else
     PYTHON="$PYTHON_BINARY"
 fi
 
+# Don't download unittest-xml-reporting from pypi, which often fails.
+HAVE_XMLRUNNER=$($PYTHON -c "import pkgutil; print(1 if pkgutil.find_loader('xmlrunner') else 0)")
+if [ $HAVE_XMLRUNNER = "1" ]; then
+    OUTPUT="--xunit-output=xunit-results"
+else
+    OUTPUT=""
+fi
 
 echo "Running $AUTH tests over $SSL with python $PYTHON, connecting to $MONGODB_URI"
 $PYTHON -c 'import sys; print(sys.version)'
@@ -46,8 +55,8 @@ $PYTHON -c 'import sys; print(sys.version)'
 
 $PYTHON setup.py clean
 if [ -z "$GREEN_FRAMEWORK" ]; then
-    $PYTHON setup.py $C_EXTENSIONS test --xunit-output=xunit-results
+    $PYTHON setup.py $C_EXTENSIONS test $OUTPUT
 else
     # --no_ext has to come before "test" so there is no way to toggle extensions here.
-    $PYTHON green_framework_test.py $GREEN_FRAMEWORK --xunit-output=xunit-results
+    $PYTHON green_framework_test.py $GREEN_FRAMEWORK $OUTPUT
 fi
