95c6513e181d8bab229cb6f8573d53bb1d3b076f
Bernie Hackett <bernie@10gen.com>
2017-10-27 13:50:58 -0700

    PYTHON-1375 - Run mockup tests against PyMongo master

--- .evergreen/run-mockupdb-tests.sh
+++ .evergreen/run-mockupdb-tests.sh
@@ -3,11 +3,16 @@
 set -o xtrace
 set -o errexit
 
-export PYTHONPATH=${PROJECT_DIRECTORY}
-
 ${PYTHON_BINARY} setup.py clean
-
 cd ..
+${PYTHON_BINARY} -m virtualenv mockuptests
+. mockuptests/bin/activate
+trap "deactivate" EXIT HUP
+
+# Install PyMongo from git clone so mockup-tests don't
+# download it from pypi.
+pip install ${PROJECT_DIRECTORY}
+
 git clone https://github.com/ajdavis/pymongo-mockup-tests.git
 cd pymongo-mockup-tests
-${PYTHON_BINARY} setup.py test
+python setup.py test
