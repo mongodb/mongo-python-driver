507f954ed4f5778b2cc2d34fd57ff5525f4d2be0
Bernie Hackett <bernie@10gen.com>
2017-12-01 17:23:39 -0800

    Update copyright dates

--- test/test_common.py
+++ test/test_common.py
@@ -1,4 +1,4 @@
-# Copyright 2011-2015 MongoDB, Inc.
+# Copyright 2011-present MongoDB, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
