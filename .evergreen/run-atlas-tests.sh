d37164e340a78b1baacbcf670c28fe4b60b78d73
Bernie Hackett <bernie.hackett@gmail.com>
2018-04-16 23:33:19 -0700

    PYTHON-1436 - Test pypy3.2 connections to Atlas

--- .evergreen/run-atlas-tests.sh
+++ .evergreen/run-atlas-tests.sh
@@ -5,9 +5,10 @@ set -o errexit
 
 export JAVA_HOME=/opt/java/jdk8
 
-PLATFORM=$(${PYTHON_BINARY} -c "import platform, sys; sys.stdout.write(platform.system())")
+IMPL=$(${PYTHON_BINARY} -c "import platform, sys; sys.stdout.write(platform.python_implementation())")
 PYTHON_VERSION=$(${PYTHON_BINARY} -c "import sys; print('.'.join(map(str, sys.version_info[:2])))")
-if [ $PYTHON_VERSION = "2.6" -o $PLATFORM = "Java" ]; then
+if [ $PYTHON_VERSION = "2.6" -o $IMPL = "Jython" ]; then
+    # TODO - When Jython has its own virtualenv install use it instead.
     /opt/python/2.6/bin/virtualenv -p ${PYTHON_BINARY} --never-download --no-wheel atlastest
     . atlastest/bin/activate
     trap "deactivate; rm -rf atlastest" EXIT HUP
@@ -15,6 +16,15 @@ if [ $PYTHON_VERSION = "2.6" -o $PLATFORM = "Java" ]; then
     if [ $PYTHON_VERSION = "2.6" ]; then
         pip install unittest2
     fi
+    PYTHON=python
+elif [ $IMPL = "PyPy" -a $PYTHON_VERSION = "3.2" ]; then
+    $PYTHON_BINARY -m virtualenv --never-download --no-wheel atlastest
+    . atlastest/bin/activate
+    trap "deactivate; rm -rf atlastest" EXIT HUP
+    pip install certifi
+    # Portable pypy3.2 can't load CA certs from the system.
+    # https://github.com/squeaky-pl/portable-pypy/issues/15
+    export SSL_CERT_FILE=$(python -c "import certifi; print(certifi.where())")
     PYTHON=python
 else
     PYTHON=$PYTHON_BINARY
