6c632614545b123c3b772520e19009ecb6bd85ef
Prashant Mital <prashantmital@gmail.com>
2018-06-15 11:44:02 -0700

    REF: unify aggregation pipeline runner for all change stream classes

--- pymongo/change_stream.py
+++ pymongo/change_stream.py
@@ -54,6 +54,17 @@ class ChangeStream(object):
         self._cursor = self._create_cursor()
 
     @property
+    def _aggregation_target(self):
+        """The argument to pass to the aggregate command."""
+        raise NotImplementedError
+
+    @property
+    def _database(self):
+        """The database against which the aggregation commands for
+        this ChangeStream will be run. """
+        raise NotImplementedError
+
+    @property
     def _default_start_at_operation_time(self):
         """
         Return the current operationTime timestamp from the server. 
@@ -61,11 +72,57 @@ class ChangeStream(object):
         """
         return self._database.command('isMaster')['operationTime']
 
+    def _run_aggregation_cmd(self, pipeline, session, explicit_session):
+        """Run the full aggregation pipeline for this ChangeStream and return
+        the corresponding CommandCursor.
+        """
+        common.validate_list('pipeline', pipeline)
+        validate_collation_or_none(self._collation)
+        common.validate_non_negative_integer_or_none(
+            "batchSize", self._batch_size
+        )
+
+        cmd = SON([("aggregate", self._aggregation_target),
+                   ("pipeline", pipeline),
+                   ("cursor", {})])
+
+        with self._database.client._socket_for_reads(self._target._read_preference_for(session)) as (sock_info, slave_ok):
+            result = sock_info.command(
+                self._database.name,
+                cmd,
+                slave_ok,
+                self._target._read_preference_for(session),
+                self._target.codec_options,
+                parse_write_concern_error=True,
+                read_concern=self._target.read_concern,
+                collation=self._collation,
+                session=session,
+                client=self._database.client)
+
+            cursor = result["cursor"]
+            ns = cursor["ns"]
+            _, collname = ns.split(".", 1)
+            aggregation_collection = self._database.get_collection(
+                collname, codec_options=self._target.codec_options,
+                read_preference=self._target._read_preference_for(session),
+                write_concern=self._target.write_concern,
+                read_concern=self._target.read_concern
+            )
+
+            return CommandCursor(
+                aggregation_collection, cursor, sock_info.address,
+                batch_size=self._batch_size or 0,
+                max_await_time_ms=self._max_await_time_ms,
+                session=session, explicit_session=explicit_session
+            )
+
     def _create_cursor(self):
-        """
-        Instantiate the cursor corresponding to this ChangeStream.
-        """
-        raise NotImplementedError
+        with self._database.client._tmp_session(self._session, close=False) as s:
+            return self._run_aggregation_cmd(
+                self._full_pipeline(),
+                session=s,
+                explicit_session=self._session is not None
+            )
 
     def _full_pipeline(self, inject_options=None):
         """Return the full aggregation pipeline for this ChangeStream."""
@@ -129,12 +186,6 @@ class ChangeStream(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
 
-    @property
-    def _database(self):
-        """The database against which the aggregation commands for
-        this ChangeStream will be run. """
-        raise NotImplementedError
-
 
 class CollectionChangeStream(ChangeStream):
     """ Class for creating a change stream on a collection. 
@@ -145,12 +196,9 @@ class CollectionChangeStream(ChangeStream):
     .. versionadded: 3.6
     .. mongodoc:: changeStreams
     """
-
-    def _create_cursor(self):
-        return self._target.aggregate(
-            self._full_pipeline(), self._session, batchSize=self._batch_size,
-            collation=self._collation, maxAwaitTimeMS=self._max_await_time_ms,
-        )
+    @property
+    def _aggregation_target(self):
+        return self._target.name
 
     @property
     def _database(self):
@@ -166,60 +214,9 @@ class DatabaseChangeStream(ChangeStream):
     .. versionadded: 3.7
     .. mongodoc:: changeStreams
     """
-
-    def _run_aggregation_cmd(self, pipeline, session, explicit_session):
-        """Run the full aggregation pipeline for this ChangeStream and return
-        the corresponding CommandCursor.
-        """
-        common.validate_list('pipeline', pipeline)
-        validate_collation_or_none(self._collation)
-        common.validate_non_negative_integer_or_none(
-            "batchSize", self._batch_size
-        )
-
-        cmd = SON([("aggregate", 1),
-                   ("pipeline", pipeline),
-                   ("cursor", {})])
-
-        with self._database.client._socket_for_reads(self._target._read_preference_for(session)) as (sock_info, slave_ok):
-            # Avoid auto-injecting a session: aggregate() passes a session,
-            # aggregate_raw_batches() passes none.
-            result = sock_info.command(
-                self._database.name,
-                cmd,
-                slave_ok,
-                self._target._read_preference_for(session),
-                self._target.codec_options,
-                parse_write_concern_error=True,
-                read_concern=self._target.read_concern,
-                collation=self._collation,
-                session=session,
-                client=self._database.client)
-
-            cursor = result["cursor"]
-            ns = cursor["ns"]
-            _, collname = ns.split(".", 1)
-            aggregation_collection = self._database.get_collection(
-                collname, codec_options=self._target.codec_options,
-                read_preference=self._target._read_preference_for(session),
-                write_concern=self._target.write_concern,
-                read_concern=self._target.read_concern
-            )
-
-            return CommandCursor(
-                aggregation_collection, cursor, sock_info.address,
-                batch_size=self._batch_size or 0,
-                max_await_time_ms=self._max_await_time_ms,
-                session=session, explicit_session=explicit_session
-            )
-
-    def _create_cursor(self):
-        with self._database.client._tmp_session(self._session, close=False) as s:
-            return self._run_aggregation_cmd(
-                self._full_pipeline(),
-                session=s,
-                explicit_session=self._session is not None
-            )
+    @property
+    def _aggregation_target(self):
+        return 1
 
     @property
     def _database(self):
