8ea87f762a307c9d5714978f9ed5eb4eba28790e
Prashant Mital <prashantmital@gmail.com>
2018-08-10 14:22:30 -0700

    Require at least 1 secondary

--- test/test_examples.py
+++ test/test_examples.py
@@ -1119,6 +1119,7 @@ class TestTransactionExamples(IntegrationTest):
 
 class TestCausalConsistencyExamples(IntegrationTest):
     @client_context.require_version_min(3, 6, 0)
+    @client_context.require_secondaries_count(1)
     def test_causal_consistency(self):
         # Causal consistency examples
         client = self.client
