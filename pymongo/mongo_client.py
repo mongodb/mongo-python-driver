30b2ecde45267a1acd3c536be89a942197344abf
Maxence Prevost <maxence.prevost@gmail.com>
2019-03-06 16:38:44 +0700

    Close sockets on object destruction
    This would be useful when used in Jupyter notebook.
    Or when using PyMongo in a generator in Keras (multi-threaded environment).

--- pymongo/mongo_client.py
+++ pymongo/mongo_client.py
@@ -1961,6 +1961,9 @@ class MongoClient(common.BaseObject):
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
 
+    def __del__(self):
+        self.close()
+         
     def __iter__(self):
         return self
 
