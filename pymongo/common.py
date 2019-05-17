2b57cbb98060c75c5b19f072e203a9dd03c5f894
Shane Harvey <shnhrv@gmail.com>
2019-05-17 13:00:24 -0700

    PYTHON-1649 Bump max wire version to 8 (MongoDB 4.2)

--- pymongo/common.py
+++ pymongo/common.py
@@ -53,7 +53,7 @@ MAX_WRITE_BATCH_SIZE = 1000
 # What this version of PyMongo supports.
 MIN_SUPPORTED_SERVER_VERSION = "2.6"
 MIN_SUPPORTED_WIRE_VERSION = 2
-MAX_SUPPORTED_WIRE_VERSION = 5
+MAX_SUPPORTED_WIRE_VERSION = 8
 
 # Frequency to call ismaster on servers, in seconds.
 HEARTBEAT_FREQUENCY = 10
