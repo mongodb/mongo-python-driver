6c4e1c93716687cbe970354054c2d1ffd599f6c7
paul fisher <paul@pfish.zone>
2019-11-22 17:57:00 -0500

    PYTHON-2061 bson: check for negative entry size in decode_file_iter (#429)
    Raise InvalidBSON instead of ValueError when decode_file_iter reads an invalid
    BSON object size.

--- bson/__init__.py
+++ bson/__init__.py
@@ -1166,7 +1166,7 @@ def decode_file_iter(file_obj, codec_options=DEFAULT_CODEC_OPTIONS):
         elif len(size_data) != 4:
             raise InvalidBSON("cut off in middle of objsize")
         obj_size = _UNPACK_INT_FROM(size_data, 0)[0] - 4
-        elements = size_data + file_obj.read(obj_size)
+        elements = size_data + file_obj.read(max(0, obj_size))
         yield _bson_to_dict(elements, codec_options)
 
 
