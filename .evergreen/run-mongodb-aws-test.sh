bb272b1d544a6f69c905949b0708022421bbd89d
Shane Harvey <shnhrv@gmail.com>
2020-03-03 15:22:17 -0800

    PYTHON-2139 Test MONGODB-AWS auth on macOS and Windows
    Replace jq with python as macOS does not have jq.
    Use sys.stdout.write instead of print to avoid trailing newlines.

--- .evergreen/run-mongodb-aws-test.sh
+++ .evergreen/run-mongodb-aws-test.sh
@@ -7,6 +7,12 @@ set -o errexit  # Exit the script with error if any of the commands fail
 #            Main Program                  #
 ############################################
 
+# Supported/used environment variables:
+#  MONGODB_URI    Set the URI, including an optional username/password to use
+#                 to connect to the server via MONGODB-AWS authentication
+#                 mechanism.
+#  PYTHON_BINARY  The Python version to use.
+
 echo "Running MONGODB-AWS authentication tests"
 # ensure no secrets are printed in log files
 set +x
@@ -29,11 +35,19 @@ set -x
 VIRTUALENV=$(command -v virtualenv)
 
 authtest () {
+    if [ "Windows_NT" = "$OS" ]; then
+      PYTHON=$(cygpath -m $PYTHON)
+    fi
+
     echo "Running MONGODB-AWS authentication tests with $PYTHON"
     $PYTHON --version
 
     $VIRTUALENV -p $PYTHON --system-site-packages --never-download venvaws
-    . venvaws/bin/activate
+    if [ "Windows_NT" = "$OS" ]; then
+      . venvaws/Scripts/activate
+    else
+      . venvaws/bin/activate
+    fi
     pip install requests botocore
 
     python test/auth_aws/test_auth_aws.py
@@ -41,5 +55,10 @@ authtest () {
     rm -rf venvaws
 }
 
-PYTHON=$(command -v python) authtest
-PYTHON=$(command -v python3) authtest
+PYTHON=${PYTHON_BINARY:-}
+if [ -z "$PYTHON" ]; then
+    echo "Cannot test without specifying PYTHON_BINARY"
+    exit 1
+fi
+
+authtest
