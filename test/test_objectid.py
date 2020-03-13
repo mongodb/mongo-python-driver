93cf0dd17647f43e3e8c4e9ab5b034fe9ebbfe45
Shane Harvey <shnhrv@gmail.com>
2020-03-13 12:47:30 -0700

    PYTHON-2102 Migrate testing to Windows 10
    Add 32-bit Python testing on Windows.

--- test/test_objectid.py
+++ test/test_objectid.py
@@ -189,15 +189,15 @@ class TestObjectId(unittest.TestCase):
 
         for tstamp, exp_datetime_args in TEST_DATA.items():
             oid = generate_objectid_with_timestamp(tstamp)
-            if (not sys.platform.startswith("java") and
-                    tstamp > 0x7FFFFFFF and sys.maxsize < 2**32):
-                # 32-bit platforms will overflow in datetime.fromtimestamp.
-                with self.assertRaises((OverflowError, ValueError)):
+            # 32-bit platforms may overflow in datetime.fromtimestamp.
+            if tstamp > 0x7FFFFFFF and sys.maxsize < 2**32:
+                try:
                     oid.generation_time
-            else:
-                self.assertEqual(
-                    oid.generation_time,
-                    datetime.datetime(*exp_datetime_args, tzinfo=utc))
+                except (OverflowError, ValueError):
+                    continue
+            self.assertEqual(
+                oid.generation_time,
+                datetime.datetime(*exp_datetime_args, tzinfo=utc))
 
     def test_random_regenerated_on_pid_change(self):
         # Test that change of pid triggers new random number generation.
