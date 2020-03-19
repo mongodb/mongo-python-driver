19dad6844c0082d57944638b45c0ad09b5b63902
Prashant Mital <prashantmital@gmail.com>
2020-03-19 03:02:43 -0700

    account for relpaths

--- .evergreen/atlas-testing/install-driver.sh
+++ .evergreen/atlas-testing/install-driver.sh
@@ -2,5 +2,5 @@
 set -o xtrace
 
 "$PYTHON_BINARY" -m virtualenv "$PYMONGO_VIRTUALENV_NAME"
-"$PYMONGO_VIRTUALENV_NAME/bin/pip" install -e .[srv]
+"$PYMONGO_BIN_DIR/pip" install -e .[srv]
 
--- .evergreen/atlas-testing/workload-executor.sh
+++ .evergreen/atlas-testing/workload-executor.sh
@@ -3,4 +3,4 @@ set -o errexit  # Exit the script with error if any of the commands fail
 
 trap "exit 0" INT
 
-"$PYMONGO_VIRTUALENV_NAME/bin/python" "driver-src/.evergreen/atlas-testing/workload-executor.py" "$1" "$2"
+"$PYMONGO_BIN_DIR/python" "driver-src/.evergreen/atlas-testing/workload-executor.py" "$1" "$2"
