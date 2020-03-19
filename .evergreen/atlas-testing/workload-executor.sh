d0897240f8bf84b1b3a38ca2d375efc41775dcec
Prashant Mital <prashantmital@gmail.com>
2020-03-19 02:05:22 -0700

    fix script paths

--- .evergreen/atlas-testing/workload-executor.sh
+++ .evergreen/atlas-testing/workload-executor.sh
@@ -3,4 +3,4 @@ set -o errexit  # Exit the script with error if any of the commands fail
 
 trap "exit 0" INT
 
-"$PYMONGO_PYTHON_RUNTIME" "driver-src/.evergreen/atlas-testing/workload-executor.py" "$1" "$2"
+"$PYMONGO_VIRTUALENV_NAME/bin/python" "driver-src/.evergreen/atlas-testing/workload-executor.py" "$1" "$2"
