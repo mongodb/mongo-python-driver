5ec01ca6100140c779281f9fb7c8a4a1299ffc20
Shane Harvey <shnhrv@gmail.com>
2020-04-10 09:39:05 -0700

    BUMP 3.11.0b0

--- pymongo/__init__.py
+++ pymongo/__init__.py
@@ -64,7 +64,7 @@ SLOW_ONLY = 1
 ALL = 2
 """Profile all operations."""
 
-version_tuple = (3, 11, 0, '.dev1')
+version_tuple = (3, 11, 0, 'b0')
 
 def get_version_string():
     if isinstance(version_tuple[-1], str):
--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ except ImportError:
     except ImportError:
         _HAVE_SPHINX = False
 
-version = "3.11.0.dev1"
+version = "3.11.0b0"
 
 f = open("README.rst")
 try:
