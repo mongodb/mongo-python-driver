e1915fc89ba01f4f22f2a3ad1074978ff0fdf5b4
Shane Harvey <shane.harvey@mongodb.com>
2020-09-15 09:06:19 -0700

    PYTHON-2372 Build macOS releases in Evergreen (#486)

--- .evergreen/build-mac.sh
+++ .evergreen/build-mac.sh
@@ -2,11 +2,14 @@
 
 for VERSION in 2.7 3.4 3.5 3.6 3.7 3.8; do
     if [[ $VERSION == "2.7" ]]; then
+        PYTHON=/System/Library/Frameworks/Python.framework/Versions/2.7/bin/python
         rm -rf build
-        python$VERSION setup.py bdist_egg
+        $PYTHON setup.py bdist_egg
+    else
+        PYTHON=/Library/Frameworks/Python.framework/Versions/$VERSION/bin/python3
     fi
     rm -rf build
-    python$VERSION setup.py bdist_wheel
+    $PYTHON setup.py bdist_wheel
 done
 
 ls dist
