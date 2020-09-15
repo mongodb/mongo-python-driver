eb563afbfce6c97333db1f768d83563d544c97f1
Shane Harvey <shnhrv@gmail.com>
2020-09-15 12:18:03 -0700

    PYTHON-2345 Refactor windows release script

--- .evergreen/build-windows.sh
+++ .evergreen/build-windows.sh
@@ -1,18 +1,16 @@
 #!/bin/bash -ex
 
 for VERSION in 27 34 35 36 37 38; do
-    PYTHON=C:/Python/Python${VERSION}/python.exe
-    PYTHON32=C:/Python/32/Python${VERSION}/python.exe
-    if [[ $VERSION == "2.7" ]]; then
+    _pythons=(C:/Python/Python${VERSION}/python.exe \
+              C:/Python/32/Python${VERSION}/python.exe)
+    for PYTHON in "${_pythons[@]}"; do
+        if [[ $VERSION == "2.7" ]]; then
+            rm -rf build
+            $PYTHON setup.py bdist_egg
+        fi
         rm -rf build
-        $PYTHON setup.py bdist_egg
-        rm -rf build
-        $PYTHON32 setup.py bdist_egg
-    fi
-    rm -rf build
-    $PYTHON setup.py bdist_wheel
-    rm -rf build
-    $PYTHON32 setup.py bdist_wheel
+        $PYTHON setup.py bdist_wheel
+    done
 done
 
 ls dist
