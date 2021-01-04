3e986fd12008e47cc23370d72ae5a62265c5f20e
Shane Harvey <shnhrv@gmail.com>
2021-01-04 10:10:51 -0800

    PYTHON-1878 Add mongodb+srv URIs to Atlas Connectivity tests

--- .evergreen/run-atlas-tests.sh
+++ .evergreen/run-atlas-tests.sh
@@ -14,28 +14,29 @@ if [ -z "$PYTHON_BINARY" ]; then
     fi
 fi
 
-IMPL=$(${PYTHON_BINARY} -c "import platform, sys; sys.stdout.write(platform.python_implementation())")
+# For createvirtualenv.
+. .evergreen/utils.sh
+
+createvirtualenv $PYTHON_BINARY atlastest
+trap "deactivate; rm -rf atlastest" EXIT HUP
+
+IMPL=$(python -c "import platform, sys; sys.stdout.write(platform.python_implementation())")
 if [ $IMPL = "Jython" -o $IMPL = "PyPy" ]; then
     echo "Using Jython or PyPy"
-    $PYTHON_BINARY -m virtualenv --never-download --no-wheel atlastest
-    . atlastest/bin/activate
-    trap "deactivate; rm -rf atlastest" EXIT HUP
-    pip install certifi
-    PYTHON=python
+    python -m pip install certifi
 else
-    IS_PRE_279=$(${PYTHON_BINARY} -c "import sys; sys.stdout.write('1' if sys.version_info < (2, 7, 9) else '0')")
+    IS_PRE_279=$(python -c "import sys; sys.stdout.write('1' if sys.version_info < (2, 7, 9) else '0')")
     if [ $IS_PRE_279 = "1" ]; then
         echo "Using a Pre-2.7.9 CPython"
-        $PYTHON_BINARY -m virtualenv --never-download --no-wheel atlastest
-        . atlastest/bin/activate
-        trap "deactivate; rm -rf atlastest" EXIT HUP
-        pip install pyopenssl>=17.2.0 service_identity>18.1.0
-        PYTHON=python
+        python -m pip install pyopenssl>=17.2.0 service_identity>18.1.0
     else
         echo "Using CPython 2.7.9+"
-        PYTHON=$PYTHON_BINARY
     fi
 fi
 
-echo "Running tests"
-$PYTHON test/atlas/test_connection.py
+echo "Running tests without dnspython"
+python test/atlas/test_connection.py
+
+python -m pip install dnspython
+echo "Running tests with dnspython"
+PYMONGO_MUST_HAVE_DNS="1" python test/atlas/test_connection.py
--- test/atlas/test_connection.py
+++ test/atlas/test_connection.py
@@ -23,15 +23,30 @@ sys.path[0:0] = [""]
 import pymongo
 from pymongo.ssl_support import HAS_SNI
 
+try:
+    import dns
+    HAS_DNS = True
+except ImportError:
+    HAS_DNS = False
 
-_REPL = os.environ.get("ATLAS_REPL")
-_SHRD = os.environ.get("ATLAS_SHRD")
-_FREE = os.environ.get("ATLAS_FREE")
-_TLS11 = os.environ.get("ATLAS_TLS11")
-_TLS12 = os.environ.get("ATLAS_TLS12")
+
+ATLAS_REPL = os.environ.get("ATLAS_REPL")
+ATLAS_SHRD = os.environ.get("ATLAS_SHRD")
+ATLAS_FREE = os.environ.get("ATLAS_FREE")
+ATLAS_TLS11 = os.environ.get("ATLAS_TLS11")
+ATLAS_TLS12 = os.environ.get("ATLAS_TLS12")
+SRV_ATLAS_REPL = os.environ.get("SRV_ATLAS_REPL")
+SRV_ATLAS_SHRD = os.environ.get("SRV_ATLAS_SHRD")
+SRV_ATLAS_FREE = os.environ.get("SRV_ATLAS_FREE")
+SRV_ATLAS_TLS11 = os.environ.get("SRV_ATLAS_TLS11")
+SRV_ATLAS_TLS12 = os.environ.get("SRV_ATLAS_TLS12")
+
+PYMONGO_MUST_HAVE_DNS = os.environ.get("PYMONGO_MUST_HAVE_DNS")
 
 
 def _connect(uri):
+    if not uri:
+        raise Exception("Must set env variable to test.")
     client = pymongo.MongoClient(uri)
     # No TLS error
     client.admin.command('ismaster')
@@ -40,29 +55,47 @@ def _connect(uri):
 
 
 class TestAtlasConnect(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        if not all([_REPL, _SHRD, _FREE]):
-            raise Exception(
-                "Must set ATLAS_REPL/SHRD/FREE env variables to test.")
+    @unittest.skipUnless(HAS_SNI, 'Free tier requires SNI support')
+    def test_free_tier(self):
+        _connect(ATLAS_FREE)
 
     def test_replica_set(self):
-        _connect(_REPL)
+        _connect(ATLAS_REPL)
 
     def test_sharded_cluster(self):
-        _connect(_SHRD)
-
-    def test_free_tier(self):
-        if not HAS_SNI:
-            raise unittest.SkipTest("Free tier requires SNI support.")
-        _connect(_FREE)
+        _connect(ATLAS_SHRD)
 
     def test_tls_11(self):
-        _connect(_TLS11)
+        _connect(ATLAS_TLS11)
 
     def test_tls_12(self):
-        _connect(_TLS12)
+        _connect(ATLAS_TLS12)
+
+    @unittest.skipUnless(HAS_DNS, 'SRV requires dnspython')
+    @unittest.skipUnless(HAS_SNI, 'Free tier requires SNI support')
+    def test_srv_free_tier(self):
+        _connect(SRV_ATLAS_FREE)
+
+    @unittest.skipUnless(HAS_DNS, 'SRV requires dnspython')
+    def test_srv_replica_set(self):
+        _connect(SRV_ATLAS_REPL)
+
+    @unittest.skipUnless(HAS_DNS, 'SRV requires dnspython')
+    def test_srv_sharded_cluster(self):
+        _connect(SRV_ATLAS_SHRD)
+
+    @unittest.skipUnless(HAS_DNS, 'SRV requires dnspython')
+    def test_srv_tls_11(self):
+        _connect(SRV_ATLAS_TLS11)
+
+    @unittest.skipUnless(HAS_DNS, 'SRV requires dnspython')
+    def test_srv_tls_12(self):
+        _connect(SRV_ATLAS_TLS12)
+
+    @unittest.skipUnless(PYMONGO_MUST_HAVE_DNS, 'dnspython is optional')
+    def test_dnspython_was_installed(self):
+        self.assertTrue(HAS_DNS, 'PYMONGO_MUST_HAVE_DNS is set but dns could '
+                                 'not be imported')
 
 
 if __name__ == '__main__':
