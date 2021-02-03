36ccdbf45395733290e83ebf3a252ed038637996
Prashant Mital <prashantmital@gmail.com>
2021-02-02 16:48:46 -0800

    BUMP 3.11.3

--- pymongo/__init__.py
+++ pymongo/__init__.py
@@ -74,7 +74,7 @@ SLOW_ONLY = 1
 ALL = 2
 """Profile all operations."""
 
-version_tuple = (3, 11, 3, 'dev0')
+version_tuple = (3, 11, 3)
 
 def get_version_string():
     if isinstance(version_tuple[-1], str):
