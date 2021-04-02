9e01a6bf1d081c62ac2173f666234de159397e0c
Shane Harvey <shane.harvey@mongodb.com>
2021-04-02 10:17:04 -0700

    PYTHON-2631 Add missing error message to InvalidBSON error (#589)
    (cherry picked from commit cc029a1e6208863eaab453777363d3935b927f32)

--- bson/_cbsonmodule.c
+++ bson/_cbsonmodule.c
@@ -2621,7 +2621,7 @@ static int _element_to_dict(PyObject* self, const char* string,
     if (name_length > BSON_MAX_SIZE || position + name_length >= max) {
         PyObject* InvalidBSON = _error("InvalidBSON");
         if (InvalidBSON) {
-            PyErr_SetNone(InvalidBSON);
+            PyErr_SetString(InvalidBSON, "field name too large");
             Py_DECREF(InvalidBSON);
         }
         return -1;
