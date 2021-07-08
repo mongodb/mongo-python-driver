00ed2321bacd16b6ecdabd32549f5e4db9b19ea0
Prashant Mital <5883388+prashantmital@users.noreply.github.com>
2021-07-07 23:55:52 -0700

    PYTHON-2475 Implement Atlas Data Lake prose specification tests (#665)
    * PYTHON-2475 Add prose specification tests for Atlas Data Lake
    
    * add prose tests
    
    * Update evergreen config to bootstrap ADL
    
    * add sleep before connecting to ADL
    
    * print buildinfo
    
    * print buildInfo in conditional block
    
    * refactor skiplogic
    
    * remove sleep
    
    * fix debugging code
    
    * Ensure ADL tests run
    
    * ensure suite fails if not connected
    
    * fix test failure
    
    * improve data_lake variable extraction
    
    * review changes

--- test/test_data_lake.py
+++ test/test_data_lake.py
@@ -19,14 +19,77 @@ import sys
 
 sys.path[0:0] = [""]
 
-from test import client_context, unittest
+from pymongo.auth import MECHANISMS
+from test import client_context, unittest, IntegrationTest
 from test.crud_v2_format import TestCrudV2
-from test.utils import TestCreator
+from test.utils import (
+    rs_client_noauth, rs_or_single_client, OvertCommandListener, TestCreator)
 
 
 # Location of JSON test specifications.
 _TEST_PATH = os.path.join(
     os.path.dirname(os.path.realpath(__file__)), "data_lake")
+
+
+class TestDataLakeMustConnect(IntegrationTest):
+    def test_connected_to_data_lake(self):
+        data_lake = os.environ.get('DATA_LAKE')
+        if not data_lake:
+            self.skipTest('DATA_LAKE is not set')
+
+        self.assertTrue(client_context.is_data_lake,
+                        'client context.is_data_lake must be True when '
+                        'DATA_LAKE is set')
+
+
+class TestDataLakeProse(IntegrationTest):
+    # Default test database and collection names.
+    TEST_DB = 'test'
+    TEST_COLLECTION = 'driverdata'
+
+    @classmethod
+    @client_context.require_data_lake
+    def setUpClass(cls):
+        super(TestDataLakeProse, cls).setUpClass()
+
+    # Test killCursors
+    def test_1(self):
+        listener = OvertCommandListener()
+        client = rs_or_single_client(event_listeners=[listener])
+        cursor = client[self.TEST_DB][self.TEST_COLLECTION].find(
+            {}, batch_size=2)
+        next(cursor)
+
+        # find command assertions
+        find_cmd = listener.results["succeeded"][-1]
+        self.assertEqual(find_cmd.command_name, "find")
+        cursor_id = find_cmd.reply["cursor"]["id"]
+        cursor_ns = find_cmd.reply["cursor"]["ns"]
+
+        # killCursors command assertions
+        cursor.close()
+        started = listener.results["started"][-1]
+        self.assertEqual(started.command_name, 'killCursors')
+        succeeded = listener.results["succeeded"][-1]
+        self.assertEqual(succeeded.command_name, 'killCursors')
+
+        self.assertIn(cursor_id, started.command["cursors"])
+        target_ns = ".".join([started.command['$db'],
+                              started.command['killCursors']])
+        self.assertEqual(cursor_ns, target_ns)
+
+        self.assertIn(cursor_id, succeeded.reply["cursorsKilled"])
+
+    # Test no auth
+    def test_2(self):
+        client = rs_client_noauth()
+        client.admin.command('ping')
+
+    # Test with auth
+    def test_3(self):
+        for mechanism in ['SCRAM-SHA-1', 'SCRAM-SHA-256']:
+            client = rs_or_single_client(authMechanism=mechanism)
+            client[self.TEST_DB][self.TEST_COLLECTION].find_one()
 
 
 class DataLakeTestSpec(TestCrudV2):
@@ -35,8 +98,7 @@ class DataLakeTestSpec(TestCrudV2):
     TEST_COLLECTION = 'driverdata'
 
     @classmethod
-    @unittest.skipUnless(client_context.is_data_lake,
-                         'Not connected to Atlas Data Lake')
+    @client_context.require_data_lake
     def setUpClass(cls):
         super(DataLakeTestSpec, cls).setUpClass()
 
