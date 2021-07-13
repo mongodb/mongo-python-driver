72787902307707a1b1ff9ef810ea1c81a35a4ec4
Shane Harvey <shnhrv@gmail.com>
2021-07-13 10:51:09 -0700

    BUMP 3.12.1.dev0

--- pymongo/__init__.py
+++ pymongo/__init__.py
@@ -99,7 +99,7 @@ See https://docs.mongodb.com/manual/tutorial/manage-the-database-profiler
    Deprecated
 """
 
-version_tuple = (3, 12, 0)
+version_tuple = (3, 12, 1, '.dev0')
 
 def get_version_string():
     if isinstance(version_tuple[-1], str):
