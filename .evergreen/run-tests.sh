d58b3851559b7e3eb4b48b70bf099f7c36ee228c
Shane Harvey <shnhrv@gmail.com>
2021-08-04 10:35:37 -0700

    PYTHON-2855 Update mock server filename for KMS testing
    (cherry picked from commit 568205135e764d176430623b65eac4ec0880b113)

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -183,8 +183,8 @@ if [ -n "$TEST_ENCRYPTION" ]; then
         python -m pip install boto3
     fi
     pushd ${DRIVERS_TOOLS}/.evergreen/csfle
-    python -u lib/kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/expired.pem --port 8000 &
-    python -u lib/kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/wrong-host.pem --port 8001 &
+    python -u kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/expired.pem --port 8000 &
+    python -u kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/wrong-host.pem --port 8001 &
     trap 'kill $(jobs -p)' EXIT HUP
     popd
     # Restore the test virtualenv.
