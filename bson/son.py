3216be79a096d3c9e7007c8a8771f25765e9aabb
julius <juliusgeo@gmail.com>
2021-09-16 11:58:57 -0700

    revert previous commit

--- bson/son.py
+++ bson/son.py
@@ -92,7 +92,7 @@ class SON(dict):
         return [v for _, v in self.items()]
 
     def items(self):
-        return ((key, self[key]) for key in self)
+        return [(key, self[key]) for key in self)]
 
     def clear(self):
         self.__keys = []
