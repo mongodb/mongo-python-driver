09dca2d8759db15061c0924c1ab41088db90c012
Shane Harvey <shane.harvey@mongodb.com>
2021-10-13 16:06:26 -0700

    PYTHON-2944 Use get-pip for EOL Python versions (#759)

--- .evergreen/utils.sh
+++ .evergreen/utils.sh
@@ -26,12 +26,9 @@ createvirtualenv () {
     # Upgrade to the latest versions of pip setuptools wheel so that
     # pip can always download the latest cryptography+cffi wheels.
     PYTHON_VERSION=$(python -c 'import sys;print("%s.%s" % sys.version_info[:2])')
-    if [[ $PYTHON_VERSION == "3.4" ]]; then
-        # pip 19.2 dropped support for Python 3.4.
-        python -m pip install --upgrade 'pip<19.2'
-    elif [[ $PYTHON_VERSION == "2.7" || $PYTHON_VERSION == "3.5" ]]; then
-        # pip 21 will drop support for Python 2.7 and 3.5.
-        python -m pip install --upgrade 'pip<21'
+    if [[ $PYTHON_VERSION == "2.7" || $PYTHON_VERSION == "3.4" || $PYTHON_VERSION == "3.5" ]]; then
+        # Use get-pip for EOL Python versions.
+        curl --retry 3 -L https://bootstrap.pypa.io/pip/$PYTHON_VERSION/get-pip.py | python
     else
         python -m pip install --upgrade pip
     fi
