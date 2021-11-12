8a2e63b4664e3d84aa98488c0055d5884103dbe8
julius <juliusgeo@gmail.com>
2021-11-12 05:12:36 -0800

    fix tests

--- test/test_create_entities.py
+++ test/test_create_entities.py
@@ -51,7 +51,7 @@ class TestCreateEntities(unittest.TestCase):
         self.assertIn("events1", final_entity_map)
         self.assertGreater(len(final_entity_map["events1"]), 0)
         for event in final_entity_map["events1"]:
-            self.assertIn("PoolCreatedEvent", event)
+            self.assertIn("PoolCreatedEvent", event["name"])
 
     def test_store_all_others_as_entities(self):
         self.scenario_runner = UnifiedSpecTestMixinV1()
