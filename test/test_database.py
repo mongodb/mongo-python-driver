c7e2c136ea673afbeaa0418283c4245e6a710cf7
Steven Silvester <steven.silvester@ieee.org>
2022-01-11 18:18:31 -0600

    clean up test_database

--- test/test_database.py
+++ test/test_database.py
@@ -17,6 +17,7 @@
 import datetime
 import re
 import sys
+from typing import Any, Dict, List, Mapping, cast
 
 sys.path[0:0] = [""]
 
@@ -104,6 +105,7 @@ class TestDatabaseNoConnect(unittest.TestCase):
 
 class TestDatabase(IntegrationTest):
     client: MongoClient
+    db: Database
 
     def test_equality(self):
         self.assertNotEqual(Database(self.client, "test"),
@@ -145,7 +147,7 @@ class TestDatabase(IntegrationTest):
         test = db.create_collection("test")
         self.assertTrue("test" in db.list_collection_names())
         test.insert_one({"hello": "world"})
-        self.assertEqual(db.test.find_one()["hello"], "world")
+        self.assertEqual(cast(Any, db.test.find_one())["hello"], "world")
 
         db.drop_collection("test.foo")
         db.create_collection("test.foo")
@@ -227,7 +229,7 @@ class TestDatabase(IntegrationTest):
             self.assertTrue("$" not in coll)
 
         # Duplicate check.
-        coll_cnt = {}
+        coll_cnt: Dict[str, Any] = {}
         for coll in colls:
             try:
                 # Found duplicate.
@@ -244,29 +246,29 @@ class TestDatabase(IntegrationTest):
         else:
             self.assertTrue(False)
 
-        colls = db.list_collections(filter={"name": {"$regex": "^test$"}})
+        colls2 = db.list_collections(filter={"name": {"$regex": "^test$"}})
         self.assertEqual(1, len(list(colls)))
 
-        colls = db.list_collections(filter={"name": {"$regex": "^test.mike$"}})
+        colls3 = db.list_collections(filter={"name": {"$regex": "^test.mike$"}})
         self.assertEqual(1, len(list(colls)))
 
         db.drop_collection("test")
 
         db.create_collection("test", capped=True, size=4096)
         results = db.list_collections(filter={'options.capped': True})
-        colls = [result["name"] for result in results]
+        colls4 = [result["name"] for result in results]
 
         # Checking only capped collections are present
-        self.assertTrue("test" in colls)
-        self.assertFalse("test.mike" in colls)
+        self.assertTrue("test" in colls4)
+        self.assertFalse("test.mike" in colls4)
 
         # No collection containing a '$'.
-        for coll in colls:
+        for coll in colls4:
             self.assertTrue("$" not in coll)
 
         # Duplicate check.
         coll_cnt = {}
-        for coll in colls:
+        for coll in colls4:
             try:
                 # Found duplicate.
                 coll_cnt[coll] += 1
@@ -276,8 +278,8 @@ class TestDatabase(IntegrationTest):
         coll_cnt = {}
 
         # Checking if is there any collection which don't exists.
-        if (len(set(colls) - set(["test"])) == 0 or
-            len(set(colls) - set(["test","system.indexes"])) == 0):
+        if (len(set(colls4) - set(["test"])) == 0 or
+            len(set(colls4) - set(["test","system.indexes"])) == 0):
             self.assertTrue(True)
         else:
             self.assertTrue(False)
@@ -428,7 +430,7 @@ class TestDatabase(IntegrationTest):
         self.assertRaises(TypeError, db.dereference, None)
 
         self.assertEqual(None, db.dereference(DBRef("test", ObjectId())))
-        obj = {"x": True}
+        obj: Dict[str, Any] = {"x": True}
         key = db.test.insert_one(obj).inserted_id
         self.assertEqual(obj, db.dereference(DBRef("test", key)))
         self.assertEqual(obj,
@@ -468,6 +470,7 @@ class TestDatabase(IntegrationTest):
         self.assertEqual(None, db.test.find_one({"hello": "test"}))
 
         b = db.test.find_one()
+        assert b is not None
         b["hello"] = "mike"
         db.test.replace_one({"_id": b["_id"]}, b)
 
@@ -484,12 +487,12 @@ class TestDatabase(IntegrationTest):
         db = self.client.pymongo_test
         db.test.drop()
         db.test.insert_one({"x": 9223372036854775807})
-        retrieved = db.test.find_one()['x']
+        retrieved = cast(Any, db.test.find_one())['x']
         self.assertEqual(Int64(9223372036854775807), retrieved)
         self.assertIsInstance(retrieved, Int64)
         db.test.delete_many({})
         db.test.insert_one({"x": Int64(1)})
-        retrieved = db.test.find_one()['x']
+        retrieved = cast(Any, db.test.find_one())['x']
         self.assertEqual(Int64(1), retrieved)
         self.assertIsInstance(retrieved, Int64)
 
@@ -511,8 +514,8 @@ class TestDatabase(IntegrationTest):
             length += 1
         self.assertEqual(length, 2)
 
-        db.test.delete_one(db.test.find_one())
-        db.test.delete_one(db.test.find_one())
+        db.test.delete_one(cast(Any, db.test.find_one()))
+        db.test.delete_one(cast(Any, db.test.find_one()))
         self.assertEqual(db.test.find_one(), None)
 
         db.test.insert_one({"x": 1})
@@ -627,7 +630,7 @@ class TestDatabase(IntegrationTest):
                    'read_preference': ReadPreference.PRIMARY,
                    'write_concern': WriteConcern(w=1),
                    'read_concern': ReadConcern(level="local")}
-        db2 = db1.with_options(**newopts)
+        db2 = db1.with_options(**newopts)  # type: ignore
         for opt in newopts:
             self.assertEqual(
                 getattr(db2, opt), newopts.get(opt, getattr(db1, opt)))
@@ -637,7 +640,7 @@ class TestDatabaseAggregation(IntegrationTest):
     client: MongoClient
 
     def setUp(self):
-        self.pipeline = [{"$listLocalSessions": {}},
+        self.pipeline: List[Mapping[str, Any]] = [{"$listLocalSessions": {}},
                          {"$limit": 1},
                          {"$addFields": {"dummy": "dummy field"}},
                          {"$project": {"_id": 0, "dummy": 1}}]
@@ -652,6 +655,7 @@ class TestDatabaseAggregation(IntegrationTest):
     @client_context.require_no_mongos
     def test_database_aggregation_fake_cursor(self):
         coll_name = "test_output"
+        write_stage: Dict[str, Any]
         if client_context.version < (4, 3):
             db_name = "admin"
             write_stage = {"$out": coll_name}
