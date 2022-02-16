6a518b9228a04ab0ac1b2a67caef83efc1d11ce2
julius <juliusgeo@gmail.com>
2022-02-16 14:01:02 -0800

    cleanup merge

--- test/test_json_util.py
+++ test/test_json_util.py
@@ -274,15 +274,6 @@ class TestJsonUtil(unittest.TestCase):
         doc = {"$regex": ""}
         self.assertIsInstance(json_util.loads(json.dumps(doc)), Regex)
 
-    def test_regex_validation(self):
-        non_str_types = [10, {}, []]
-        docs = [{"$regex": i} for i in non_str_types]
-        for doc in docs:
-            self.assertEqual(doc, json_util.loads(json.dumps(doc)))
-
-        doc = {"$regex": ""}
-        self.assertIsInstance(json_util.loads(json.dumps(doc)), Regex)
-
     def test_minkey(self):
         self.round_trip({"m": MinKey()})
 
