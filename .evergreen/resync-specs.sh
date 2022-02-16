1a79c0d6bee3af85cb8cafd9ea391a1c927690c0
julius <juliusgeo@gmail.com>
2022-02-16 13:07:49 -0800

    some tweaks to resync_specs.sh that should hopefully resolve my issue

--- .evergreen/resync-specs.sh
+++ .evergreen/resync-specs.sh
@@ -56,10 +56,13 @@ cpjson () {
     cd "$SPECS"/source/$1
     find . -name '*.json' | grep -Ev "${BLOCKLIST}" | cpio -pdm \
     $PYMONGO/test/$2
-    printf "\nIgnored files for ${PWD}"
-    printf "\n%s\n" "$(diff <(find . -name '*.json' | sort) \
+    printf "\nIgnored files for ${PWD}\n"
+    IGNORED_FILES="$(printf "\n%s\n" "$(diff <(find . -name '*.json' | sort) \
     <(find . -name '*.json' | grep -Ev "${BLOCKLIST}" | sort))" | \
-    sed -e '/^[0-9]/d' | sed -e 's|< ./||g'
+    sed -e '/^[0-9]/d' | sed -e 's|< ./||g' )"
+    printf "%s\n" $IGNORED_FILES
+    printf "%s\n" $IGNORED_FILES | xargs git checkout upstream/master
+
 }
 
 for spec in "$@"
