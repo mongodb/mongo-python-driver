b9f8972eeb5a24b4b131031b7111ebbf878cd72c
julius <juliusgeo@gmail.com>
2022-02-16 13:44:08 -0800

    fix config.yml and fix stray occurences of popping serviceid

--- pymongo/pool.py
+++ pymongo/pool.py
@@ -582,8 +582,6 @@ class SocketInfo(object):
             cmd['speculativeAuthenticate'] = auth_ctx.speculate_command()
 
         doc = self.command("admin", cmd, publish_events=False, exhaust_allowed=awaitable)
-        if not self.opts.load_balanced:
-            doc.pop('serviceId', None)
         hello = IsMaster(doc, awaitable=awaitable)
         self.is_writable = hello.is_writable
         self.max_wire_version = hello.max_wire_version
@@ -621,9 +619,6 @@ class SocketInfo(object):
         unpacked_docs = reply.unpack_response()
         response_doc = unpacked_docs[0]
         helpers._check_command_response(response_doc, self.max_wire_version)
-        # Remove after PYTHON-2712.
-        if not self.opts.load_balanced:
-            response_doc.pop('serviceId', None)
         return response_doc
 
     def command(self, dbname, spec, secondary_ok=False,
