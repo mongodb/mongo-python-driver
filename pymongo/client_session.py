c5a8a17c41342472b1f302d48ef6946accd8e30f
julius <juliusgeo@gmail.com>
2022-02-18 15:45:32 -0800

    make argument optional

--- pymongo/client_session.py
+++ pymongo/client_session.py
@@ -946,11 +946,12 @@ class ClientSession(Generic[_DocumentType]):
             return self._transaction.opts.read_preference
         return None
 
-    def _start_serv_sesh(self, sock_info):
+    def _start_serv_sesh(self, sock_info=None):
+        supports_sessions = False if sock_info is None else sock_info.supports_sessions
         if isinstance(self._server_session, _EmptyServerSession):
             self._server_session, old = (
                 self._client._topology.get_server_session(
-                    supports_sessions=sock_info.supports_sessions
+                    supports_sessions=supports_sessions
                 ),
                 self._server_session,
             )
