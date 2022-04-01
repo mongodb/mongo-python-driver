bd752e58c237a3e93c37331ce3a4e1f000d06c24
Duncan <52967253+dunkOnIT@users.noreply.github.com>
2022-04-01 17:27:17 +0200

    Removed a space I erroneously left on line 163
    (and learned about the power of "Preview changes" in the process!)

--- pymongo/collection.py
+++ pymongo/collection.py
@@ -160,7 +160,7 @@ class Collection(common.BaseObject, Generic[_DocumentType]):
             be passed as options for the create collection command
 
         .. versionchanged:: 4.0
-           Removed the reindex, map_reduce, inline_map_reduce, 
+           Removed the reindex, map_reduce, inline_map_reduce,
            parallel_scan, initialize_unordered_bulk_op,
            initialize_ordered_bulk_op, group, count, insert, save,
            update, remove, find_and_modify, and ensure_index methods. See the
