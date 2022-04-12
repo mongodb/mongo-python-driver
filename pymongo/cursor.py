6ee62c64339b649a7fade0bc9c15710251e847dd
Steven Silvester <steven.silvester@ieee.org>
2022-04-12 08:28:01 -0500

    fix link

--- pymongo/cursor.py
+++ pymongo/cursor.py
@@ -193,7 +193,7 @@ class Cursor(Generic[_DocumentType]):
         Should not be called directly by application developers - see
         :meth:`~pymongo.collection.Collection.find` instead.
 
-        .. seealso:: The MongoDB documentation on `cursors <https://www.mongodb.com/docs/manual/tutorial/query-documents/>`__.
+        .. seealso:: The MongoDB documentation on `cursors <https://www.mongodb.com/docs/manual/tutorial/query-documents/>`_.
         """
         # Initialize all attributes used in __del__ before possibly raising
         # an error to avoid attribute errors during garbage collection.
@@ -1319,7 +1319,7 @@ class RawBatchCursor(Cursor, Generic[_DocumentType]):
         see :meth:`~pymongo.collection.Collection.find_raw_batches`
         instead.
 
-        .. seealso:: The MongoDB documentation on `cursors <https://www.mongodb.com/docs/manual/tutorial/query-documents/>`__.
+        .. seealso:: The MongoDB documentation on `cursors <https://www.mongodb.com/docs/manual/tutorial/query-documents/>`_.
         """
         super(RawBatchCursor, self).__init__(collection, *args, **kwargs)
 
