21f17ad41e6fef653ed31c2d93f34ba875d6aea8
Steven Silvester <steven.silvester@ieee.org>
2022-05-18 15:38:16 -0500

    more fixes for exists operator

--- test/unified_format.py
+++ test/unified_format.py
@@ -471,12 +471,18 @@ class MatchEvaluatorUtil(object):
     def _operation_exists(self, spec, actual, key_to_compare):
         if spec is True:
             if key_to_compare is None:
-                assert actual is not None
+                if isinstance(actual, dict):
+                    assert len(actual)
+                else:
+                    assert actual is not None
             else:
                 self.test.assertIn(key_to_compare, actual)
         elif spec is False:
             if key_to_compare is None:
-                assert actual is None
+                if isinstance(actual, dict):
+                    assert len(actual) == 0
+                else:
+                    assert actual is None
             else:
                 self.test.assertNotIn(key_to_compare, actual)
         else:
