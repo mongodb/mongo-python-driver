e5f7c5aa34eddbf32ec8fba089cb9b5f3ef24751
Steven Silvester <steven.silvester@ieee.org>
2022-06-21 10:48:38 -0500

    handle RewrapManyDataKeyOpts

--- test/utils.py
+++ test/utils.py
@@ -1106,5 +1106,10 @@ def prepare_spec_arguments(spec, arguments, opname, entity_map, with_txn_callbac
                 arguments["cursor_type"] = CursorType.TAILABLE
             else:
                 assert False, f"Unsupported cursorType: {cursor_type}"
+        elif arg_name == "opts":
+            opts = arguments["opts"]
+            from pymongocrypt.mongocrypt import RewrapManyDataKeyOpts
+
+            arguments["opts"] = RewrapManyDataKeyOpts(opts.get("provider"), opts.get("masterKey"))
         else:
             arguments[c2s] = arguments.pop(arg_name)
