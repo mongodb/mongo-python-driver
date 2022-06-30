a6c39acfd501a5cd7931fb9e30d6f4198add300f
Steven Silvester <steven.silvester@ieee.org>
2022-06-30 09:47:07 -0500

    wip try poisoning test

--- test/auth_aws/test_auth_aws.py
+++ test/auth_aws/test_auth_aws.py
@@ -21,14 +21,16 @@ from unittest import mock
 
 sys.path[0:0] = [""]
 
-from test.utils import get_pool
+from test import PyMongoTestCase
+from test.utils import EventListener, get_pool
 
+from bson.son import SON
 from pymongo import MongoClient
 from pymongo.errors import OperationFailure
 from pymongo.uri_parser import parse_uri
 
 
-class TestAuthAWS(unittest.TestCase):
+class TestAuthAWS(PyMongoTestCase):
     uri: str
 
     @classmethod
@@ -57,13 +59,29 @@ class TestAuthAWS(unittest.TestCase):
             client.get_database().test.find_one()
 
     def test_cache_credentials(self):
-        with MongoClient(self.uri) as client:
-            client.get_database().test.find_one()
+        listener = EventListener()
+        client = MongoClient(self.uri, event_listeners=[listener])
+        # client.get_database().test.find_one()
+        # pool = get_pool(client)
+        # pool.reset()
+        fail_point = {
+            "configureFailPoint": "failCommand",
+            "mode": {"times": 10},
+            "data": {
+                "failCommands": ["saslStart"],
+                "closeConnection": True,
+                "errorCode": 10107,
+                "appName": "failingSaslTest",
+            },
+        }
+        with self.fail_point(fail_point):
             pool = get_pool(client)
-        pool.reset()
-        with mock.patch.dict(os.environ, {}, clear=True):
-            with MongoClient(self.uri) as client:
-                client.get_database().test.find_one()
+            pool.reset()
+            client.get_database().test.find_one()
+            pool.reset()
+            client.get_database().test.find_one()
+        client.get_database().test.find_one()
+        client.close()
 
 
 class TestAWSLambdaExamples(unittest.TestCase):
