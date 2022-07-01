3e9a974951d982f3e83e20e7d1eb59c5c14040d9
Steven Silvester <steven.silvester@ieee.org>
2022-07-01 14:07:47 -0500

    use explicit file

--- .evergreen/run-mongodb-aws-test.sh
+++ .evergreen/run-mongodb-aws-test.sh
@@ -61,6 +61,9 @@ authtest () {
       . venvaws/bin/activate
     fi
     python -m pip install '.[aws]'
+    git clone https://github.com/blink1073/pymongo-auth-aws.git
+    git fetch origin DRIVERS-2333-2
+    git checkout DRIVERS-2333-2
     python test/auth_aws/test_auth_aws.py
     deactivate
     rm -rf venvaws
