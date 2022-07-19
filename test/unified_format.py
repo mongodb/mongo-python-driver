30c5686e648c7ff66a7c1a2f8340b4ad4620497b
Shane Harvey <shnhrv@gmail.com>
2022-07-19 13:02:29 -0700

    PYTHON-2459 Keep skipping CSOT gridfs tests for now

--- test/unified_format.py
+++ test/unified_format.py
@@ -910,10 +910,21 @@ class UnifiedSpecTestMixinV1(IntegrationTest):
             if not client_context.test_commands_enabled:
                 if name == "failPoint" or name == "targetedFailPoint":
                     self.skipTest("Test commands must be enabled to use fail points")
-            if "timeoutMode" in op.get("arguments", {}):
-                self.skipTest("PyMongo does not support timeoutMode")
             if name == "modifyCollection":
                 self.skipTest("PyMongo does not support modifyCollection")
+            if "timeoutMode" in op.get("arguments", {}):
+                self.skipTest("PyMongo does not support timeoutMode")
+            if "csot" in class_name:
+                if "bucket" in op["object"]:
+                    self.skipTest("CSOT not implemented for GridFS")
+                if name == "createEntities":
+                    self.maybe_skip_entity(op.get("arguments", {}).get("entities", []))
+
+    def maybe_skip_entity(self, entities):
+        for entity in entities:
+            entity_type = next(iter(entity))
+            if entity_type == "bucket":
+                self.skipTest("GridFS is not currently supported (PYTHON-2459)")
 
     def process_error(self, exception, spec):
         is_error = spec.get("isError")
@@ -1133,6 +1144,11 @@ class UnifiedSpecTestMixinV1(IntegrationTest):
         if "content_type" in kwargs:
             kwargs.setdefault("metadata", {})["contentType"] = kwargs.pop("content_type")
         return target.upload_from_stream_with_id(*args, **kwargs)
+
+    def _bucketOperation_drop(self, target: GridFSBucket, *args: Any, **kwargs: Any) -> None:
+        # PyMongo does not support GridFSBucket.drop(), emulate it.
+        target._files.drop(*args, **kwargs)
+        target._chunks.drop(*args, **kwargs)
 
     def run_entity_operation(self, spec):
         target = self.entity_map[spec["object"]]
