e192c7f85ec627e09cf934e15bc7c009b64c51a6
Shane Harvey <shnhrv@gmail.com>
2022-07-20 08:37:16 -0700

    BUMP 4.2.1.dev0

--- pymongo/__init__.py
+++ pymongo/__init__.py
@@ -84,7 +84,7 @@ TEXT = "text"
 .. _text index: http://mongodb.com/docs/manual/core/index-text/
 """
 
-version_tuple: Tuple[Union[int, str], ...] = (4, 2, 0)
+version_tuple: Tuple[Union[int, str], ...] = (4, 2, 1, ".dev0")
 
 
 def get_version_string() -> str:
