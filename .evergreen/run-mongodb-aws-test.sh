d46b47c00936a06175dddea9ef0d92e6b5a9c0ae
Steven Silvester <steven.silvester@ieee.org>
2022-07-25 16:43:19 -0500

    fix script

--- .evergreen/run-mongodb-aws-test.sh
+++ .evergreen/run-mongodb-aws-test.sh
@@ -60,7 +60,8 @@ authtest () {
     else
       . venvaws/bin/activate
     fi
-    sudo apt-get install git || true
+    apt-get update
+    apt-get install git || true
     python -m pip install '.[aws]'
     python test/auth_aws/test_auth_aws.py -v
     deactivate
