a20ff68d51734d272542d03329b657101a093806
Ben Warner <bcwarner@users.noreply.github.com>
2022-08-15 12:07:49 -0700

    PYTHON-3390 Test for encrypted client post-fork (#1037)

--- test/test_encryption.py
+++ test/test_encryption.py
@@ -329,6 +329,25 @@ class TestClientSimple(EncryptionIntegrationTest):
         with self.assertRaisesRegex(InvalidOperation, "Cannot use MongoClient after close"):
             client.admin.command("ping")
 
+    # Not available for versions of Python without "register_at_fork"
+    @unittest.skipIf(
+        not hasattr(os, "register_at_fork"),
+        "register_at_fork not available in this version of Python",
+    )
+    def test_fork(self):
+        opts = AutoEncryptionOpts(KMS_PROVIDERS, "keyvault.datakeys")
+        client = rs_or_single_client(auto_encryption_opts=opts)
+
+        lock_pid = os.fork()
+        if lock_pid == 0:
+            client.admin.command("ping")
+            client.close()
+            os._exit(0)
+        else:
+            self.assertEqual(0, os.waitpid(lock_pid, 0)[1])
+            client.admin.command("ping")
+            client.close()
+
 
 class TestEncryptedBulkWrite(BulkTestBase, EncryptionIntegrationTest):
     def test_upsert_uuid_standard_encrypt(self):
