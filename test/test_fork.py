088eb9569af2cc4db691dd75fa4acbdf1fadd972
Shane Harvey <shnhrv@gmail.com>
2022-08-30 14:41:48 -0700

    Revert "PYTHON-3406 Reset SSLContext after fork"
    This reverts commit 63e95543bef30b311fd651b49850ba0187ed9f82.

--- test/test_fork.py
+++ test/test_fork.py
@@ -119,7 +119,7 @@ class TestFork(IntegrationTest):
                 c.close()
 
         try:
-            for _ in range(20):
+            for _ in range(100):
                 with self.fork(child_callback) as proc:
                     self.assertTrue(proc.pid)
         finally:
