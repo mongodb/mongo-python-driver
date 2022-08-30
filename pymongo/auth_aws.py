aa394bf407cf1309aecf8cb985f0517fe7adee17
Steven Silvester <steven.silvester@ieee.org>
2022-08-30 17:04:26 -0500

    more assurances

--- pymongo/auth_aws.py
+++ pymongo/auth_aws.py
@@ -68,9 +68,10 @@ def _authenticate_aws(credentials, sock_info):
             )
         )
         import boto3
-        session = boto3.Session()
         if not credentials.username:
+            session = boto3.Session()
             credentials = session.get_credentials()
+            assert credentials.username is not None
             ctx._credentials = AwsCredential(
                 credentials.access_key,
                 credentials.secret_key,
