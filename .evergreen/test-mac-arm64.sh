7e8977b49cf247852538b2b946e233a1386102b0
Steven Silvester <steven.silvester@ieee.org>
2022-09-08 18:44:07 -0500

    fix bash script

--- .evergreen/test-mac-arm64.sh
+++ .evergreen/test-mac-arm64.sh
@@ -8,8 +8,8 @@ regex="cp3([[:digit:]]+)-.*universal2"
 # Test each universal wheel.
 for release in releases/*; do
     # Extract the python version from the file name.
-    if [ $release ~= "universal2" ]; then
-        VERSION="3.{${BASH_REMATCH[1]}"
+    if [[ $release =~ $regex ]]; then
+        VERSION="3.${BASH_REMATCH[1]}"
         PYTHON=/Library/Frameworks/Python.framework/Versions/$VERSION/bin/python3
         createvirtualenv $PYTHON releasevenv
         testinstall $PYTHON $release
