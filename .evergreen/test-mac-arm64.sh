cbc683531a50e24831ccc1bb4c169bb5ec01d1b2
Steven Silvester <steven.silvester@ieee.org>
2022-09-08 19:39:00 -0500

    fix bash script

--- .evergreen/test-mac-arm64.sh
+++ .evergreen/test-mac-arm64.sh
@@ -5,10 +5,13 @@
 
 regex="cp3([[:digit:]]+)-.*universal2"
 
+called=false
+
 # Test each universal wheel.
-for release in releases/*; do
+for release in ../releases/*; do
     # Extract the python version from the file name.
     if [[ $release =~ $regex ]]; then
+        called=true
         VERSION="3.${BASH_REMATCH[1]}"
         PYTHON=/Library/Frameworks/Python.framework/Versions/$VERSION/bin/python3
         createvirtualenv $PYTHON releasevenv
@@ -16,3 +19,8 @@ for release in releases/*; do
         rm -rf releasevenv
     fi
 done
+
+if ! $called; then
+    echo "No files found!"
+    exit 1
+fi
