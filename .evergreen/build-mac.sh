68e51b75f9c00f966fdd7a7b17ccab125bd8c4ca
Steven Silvester <steven.silvester@ieee.org>
2022-09-12 14:55:13 -0500

    fix comparison

--- .evergreen/build-mac.sh
+++ .evergreen/build-mac.sh
@@ -21,7 +21,7 @@ rm -rf releasevenv
 
 # Test that each wheel is installable.
 for release in dist/*; do
-    if [[ "$release" = "*universal2*'" ]]; then
+    if [[ "$release" == "*universal2*" ]]; then
         export ENSURE_UNIVERSAL2="1"
     fi
     testinstall $PYTHON $release
