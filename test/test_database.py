2af12e64639b55c2d8c5d52892d42fab89355220
Julius Park <Juliusgeo@gmail.com>
2022-09-26 15:48:48 -0700

    PYTHON-3444 MyPy Errors With Version 0.981 (#1063)

--- test/test_database.py
+++ test/test_database.py
@@ -604,14 +604,13 @@ class TestDatabase(IntegrationTest):
         try:
             db = self.client.pymongo_test
             db.command("count", "test")
-            # Remove type ignore after: https://github.com/python/mypy/issues/13220
-            self.assertRaises(ExecutionTimeout, db.command, "count", "test", maxTimeMS=1)  # type: ignore[arg-type]
+            self.assertRaises(ExecutionTimeout, db.command, "count", "test", maxTimeMS=1)
             pipeline = [{"$project": {"name": 1, "count": 1}}]
             # Database command helper.
             db.command("aggregate", "test", pipeline=pipeline, cursor={})
             self.assertRaises(
                 ExecutionTimeout,
-                db.command,  # type: ignore[arg-type]
+                db.command,
                 "aggregate",
                 "test",
                 pipeline=pipeline,
