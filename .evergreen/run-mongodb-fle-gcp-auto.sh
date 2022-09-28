e08290d4355e0c3dd77d413ca760fa8c0a6a20e8
Steven Silvester <steven.silvester@ieee.org>
2022-09-28 11:36:11 -0500

    fix script

--- .evergreen/run-mongodb-fle-gcp-auto.sh
+++ .evergreen/run-mongodb-fle-gcp-auto.sh
@@ -27,10 +27,8 @@ authtest () {
     echo "Running GCP Credential Acquisition Test with $PYTHON"
     $PYTHON --version
     $PYTHON -m pip install --upgrade wheel setuptools pip
-    pushd src
     $PYTHON -m pip install '.[encryption]'
     TEST_FLE_GCP_AUTO=1 $PYTHON test/test_encryption.py
-    popd
 }
 
 PYTHON="python3" authtest
