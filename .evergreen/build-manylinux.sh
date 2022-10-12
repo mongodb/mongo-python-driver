3808c6cd5890f8c6c414187ed2ac188e281d06b1
Steven Silvester <steven.silvester@ieee.org>
2022-10-12 14:17:42 -0500

    fix syntax

--- .evergreen/build-manylinux.sh
+++ .evergreen/build-manylinux.sh
@@ -38,7 +38,7 @@ unexpected=$(find dist \! \( -iname dist -or \
                              -iname '*cp38*' -or \
                              -iname '*cp39*' -or \
                              -iname '*cp310*' -or \
-                             -iname '*cp311*' \ ))
+                             -iname '*cp311*' \))
 if [ -n "$unexpected" ]; then
   echo "Unexpected files:" $unexpected
   exit 1
