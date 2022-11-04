1a9e5d761b758b4649f279cd59b9e45b9161e8c6
Julius Park <juliusgeo@gmail.com>
2022-11-04 13:05:29 -0700

    fix autoformatter mangling again

--- test/test_server_selection.py
+++ test/test_server_selection.py
@@ -87,8 +87,9 @@ class TestCustomServerSelectorFunction(IntegrationTest):
         wait_until(all_hosts_started, "receive heartbeat from all hosts")
         expected_port = max(
             [
-                n.address[1] for n in client._topology._description.readable_servers
-            ]  # type:ignore[type-var]
+                n.address[1]
+                for n in client._topology._description.readable_servers  # type:ignore[type-var]
+            ]
         )
 
         # Insert 1 record and access it 10 times.
