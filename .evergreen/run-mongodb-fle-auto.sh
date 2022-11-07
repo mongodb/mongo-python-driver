975a39fad76fdaeab1388ca78b683a62d7bcd99b
Steven Silvester <steven.silvester@ieee.org>
2022-11-07 14:55:37 -0600

    address review

--- .evergreen/run-mongodb-fle-auto.sh
+++ .evergreen/run-mongodb-fle-auto.sh
@@ -10,26 +10,25 @@ set -o errexit  # Exit the script with error if any of the commands fail
 #            Main Program                  #
 ############################################
 
-if [[ -z "$1" ]]; then
-    echo "usage: $0 <MONGODB_URI>"
+if [[ -z "$MONGODB_URI" ]]; then
+    echo "Must define MONGODB_URI"
     exit 1
 fi
-export MONGODB_URI="$1"
 
 if echo "$MONGODB_URI" | grep -q "@"; then
-  echo "MONGODB_URI unexpectedly contains user credentials in FLE GCP test!";
+  echo "MONGODB_URI unexpectedly contains user credentials in FLE test!";
   exit 1
 fi
 
 authtest () {
-    echo "Running GCP Credential Acquisition Test with $PYTHON"
+    echo "Running Credential Acquisition Test with $PYTHON"
     $PYTHON --version
     $PYTHON -m pip install --upgrade wheel setuptools pip
     $PYTHON -m pip install https://github.com/mongodb/libmongocrypt/archive/refs/heads/master.zip#subdirectory=bindings/python
     curl -O https://s3.amazonaws.com/mciuploads/libmongocrypt/all/master/latest/libmongocrypt-all.tar.gz
     mkdir libmongocrypt-all && tar xzf libmongocrypt-all.tar.gz -C libmongocrypt-all
     $PYTHON -m pip install '.'
-    PYMONGOCRYPT_LIB=$(pwd)/libmongocrypt-all/debian10/nocrypto/lib/libmongocrypt.so TEST_FLE_GCP_AUTO=1 $PYTHON test/test_on_demand_csfle.py
+    PYMONGOCRYPT_LIB=$(pwd)/libmongocrypt-all/debian10/nocrypto/lib/libmongocrypt.so $PYTHON test/test_on_demand_csfle.py
 }
 
 PYTHON="python3" authtest
