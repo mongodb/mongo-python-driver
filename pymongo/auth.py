b236f2a55f745c91667cad873b24008566d07855
Steven Silvester <steven.silvester@ieee.org>
2023-02-08 14:33:49 -0600

    try another way to re-auth

--- pymongo/auth.py
+++ pymongo/auth.py
@@ -757,8 +757,8 @@ def authenticate(credentials, sock_info, reauthenticate=False):
     """Authenticate sock_info."""
     mechanism = credentials.mechanism
     auth_func = _AUTH_MAP[mechanism]
-    if reauthenticate:
-        sock_info.auth_ctx = None
+    if reauthenticate and sock_info.pool_ref().handshake:
+        sock_info.hello()
     if mechanism == "MONGODB-OIDC":
         auth_func(credentials, sock_info, reauthenticate)  # type:ignore
     else:
