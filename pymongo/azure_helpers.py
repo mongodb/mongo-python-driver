348ebde2237635846130de1747054d1ae1c02151
Steven Silvester <steven.silvester@ieee.org>
2023-05-12 09:55:14 -0500

    fix cache init

--- pymongo/azure_helpers.py
+++ pymongo/azure_helpers.py
@@ -1,9 +1,24 @@
+# Copyright 2023-present MongoDB, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Azure helpers."""
 import json
 from datetime import datetime, timedelta, timezone
 from typing import Dict, Tuple
 from urllib.request import Request, urlopen
 
-_CACHE: Dict[str, Tuple[str, datetime]]
+_CACHE: Dict[str, Tuple[str, datetime]] = {}
 _TOKEN_BUFFER_MINUTES = 5
 
 
