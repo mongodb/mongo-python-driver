7723521c76e9ea2af41ca2b8637ec240bcebfc47
Steven Silvester <steven.silvester@ieee.org>
2023-05-17 09:27:59 -0500

    add reauth test

--- test/auth_oidc/test_auth_oidc_azure.py
+++ test/auth_oidc/test_auth_oidc_azure.py
@@ -17,9 +17,13 @@
 import os
 import sys
 import unittest
+from contextlib import contextmanager
 
 sys.path[0:0] = [""]
 
+from test.utils import EventListener
+
+from bson import SON
 from pymongo import MongoClient
 from pymongo.auth_oidc import _CACHE as _oidc_cache
 from pymongo.azure_helpers import _CACHE
@@ -35,6 +39,17 @@ class TestAuthOIDCAzure(unittest.TestCase):
     def setUp(self):
         _oidc_cache.clear()
         _CACHE.clear()
+
+    @contextmanager
+    def fail_point(self, command_args):
+        cmd_on = SON([("configureFailPoint", "failCommand")])
+        cmd_on.update(command_args)
+        client = MongoClient(self.uri)
+        client.admin.command(cmd_on)
+        try:
+            yield
+        finally:
+            client.admin.command("configureFailPoint", cmd_on["configureFailPoint"], mode="off")
 
     def test_connect(self):
         client = MongoClient(self.uri)
@@ -65,6 +80,54 @@ class TestAuthOIDCAzure(unittest.TestCase):
         client.close()
 
         assert len(_CACHE) == 1
+
+    def test_reauthenticate_succeeds(self):
+        listener = EventListener()
+
+        client = MongoClient(self.uri, event_listeners=[listener])
+
+        # Perform a find operation.
+        client.test.test.find_one()
+
+        # Assert that the refresh callback has not been called.
+        # self.assertEqual(self.refresh_called, 0)
+
+        listener.reset()
+
+        with self.fail_point(
+            {
+                "mode": {"times": 1},
+                "data": {"failCommands": ["find"], "errorCode": 391},
+            }
+        ):
+            # Perform a find operation.
+            client.test.test.find_one()
+
+        started_events = [
+            i.command_name for i in listener.started_events if not i.command_name.startswith("sasl")
+        ]
+        succeeded_events = [
+            i.command_name
+            for i in listener.succeeded_events
+            if not i.command_name.startswith("sasl")
+        ]
+        failed_events = [
+            i.command_name for i in listener.failed_events if not i.command_name.startswith("sasl")
+        ]
+
+        self.assertEqual(
+            started_events,
+            [
+                "find",
+                "find",
+            ],
+        )
+        self.assertEqual(succeeded_events, ["find"])
+        self.assertEqual(failed_events, ["find"])
+
+        # Assert that the refresh callback has been called.
+        # self.assertEqual(self.refresh_called, 1)
+        client.close()
 
 
 if __name__ == "__main__":
