20cc793f636f7d372eb2dcda309fd231f0342570
Shane Harvey <shnhrv@gmail.com>
2023-06-08 11:26:39 -0700

    PYTHON-3728 Simplify convert_codec_options signature

--- setup.py
+++ setup.py
@@ -263,12 +263,7 @@ ext_modules = [
     Extension(
         "pymongo._cmessage",
         include_dirs=["bson"],
-        sources=[
-            "pymongo/_cmessagemodule.c",
-            "bson/_cbsonmodule.c",
-            "bson/time64.c",
-            "bson/buffer.c",
-        ],
+        sources=["pymongo/_cmessagemodule.c", "bson/buffer.c"],
     ),
 ]
 
