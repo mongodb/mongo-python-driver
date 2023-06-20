c09065b05a23d2099f22abb8b8e70d6c36b3cef1
sleepyStick <itsirisho@gmail.com>
2023-06-20 11:15:17 -0700

    PYTHON-3740 use Mapping instead of Dict

--- pymongo/helpers.py
+++ pymongo/helpers.py
@@ -156,7 +156,7 @@ def _index_document(index_list: _IndexList) -> SON[str, Any]:
 
 
 def _check_command_response(
-    response: Dict[str, Any],
+    response: Mapping[str, Any],
     max_wire_version: Optional[int],
     allowable_errors: Optional[List[int]] = None,
     parse_write_concern_error: bool = False,
