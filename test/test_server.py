c3ed132723595a4b1bdc530d9c4703784a88c64f
sleepyStick <itsirisho@gmail.com>
2023-06-20 12:22:03 -0700

    PYTHON-3750 ignore types in test_repr

--- test/test_server.py
+++ test/test_server.py
@@ -15,21 +15,20 @@
 """Test the server module."""
 
 import sys
-
-sys.path[0:0] = [""]
-
 from test import unittest
 
 from pymongo.hello import Hello
 from pymongo.server import Server
 from pymongo.server_description import ServerDescription
 
+sys.path[0:0] = [""]
+
 
 class TestServer(unittest.TestCase):
     def test_repr(self):
         hello = Hello({"ok": 1})
         sd = ServerDescription(("localhost", 27017), hello)
-        server = Server(sd, pool=object(), monitor=object())
+        server = Server(sd, pool=object(), monitor=object())  # type: ignore[arg-type]
         self.assertTrue("Standalone" in str(server))
 
 
