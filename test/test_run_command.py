efbfb412085af8e4dbaae13429ebbd562f67e89b
Noah Stapp <noah.stapp@mongodb.com>
2023-06-21 14:29:55 -0700

    WIP

---
+++ test/test_run_command.py
@@ -0,0 +1,17 @@
+import os
+import unittest
+from test.unified_format import generate_test_classes
+
+_TEST_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "run_command")
+
+
+globals().update(
+    generate_test_classes(
+        os.path.join(_TEST_PATH, "unified"),
+        module=__name__,
+    )
+)
+
+
+if __name__ == "__main__":
+    unittest.main()
