e1369fe6d461abf934e3fa074c2c96b81a5ce876
sleepyStick <itsirisho@gmail.com>
2023-06-28 10:18:16 -0700

    PYTHON-3786 fix type ignore

--- pymongo/srv_resolver.py
+++ pymongo/srv_resolver.py
@@ -133,4 +133,6 @@ class _SrvResolver:
 
     def get_hosts_and_min_ttl(self) -> Tuple[List[Tuple[str, Any]], int]:
         results, nodes = self._get_srv_response_and_hosts(False)
-        return nodes, results.rrset.ttl  # type: ignore[union-attr]
+        rrset = results.rrset
+        ttl = rrset.ttl if rrset else 0
+        return nodes, ttl
