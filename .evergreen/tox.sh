10451e7e045d46389d45b8120b3db3e6fa9675da
Noah Stapp <noah.stapp@mongodb.com>
2023-06-28 14:24:20 -0700

    Try createvirtualenv again

--- .evergreen/tox.sh
+++ .evergreen/tox.sh
@@ -3,6 +3,8 @@ set -o errexit  # Exit the script with error if any of the commands fail
 
 PYTHON_BINARY="$1"
 
+. ./utils.sh
+
 if $PYTHON_BINARY -m tox --version; then
     alias run_tox='$PYTHON_BINARY -m tox'
 elif python3 -m tox --version; then
@@ -10,16 +12,7 @@ elif python3 -m tox --version; then
 elif tox --version; then
     alias run_tox=tox
 else # No toolchain present, set up venv before installing tox
-    $PYTHON_BINARY -m venv env
-    if [ "Windows_NT" = "$OS" ]; then
-        # Workaround https://bugs.python.org/issue32451:
-        # mongovenv/Scripts/activate: line 3: $'\r': command not found
-        dos2unix env/Scripts/activate || true
-        . env/Scripts/activate
-    else
-        . env/bin/activate
-    fi
-    $PYTHON_BINARY -m pip install tox
+    createvirtualenv PYTHON_BINARY env
     alias run_tox='$PYTHON_BINARY -m tox'
 fi
 
@@ -27,5 +20,5 @@ alias python='$PYTHON_BINARY'
 run_tox "${@:2}"
 
 if command -v deactivate &> /dev/null; then
-    deactivate
+    trap "deactivate; rm -rf env" EXIT HUP
 fi
