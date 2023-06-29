fc26849a7be8a0e6947a978181865ff55798e06b
Noah Stapp <noah.stapp@mongodb.com>
2023-06-29 13:55:02 -0700

    Use bash function instead of alias

--- .evergreen/tox.sh
+++ .evergreen/tox.sh
@@ -6,17 +6,24 @@ PYTHON_BINARY="$1"
 . .evergreen/utils.sh
 
 if $PYTHON_BINARY -m tox --version; then
-    alias run_tox='$PYTHON_BINARY -m tox'
+    run_tox() {
+      $PYTHON_BINARY -m tox "$@"
+    }
 elif python3 -m tox --version; then
-    alias run_tox='python3 -m tox'
+    run_tox() {
+      python3 -m tox "$@"
+    }
 elif tox --version; then
-    alias run_tox=tox
+    run_tox() {
+      tox "$@"
+    }
 else # No toolchain present, set up venv before installing tox
     createvirtualenv PYTHON_BINARY env
-    alias run_tox='$PYTHON_BINARY -m tox'
+    run_tox() {
+      $PYTHON_BINARY -m tox "$@"
+    }
 fi
 
-alias python='$PYTHON_BINARY'
 run_tox "${@:2}"
 
 if command -v deactivate &> /dev/null; then
