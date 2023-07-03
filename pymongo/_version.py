8212683a1359a76de3d5da07eee74403b8da40d8
sleepyStick <itsirisho@gmail.com>
2023-07-03 10:31:39 -0700

    BUMP 4.4.1.dev0

--- pymongo/_version.py
+++ pymongo/_version.py
@@ -15,7 +15,7 @@
 """Current version of PyMongo."""
 from typing import Tuple, Union
 
-version_tuple: Tuple[Union[int, str], ...] = (4, 4, 0)
+version_tuple: Tuple[Union[int, str], ...] = (4, 4, 1, ".dev0")
 
 
 def get_version_string() -> str:
