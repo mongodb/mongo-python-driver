fb2108b90a39127d0eab2bf9fcfd50b142503e4d
Steven Silvester <steven.silvester@ieee.org>
2023-07-12 09:54:10 -0500

    lint

--- pymongo/aggregation.py
+++ pymongo/aggregation.py
@@ -25,7 +25,6 @@ from pymongo.errors import ConfigurationError
 from pymongo.read_preferences import ReadPreference, _AggWritePref
 
 if TYPE_CHECKING:
-    from bson import CodecOptions
     from pymongo.client_session import ClientSession
     from pymongo.collection import Collection
     from pymongo.command_cursor import CommandCursor
