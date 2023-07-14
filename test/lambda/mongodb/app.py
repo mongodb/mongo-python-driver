525e29bf17d0a76a7c16aca58b58c8c51a8a8969
Steven Silvester <steven.silvester@ieee.org>
2023-07-14 15:35:08 -0500

    PYTHON-3461 Test FaaS (AWS Lambda) Behavior Per Driver

---
+++ test/lambda/mongodb/app.py
@@ -0,0 +1,139 @@
+"""
+Lambda function for Python Driver testing
+
+Creates the client that is cached for all requests, subscribes to
+relevant events, and forces the connection pool to get populated.
+"""
+import json
+import os
+
+from pymongo import MongoClient
+from pymongo.monitoring import (
+    CommandListener,
+    ConnectionPoolListener,
+    ServerHeartbeatListener,
+)
+
+open_connections = 0
+heartbeat_count = 0
+total_heartbeat_duration = 0
+total_commands = 0
+total_command_duration = 0
+
+
+class CommandHandler(CommandListener):
+    def started(self, event):
+        print("command started", event)
+
+    def succeeded(self, event):
+        global total_commands, total_command_duration
+        total_commands += 1
+        total_command_duration += event.duration_micros / 1e6
+        print("command succeeded", event)
+
+    def failed(self, event):
+        global total_commands, total_command_duration
+        total_commands += 1
+        total_command_duration += event.duration_micros / 1e6
+        print("command failed", event)
+
+
+class ServerHeartbeatHandler(ServerHeartbeatListener):
+    def started(self, event):
+        print("server heartbeat started", event)
+
+    def succeeded(self, event):
+        global heartbeat_count, total_heartbeat_duration
+        heartbeat_count += 1
+        total_heartbeat_duration += event.duration
+        print("server heartbeat succeeded", event)
+
+    def failed(self, event):
+        global heartbeat_count, total_heartbeat_duration
+        heartbeat_count += 1
+        total_heartbeat_duration += event.duration
+        print("server heartbeat  failed", event)
+
+
+class ConnectionHandler(ConnectionPoolListener):
+    def connection_created(self, event):
+        global open_connections
+        open_connections += 1
+        print("connection created")
+
+    def connection_ready(self, event):
+        pass
+
+    def connection_closed(self, event):
+        global open_connections
+        open_connections -= 1
+        print("connection closed")
+
+    def connection_check_out_started(self, event):
+        pass
+
+    def connection_check_out_failed(self, event):
+        pass
+
+    def connection_checked_out(self, event):
+        pass
+
+    def connection_checked_in(self, event):
+        pass
+
+    def pool_created(self, event):
+        pass
+
+    def pool_ready(self, event):
+        pass
+
+    def pool_cleared(self, event):
+        pass
+
+    def pool_closed(self, event):
+        pass
+
+
+listeners = [CommandHandler(), ServerHeartbeatHandler(), ConnectionHandler()]
+client = MongoClient(os.environ["MONGODB_URI"], event_listeners=listeners)
+
+
+# Populate the connection pool.
+client.lambdaTest.list_collections()
+
+# Create the response to send back.
+def create_response():
+    return dict(
+        averageCommandDuration=total_command_duration / total_commands,
+        averageHeartbeatDuration=total_heartbeat_duration / heartbeat_count,
+        openConnections=open_connections,
+        heartbeatCount=heartbeat_count,
+    )
+
+
+# Reset the numbers.
+def reset():
+    global open_connections, heartbeat_count, total_heartbeat_duration, total_commands, total_command_duration
+    open_connections = 0
+    heartbeat_count = 0
+    total_heartbeat_duration = 0
+    total_commands = 0
+    total_command_duration = 0
+
+
+"""
+The handler function itself performs an insert/delete and returns the
+id of the document in play.
+"""
+
+
+def lambda_handler(event):
+    db = client.lambdaTest
+    collection = db.test
+    inserted_id = collection.insert_one({"n": 1})
+    collection.delete_one({"_id": inserted_id})
+    # Create the response and then reset the numbers.
+    response = json.dumps(create_response())
+    reset()
+
+    return dict(statusCode=200, body=response)
