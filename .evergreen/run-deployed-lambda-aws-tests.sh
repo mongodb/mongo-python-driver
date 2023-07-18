a3940e3fab6ac202d56917328e90705bfc93f093
Steven Silvester <steven.silvester@ieee.org>
2023-07-17 22:25:03 -0500

    debug

--- .evergreen/run-deployed-lambda-aws-tests.sh
+++ .evergreen/run-deployed-lambda-aws-tests.sh
@@ -2,4 +2,5 @@
 set -o errexit  # Exit the script with error if any of the commands fail
 
 export PATH="/opt/python/3.9/bin:$PATH"
+python --version
 . ${DRIVERS_TOOLS}/.evergreen/run-deployed-lambda-aws-tests.sh
