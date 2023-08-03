8579b47064e3ab2b89851e2679b6b9431aa34d36
Steven Silvester <steven.silvester@ieee.org>
2023-08-03 05:36:26 -0500

    clean up green test

--- green_test.py
+++ green_test.py
@@ -1,7 +1,10 @@
 import asyncio
 
-from greenletio import await_
-from greenletio.green.threading import Thread
+from greenletio import await_, patch_blocking
+
+with patch_blocking():
+    import time
+    from threading import Thread
 
 from bson import SON
 from pymongo import MongoClient
@@ -11,32 +14,71 @@ from pymongo.database import Database as SyncDatabase
 
 
 def test_database():
+    async def printer():
+        while 1:
+            print("sleep:  ", int(time.time()))
+            await asyncio.sleep(1)
+
+    def async_main_entry():
+        await_(async_main())
+
     async def async_main():
+        print("starting async!")
+        task = asyncio.create_task(printer())
+
         command = SON(
             [("insert", "test"), ("ordered", False), ("documents", [{"hello2": "world2"}])]
         )
         client = MongoClient("mongodb://127.0.0.1:27017/", directConnection=True)
         db = AsyncDatabase(client, "test")
-        await db.async_command(command)
+        for i in range(10):
+            print("insert: ", int(time.time()))
+            await db.async_command(command)
+            await asyncio.sleep(1)
+        task.cancel()
+        client.close()
+        await test_collection()
+        print("ended async")
 
     def main():
+        print("starting sync!")
         client = MongoClient("mongodb://127.0.0.1:27017/", directConnection=True)
         db = SyncDatabase(client, "test")
         # db.test.insert_one({"hello": "world"})
-        found = db.test.find({"hello2": "world2"})
-        for val in found:
-            print(val)
+        for i in range(10):
+            found = db.test.find_one({"hello2": "world2"})
+            print("find:   ", int(time.time()))
+            time.sleep(1)
+        client.close()
+        print("ended sync")
 
     sync_thread = Thread(target=main)
-    async_thread = Thread(target=await_(async_main()))
+    async_thread = Thread(target=async_main_entry)
+
+    """
+    Notes: if we use synchronous function calls, it still blocks as if they were both on the main thread if they use any primitives from the stdlib.
+    We probably need to somehow opt-in to using asyncio rather than basing
+    it on if greenletio is installed.
+    We could offer the equivalent of eventlet.monkey_patch and require that
+    for the use of pymongo.asyncio
+    """
+    client = MongoClient("mongodb://127.0.0.1:27017/", directConnection=True)
+    client.drop_database("test")
+    client.close()
 
     async_thread.start()
     sync_thread.start()
+
+    for i in range(10):
+        print("waiting:", int(time.time()))
+        time.sleep(1)
+
     async_thread.join()
     sync_thread.join()
 
 
 async def test_collection():
+    print("hello from test collection!")
     client = MongoClient("mongodb://127.0.0.1:27017/", directConnection=True)
     db = SyncDatabase(client, "test")
     collection = AsyncCollection(db, name="test")
@@ -45,5 +87,4 @@ async def test_collection():
         print(val)
 
 
-# test_database()
-asyncio.run(test_collection())
+test_database()
