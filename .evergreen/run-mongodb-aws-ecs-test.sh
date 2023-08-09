2fdb6de00507084445a9cc892fc86da13c74a50e
Steven Silvester <steven.silvester@ieee.org>
2023-08-08 20:44:54 -0500

    fix typo

--- .evergreen/run-mongodb-aws-ecs-test.sh
+++ .evergreen/run-mongodb-aws-ecs-test.sh
@@ -27,4 +27,4 @@ apt install python3.7 python3-pip -y
 export PYTHON_BINARY="python3.7"
 export AWS_AUTH_TEST=1
 export SET_XTRACE_ON=1
-bash ./src/.evergeen/tox.sh -m test-eg
+bash ./src/.evergreen/tox.sh -m test-eg
