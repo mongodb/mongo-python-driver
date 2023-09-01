f3d309353ad5bd49b1fbce574ba5d067ec9b9516
Noah Stapp <noah.stapp@mongodb.com>
2023-09-01 11:26:46 -0700

    Fix

--- pymongo/io/dns.py
+++ pymongo/io/dns.py
@@ -1,39 +1,38 @@
-#
-# # try:
-# from typing import Optional, Union
-#
-# from greenletio import await_
-# from dns import rdatatype, rdataclass, asyncresolver
-# from dns._asyncbackend import Backend
-# from dns.name import Name
-# from dns.rdataclass import RdataClass
-# from dns.rdatatype import RdataType
-# from dns.resolver import NXDOMAIN, Answer, NoAnswer
-#
-# def resolve(
-#     qname: Union[Name, str],
-#     rdtype: Union[RdataType, str] = rdatatype.A,
-#     rdclass: Union[RdataClass, str] = rdataclass.IN,
-#     tcp: bool = False,
-#     source: Optional[str] = None,
-#     raise_on_no_answer: bool = True,
-#     source_port: int = 0,
-#     lifetime: Optional[float] = None,
-#     search: Optional[bool] = None,
-# ) -> Answer:
-#     return await_(
-#         asyncresolver.resolve(
-#             qname,
-#             rdtype,
-#             rdclass,
-#             tcp,
-#             source,
-#             raise_on_no_answer,
-#             source_port,
-#             lifetime,
-#             search,
-#         )
-#     )
-#
-# # except ImportError:
-from dns.resolver import NXDOMAIN, Answer, NoAnswer, resolve
+try:
+    from typing import Optional, Union
+
+    from dns import asyncresolver, rdataclass, rdatatype
+    from dns._asyncbackend import Backend
+    from dns.name import Name
+    from dns.rdataclass import RdataClass
+    from dns.rdatatype import RdataType
+    from dns.resolver import NXDOMAIN, Answer, NoAnswer
+    from greenletio import await_
+
+    def resolve(
+        qname: Union[Name, str],
+        rdtype: Union[RdataType, str] = rdatatype.A,
+        rdclass: Union[RdataClass, str] = rdataclass.IN,
+        tcp: bool = False,
+        source: Optional[str] = None,
+        raise_on_no_answer: bool = True,
+        source_port: int = 0,
+        lifetime: Optional[float] = None,
+        search: Optional[bool] = None,
+    ) -> Answer:
+        return await_(
+            asyncresolver.resolve(
+                qname,
+                rdtype,
+                rdclass,
+                tcp,
+                source,
+                raise_on_no_answer,
+                source_port,
+                lifetime,
+                search,
+            )
+        )
+
+except ImportError:
+    from dns.resolver import NXDOMAIN, Answer, NoAnswer, resolve
