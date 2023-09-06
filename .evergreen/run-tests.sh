7a15bd052879c4b30f1ee7e5bbcb27cf80ffbd68
Steven Silvester <steven.silvester@ieee.org>
2023-09-05 21:35:13 -0500

    debug on macos

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -259,6 +259,9 @@ fi
 # Show the installed packages
 PIP_QUIET=0 python -m pip list
 
+python -c "import dns.resolver;print(dns.resolver.canonical_name('https://www.google.com'))"
+exit 0
+
 if [ -z "$GREEN_FRAMEWORK" ]; then
     if [ -z "$C_EXTENSIONS" ] && [ "$PYTHON_IMPL" = "CPython" ]; then
         python setup.py build_ext -i
