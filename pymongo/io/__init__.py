b65d3a4d5c3f2720e10164dbf8b373dd9af936de
Noah Stapp <noah.stapp@mongodb.com>
2023-09-06 13:12:28 -0700

    Add select patch

--- pymongo/io/__init__.py
+++ pymongo/io/__init__.py
@@ -1,9 +1,11 @@
 try:
-    from greenletio.green import select as select
+    # from greenletio.green import select as select
     from greenletio.green import socket as socket
     from greenletio.green import ssl as ssl
     from greenletio.green import threading as threading
     from greenletio.green import time as time
+
+    from pymongo.io import select as select
 except ImportError:
     import select
     import socket
