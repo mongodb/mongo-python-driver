e8fac310c52463abacf9b9afe5c692c28cce4865
Steven Silvester <steven.silvester@ieee.org>
2023-09-28 18:02:25 -0500

    fix integration test

--- test/test_on_demand_csfle.py
+++ test/test_on_demand_csfle.py
@@ -77,8 +77,8 @@ class TestonDemandAzureCredentials(IntegrationTest):
     def setUp(self):
         super().setUp()
         self.master_key = {
-            "keyVaultEndpoint": "https://keyvault-drivers-2411.vault.azure.net/keys/",
-            "keyName": "KEY-NAME",
+            "keyVaultEndpoint": os.environ["AZUREKMS_KEYVAULTENDPOINT"],
+            "keyName": os.environ["AZUREKMS_KEYNAME"],
         }
 
     @unittest.skipIf(not os.getenv("TEST_FLE_AZURE_AUTO"), "Not testing FLE Azure auto")
