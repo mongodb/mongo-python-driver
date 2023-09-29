bb757749d7c7386edeb8fd09911957307f87d922
Noah Stapp <noah.stapp@mongodb.com>
2023-09-29 12:18:43 -0700

    TESTING

--- pymongo/network.py
+++ pymongo/network.py
@@ -116,6 +116,8 @@ def command(
     ns = dbname + ".$cmd"
     speculative_hello = False
 
+    print(f"EXHAUST_ALLOWED: {exhaust_allowed}")
+
     # Publish the original command document, perhaps with lsid and $clusterTime.
     orig = spec
     if is_mongos and not use_op_msg:
