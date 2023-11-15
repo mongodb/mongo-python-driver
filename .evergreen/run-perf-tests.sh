321dbb6f6088fc412515acd156be13de49dffbd3
Shane Harvey <shnhrv@gmail.com>
2023-11-15 15:51:49 -0800

    PYTHON-3823 debug pythons

--- .evergreen/run-perf-tests.sh
+++ .evergreen/run-perf-tests.sh
@@ -13,6 +13,11 @@ cd ..
 export TEST_PATH="${PROJECT_DIRECTORY}/driver-performance-test-data"
 export OUTPUT_FILE="${PROJECT_DIRECTORY}/results.json"
 
+echo "python3 version:"
+python3 --version || true
+echo "mongodbtoolchain versions:"
+ls -la /opt/mongodbtoolchain/
+
 export PYTHON_BINARY=/opt/mongodbtoolchain/v3/bin/python3
 export PERF_TEST=1
 
