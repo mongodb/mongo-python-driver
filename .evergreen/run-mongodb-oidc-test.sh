df39c530e84a711e808f0724b1d48aad76e70cd0
Steven Silvester <steven.silvester@ieee.org>
2024-02-04 20:58:08 -0600

    fix for oidc and aws

--- .evergreen/run-mongodb-oidc-test.sh
+++ .evergreen/run-mongodb-oidc-test.sh
@@ -16,13 +16,13 @@ if [ $OIDC_PROVIDER_NAME == "aws" ]; then
 
     # Get the drivers secrets.  Use an existing secrets file first.
     if [ ! -f "./secrets-export.sh" ]; then
-        bash ${DRIVERS_TOOLS}/.evergreen/auth_oidc/setup_secrets.sh
+        bash ${DRIVERS_TOOLS}/.evergreen/auth_oidc/setup-secrets.sh
     fi
     source ./secrets-export.sh
 
     # # If the file did not have our creds, get them from the vault.
     if [ -z "$OIDC_ATLAS_URI_SINGLE" ]; then
-        bash ${DRIVERS_TOOLS}/.evergreen/auth_oidc/setup_secrets.sh
+        bash ${DRIVERS_TOOLS}/.evergreen/auth_oidc/setup-secrets.sh
         source ./secrets-export.sh
     fi
 
