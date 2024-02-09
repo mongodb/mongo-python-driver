496ba1e0d5b7fcc1fb7fff7e754ac4d9467dbdc1
Steven Silvester <steven.silvester@ieee.org>
2024-02-09 15:16:14 -0600

    try without authMech guard

--- test/utils.py
+++ test/utils.py
@@ -558,7 +558,7 @@ def _mongo_client(host, port, authenticate=True, directConnection=None, **kwargs
     client_options.update(kwargs)
 
     uri = _connection_string(host)
-    if client_context.auth_enabled and authenticate and "authMechanism" not in kwargs:
+    if client_context.auth_enabled:
         # Only add the default username or password if one is not provided.
         res = parse_uri(uri)
         if (
@@ -569,7 +569,6 @@ def _mongo_client(host, port, authenticate=True, directConnection=None, **kwargs
         ):
             client_options["username"] = db_user
             client_options["password"] = db_pwd
-
     return MongoClient(uri, port, **client_options)
 
 
