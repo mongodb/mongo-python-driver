a2cf50027e032ff72e267b03deb4a7177c24564d
Steven Silvester <steve.silvester@mongodb.com>
2024-02-28 11:41:47 -0600

    clean up setup

--- .evergreen/run-mongodb-oidc-test.sh
+++ .evergreen/run-mongodb-oidc-test.sh
@@ -43,24 +43,37 @@ if [ $OIDC_PROVIDER_NAME == "aws" ]; then
     export OIDC_ADMIN_USER=$OIDC_ALTAS_USER
     export OIDC_ADMIN_PWD=$OIDC_ATLAS_PASSWORD
 
-elif [ $OIDC_PROVIDER_NAME == "azure" ] || [ $OIDC_PROVIDER_NAME == "gcp" ]; then
-    if [ -z "${OIDC_AUDIENCE:-}" ]; then
-        echo "Must specify an OIDC_AUDIENCE"
+elif [ $OIDC_PROVIDER_NAME == "azure" ]; then
+    if [ -z "${AZUREOIDC_AUDIENCE:-}" ]; then
+        echo "Must specify an AZUREOIDC_AUDIENCE"
         exit 1
     fi
+    export OIDC_AUDIENCE=$AZUREOIDC_AUDIENCE
     set +x   # turn off xtrace for this portion
-    export OIDC_ADMIN_USER=$OIDC_USERNAME
+    export OIDC_ADMIN_USER=$AZUREOIDC_USERNAME
     export OIDC_ADMIN_PWD=pwd123
     set -x
+elif [ $OIDC_PROVIDER_NAME == "azure" ]; then
+    if [ -z "${GCPOIDC_AUDIENCE:-}" ]; then
+        echo "Must specify an GCPOIDC_AUDIENCE"
+        exit 1
+    fi
+    export OIDC_AUDIENCE=$GCPOIDC_AUDIENCE
+    set +x   # turn off xtrace for this portion
+    export OIDC_ADMIN_USER=$GCPOIDC_ALTAS_USER
+    export OIDC_ADMIN_PWD=$GPDCOIDC_ATLAS_PASSWORD
+    set -x
+else
+    echo "Unrecognized OIDC_PROVIDER_NAME $OIDC_PROVIDER_NAME"
+    exit 1
+fi
+
+if [ $OIDC_PROVIDER_NAME == "azure" ] || [ $OIDC_PROVIDER_NAME == "gcp" ]; then
     export MONGODB_URI=${MONGODB_URI:-"mongodb://localhost"}
     MONGODB_URI_SINGLE="${MONGODB_URI}/?authMechanism=MONGODB-OIDC"
     MONGODB_URI_SINGLE="${MONGODB_URI_SINGLE}&authMechanismProperties=PROVIDER_NAME:${OIDC_PROVIDER_NAME}"
     export MONGODB_URI_SINGLE="${MONGODB_URI_SINGLE},TOKEN_AUDIENCE:${OIDC_AUDIENCE}"
     export MONGODB_URI_MULTI=$MONGODB_URI_SINGLE
-
-else
-    echo "Unrecognized OIDC_PROVIDER_NAME $OIDC_PROVIDER_NAME"
-    exit 1
 fi
 
 export TEST_AUTH_OIDC=1
