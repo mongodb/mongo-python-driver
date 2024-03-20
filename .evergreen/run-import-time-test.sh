0b2e31a13d0b89718fa21defc55bd0bf87375609
Steven Silvester <steve.silvester@mongodb.com>
2024-03-20 07:51:30 -0500

    try again

--- .evergreen/run-import-time-test.sh
+++ .evergreen/run-import-time-test.sh
@@ -17,7 +17,6 @@ fi
 
 function get_import_time() {
     local log_file
-    local last_line
     createvirtualenv "$PYTHON_BINARY" import-venv
     python -m pip install -q ".[aws,encryption,gssapi,ocsp,snappy,zstd]"
     # Import once to cache modules
@@ -26,12 +25,13 @@ function get_import_time() {
     python -X importtime -c "import pymongo" 2> $log_file
     last_line=$(echo "$(tail -n 1 $log_file)" | cut -d " " -f 5)
     rm -rf import-venv
-    echo $last_line
 }
 
-import_time_curr=$(get_import_time $HEAD_SHA)
+get_import_time $HEAD_SHA
+import_time_curr=$last_line
 git checkout $BASE_SHA
-import_time_prev=$(get_import_time $BASE_SHA)
+get_import_time $BASE_SHA
+import_time_prev=$last_line
 
 # Check if we got 20% or more slower
 let diff=$import_time_curr-$import_time_prev
