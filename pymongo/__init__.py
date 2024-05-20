ac18c7f209b742236e59e64dde6f332247fc9424
Steven Silvester <steve.silvester@mongodb.com>
2024-05-20 12:37:51 -0400

    fix import

--- pymongo/__init__.py
+++ pymongo/__init__.py
@@ -25,7 +25,6 @@ __all__ = [
     "HASHED",
     "TEXT",
     "version_tuple",
-    "get_version_string",
     "__version__",
     "version",
     "ReturnDocument",
@@ -86,7 +85,7 @@ TEXT = "text"
 """
 
 from pymongo import _csot
-from pymongo._version import __version__, get_version_string, version_tuple
+from pymongo._version import __version__, version_tuple
 from pymongo.collection import ReturnDocument
 from pymongo.common import MAX_SUPPORTED_WIRE_VERSION, MIN_SUPPORTED_WIRE_VERSION
 from pymongo.cursor import CursorType
