241894e4cff81ddce45fb276faa8df414244b608
Noah Stapp <noah.stapp@mongodb.com>
2024-06-03 10:28:32 -0700

    Remove unnecessary imports from pymongo init

--- pymongo/__init__.py
+++ pymongo/__init__.py
@@ -106,10 +106,7 @@ from pymongo.synchronous.operations import (
 from pymongo.asynchronous.mongo_client import AsyncMongoClient
 
 # isort: on
-import pymongo.synchronous
 
-# This is required to break an implicit import issue, can go away in 5.0
-import pymongo.synchronous.encryption  # noqa: F401
 from pymongo.synchronous.read_preferences import ReadPreference
 from pymongo.write_concern import WriteConcern
 
