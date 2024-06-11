0e53cc83189a96082e2f4a1f477a8cfc9f5d960a
Shane Harvey <shnhrv@gmail.com>
2024-06-11 14:54:43 -0700

    PYTHON-4347 Improve performance by only calling get_topology once

--- pymongo/synchronous/mongo_client.py
+++ pymongo/synchronous/mongo_client.py
@@ -853,6 +853,7 @@ class MongoClient(common.BaseObject, Generic[_DocumentType]):
             server_monitoring_mode=options.server_monitoring_mode,
         )
 
+        self._opened = False
         self._init_background()
 
         if _IS_SYNC and connect:
@@ -1527,9 +1528,11 @@ class MongoClient(common.BaseObject, Generic[_DocumentType]):
         If this client was created with "connect=False", calling _get_topology
         launches the connection process in the background.
         """
-        self._topology.open()
-        with self._lock:
-            self._kill_cursors_executor.open()
+        if not self._opened:
+            self._topology.open()
+            with self._lock:
+                self._kill_cursors_executor.open()
+            self._opened = True
         return self._topology
 
     @contextlib.contextmanager
