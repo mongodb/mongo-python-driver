b89f7df2e881b7bfa2a3c6ff08e4407ac01c827b
Shane Harvey <shnhrv@gmail.com>
2024-06-26 08:55:07 -0700

    PYTHON-4507 pip>=21.3 is required for editable installs

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
-msg = "PyMongo>=4.8 no longer supports building via setup.py, use python -m pip install <path/to/pymongo> instead"
+msg = (
+    "PyMongo>=4.8 no longer supports building via setup.py, use python -m pip install <path/to/pymongo> instead. If "
+    "this is an editable install (-e) please upgrade to pip>=21.3 first: python -m pip install --upgrade pip"
+)
 
 raise RuntimeError(msg)
