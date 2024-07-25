d718f91aa175bcc85113e1c6d46f8578a7f09771
Noah Stapp <noah.stapp@mongodb.com>
2024-07-25 13:43:49 -0700

    PYTHON-4537 - Use selector asyncio loop on windows tests

--- test/__init__.py
+++ test/__init__.py
@@ -79,6 +79,12 @@ from pymongo.synchronous.mongo_client import MongoClient
 
 _IS_SYNC = True
 
+# The default asyncio loop implementation on Windows
+# has issues with sharing sockets across loops (https://github.com/python/cpython/issues/122240)
+# We explicitly use a different loop implementation here to prevent that issue
+if not _IS_SYNC and sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
 
 class ClientContext:
     client: MongoClient
