909f12396a5360b8737f4656cd696176248e5368
Steven Silvester <steve.silvester@mongodb.com>
2024-08-01 10:54:00 -0500

    fixups

--- .evergreen/run-azurekms-fail-test.sh
+++ .evergreen/run-azurekms-fail-test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -o errexit  # Exit the script with error if any of the commands fail
-SCRIPT_DIR=$(dirname ${BASH_SOURCE:-$0})
+HERE=$(dirname ${BASH_SOURCE:-$0})
 . $DRIVERS_TOOLS/.evergreen/csfle/azurekms/setup-secrets.sh
 LIBMONGOCRYPT_URL=https://s3.amazonaws.com/mciuploads/libmongocrypt/debian11/master/latest/libmongocrypt.tar.gz
-SKIP_SERVERS=1 bash $SCRIPT_DIR/setup-encryption.sh
+SKIP_SERVERS=1 bash $HERE/setup-encryption.sh
 PYTHON_BINARY=/opt/mongodbtoolchain/v4/bin/python3 \
     KEY_NAME="${AZUREKMS_KEYNAME}" \
     KEY_VAULT_ENDPOINT="${AZUREKMS_KEYVAULTENDPOINT}" \
     SUCCESS=false TEST_FLE_AZURE_AUTO=1 \
     $SCRIPT_DIR/hatch.sh test:test-eg
-bash $SCRIPT_DIR/teardown-encryption.sh
+bash $HERE/teardown-encryption.sh
