9f95f4ed23a2c47a420c71f85b6c1f984b7fd42d
Shane Harvey <shnhrv@gmail.com>
2024-08-06 17:01:19 -0700

    PYTHON-4632 Revert unneeded changes

--- test/asynchronous/__init__.py
+++ test/asynchronous/__init__.py
@@ -730,9 +730,9 @@ class AsyncClientContext:
 
     def require_failCommand_appName(self, func):
         """Run a test only if the server supports the failCommand appName."""
-        # SERVER-47195 and SERVER-49336.
+        # SERVER-47195
         return self._require(
-            lambda: (self.test_commands_enabled and self.version >= (4, 4, 7)),
+            lambda: (self.test_commands_enabled and self.version >= (4, 4, -1)),
             "failCommand appName must be supported",
             func=func,
         )
