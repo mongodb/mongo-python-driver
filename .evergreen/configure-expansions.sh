aa44ef73972e9247c820c6fa8cf66cb425050f0f
Jib <jib.adegunloye@mongodb.com>
2024-08-12 13:57:36 -0400

    Pushed PREPARE_SHELL creation into an env.sh file

---
+++ .evergreen/configure-expansions.sh
@@ -0,0 +1,40 @@
+set +x
+# Get the current unique version of this checkout
+if [ "$is_patch" = "true" ]; then
+    CURRENT_VERSION="$(git describe)-patch-$version_id"
+else
+    CURRENT_VERSION=latest
+fi
+
+export DRIVERS_TOOLS="$(dirname $(pwd))/drivers-tools"
+export PROJECT_DIRECTORY="$(pwd)"
+
+# Python has cygwin path problems on Windows. Detect prospective mongo-orchestration home directory
+if [ "Windows_NT" = "$OS" ]; then # Magic variable in cygwin
+    export DRIVERS_TOOLS=$(cygpath -m $DRIVERS_TOOLS)
+    export PROJECT_DIRECTORY=$(cygpath -m $PROJECT_DIRECTORY)
+fi
+
+cat <<EOT > env.sh
+export MONGO_ORCHESTRATION_HOME="$DRIVERS_TOOLS/.evergreen/orchestration"
+export MONGODB_BINARIES="$DRIVERS_TOOLS/mongodb/bin"
+
+export CURRENT_VERSION="$CURRENT_VERSION"
+export DRIVERS_TOOLS="$DRIVERS_TOOLS"
+export MONGO_ORCHESTRATION_HOME="$MONGO_ORCHESTRATION_HOME"
+export MONGODB_BINARIES="$MONGODB_BINARIES"
+export PROJECT_DIRECTORY="$PROJECT_DIRECTORY"
+export PROJECT="$PROJECT"
+
+set -o errexit
+export SKIP_LEGACY_SHELL=1
+export DRIVERS_TOOLS="$DRIVERS_TOOLS"
+export MONGO_ORCHESTRATION_HOME="$MONGO_ORCHESTRATION_HOME"
+export MONGODB_BINARIES="$MONGODB_BINARIES"
+export PROJECT_DIRECTORY="$PROJECT_DIRECTORY"
+
+export TMPDIR="$MONGO_ORCHESTRATION_HOME/db"
+export PATH="$MONGODB_BINARIES:$PATH"
+export PROJECT="$project"
+export PIP_QUIET=1
+EOT
