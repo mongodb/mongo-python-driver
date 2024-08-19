efcecc9a7fbe9ee3850d274b8a6dff74e0a0b30e
Steven Silvester <steven.silvester@ieee.org>
2024-08-19 12:57:15 -0500

    PYTHON-4648 Fix handling of event_loop_policy in tests (#1799)

--- test/conftest.py
+++ test/conftest.py
@@ -1,10 +1,23 @@
 from __future__ import annotations
 
+import asyncio
+import sys
 from test import pytest_conf, setup, teardown
 
 import pytest
 
 _IS_SYNC = True
+
+
+@pytest.fixture(scope="session")
+def event_loop_policy():
+    # The default asyncio loop implementation on Windows
+    # has issues with sharing sockets across loops (https://github.com/python/cpython/issues/122240)
+    # We explicitly use a different loop implementation here to prevent that issue
+    if sys.platform == "win32":
+        return asyncio.WindowsSelectorEventLoopPolicy()  # type: ignore[attr-defined]
+
+    return asyncio.get_event_loop_policy()
 
 
 @pytest.fixture(scope="session", autouse=True)
