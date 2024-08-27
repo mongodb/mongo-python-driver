a0039edd85f5eb449e9c95d7a63e18370275f909
Steven Silvester <steve.silvester@mongodb.com>
2024-08-27 10:08:54 -0500

    fix typing

--- bson/__init__.py
+++ bson/__init__.py
@@ -1183,17 +1183,18 @@ def _decode_selective(
 def _array_of_documents_to_buffer(data: Union[memoryview, bytes]) -> bytes:
     # Extract the raw bytes of each document.
     position = 0
-    _, end = _get_object_size(data, position, len(data))
+    view = memoryview(data)
+    _, end = _get_object_size(view, position, len(view))
     position += 4
     buffers: list[memoryview] = []
     append = buffers.append
     while position < end - 1:
         # Just skip the keys.
-        while data[position] != 0:
+        while view[position] != 0:
             position += 1
         position += 1
-        obj_size, _ = _get_object_size(data, position, end)
-        append(data[position : position + obj_size])
+        obj_size, _ = _get_object_size(view, position, end)
+        append(view[position : position + obj_size])
         position += obj_size
     if position != end:
         raise InvalidBSON("bad object or element length")
