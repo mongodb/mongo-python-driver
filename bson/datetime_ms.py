a2059dc9cb5485d16fbf31e32d3d816604e1bef9
Shane Harvey <shnhrv@gmail.com>
2024-08-28 14:20:55 -0700

    PYTHON-4663 Fix compatibility with dateutil timezones (#1812)

--- bson/datetime_ms.py
+++ bson/datetime_ms.py
@@ -20,7 +20,6 @@ from __future__ import annotations
 
 import calendar
 import datetime
-import functools
 from typing import Any, Union, cast
 
 from bson.codec_options import DEFAULT_CODEC_OPTIONS, CodecOptions, DatetimeConversion
@@ -127,11 +126,8 @@ _MIN_UTC_MS = _datetime_to_millis(_MIN_UTC)
 _MAX_UTC_MS = _datetime_to_millis(_MAX_UTC)
 
 
-# Inclusive and exclusive min and max for timezones.
-# Timezones are hashed by their offset, which is a timedelta
-# and therefore there are more than 24 possible timezones.
-@functools.lru_cache(maxsize=None)
-def _min_datetime_ms(tz: datetime.timezone = datetime.timezone.utc) -> int:
+# Inclusive min and max for timezones.
+def _min_datetime_ms(tz: datetime.tzinfo = utc) -> int:
     delta = tz.utcoffset(_MIN_UTC)
     if delta is not None:
         offset_millis = (delta.days * 86400 + delta.seconds) * 1000 + delta.microseconds // 1000
@@ -140,8 +136,7 @@ def _min_datetime_ms(tz: datetime.timezone = datetime.timezone.utc) -> int:
     return max(_MIN_UTC_MS, _MIN_UTC_MS - offset_millis)
 
 
-@functools.lru_cache(maxsize=None)
-def _max_datetime_ms(tz: datetime.timezone = datetime.timezone.utc) -> int:
+def _max_datetime_ms(tz: datetime.tzinfo = utc) -> int:
     delta = tz.utcoffset(_MAX_UTC)
     if delta is not None:
         offset_millis = (delta.days * 86400 + delta.seconds) * 1000 + delta.microseconds // 1000
@@ -159,7 +154,7 @@ def _millis_to_datetime(
         or opts.datetime_conversion == DatetimeConversion.DATETIME_CLAMP
         or opts.datetime_conversion == DatetimeConversion.DATETIME_AUTO
     ):
-        tz = opts.tzinfo or datetime.timezone.utc
+        tz = opts.tzinfo or utc
         if opts.datetime_conversion == DatetimeConversion.DATETIME_CLAMP:
             millis = max(_min_datetime_ms(tz), min(millis, _max_datetime_ms(tz)))
         elif opts.datetime_conversion == DatetimeConversion.DATETIME_AUTO:
--- bson/objectid.py
+++ bson/objectid.py
@@ -16,7 +16,6 @@
 from __future__ import annotations
 
 import binascii
-import calendar
 import datetime
 import os
 import struct
@@ -25,6 +24,7 @@ import time
 from random import SystemRandom
 from typing import Any, NoReturn, Optional, Type, Union
 
+from bson.datetime_ms import _datetime_to_millis
 from bson.errors import InvalidId
 from bson.tz_util import utc
 
@@ -131,11 +131,10 @@ class ObjectId:
         :param generation_time: :class:`~datetime.datetime` to be used
             as the generation time for the resulting ObjectId.
         """
-        offset = generation_time.utcoffset()
-        if offset is not None:
-            generation_time = generation_time - offset
-        timestamp = calendar.timegm(generation_time.timetuple())
-        oid = _PACK_INT(int(timestamp)) + b"\x00\x00\x00\x00\x00\x00\x00\x00"
+        oid = (
+            _PACK_INT(_datetime_to_millis(generation_time) // 1000)
+            + b"\x00\x00\x00\x00\x00\x00\x00\x00"
+        )
         return cls(oid)
 
     @classmethod
