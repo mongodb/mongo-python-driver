2a6d3eee9bd11d54b859bdd87a0d498186d64324
Steven Silvester <steve.silvester@mongodb.com>
2024-08-30 20:10:58 -0500

    PYTHON-4706 Allow running pytest directly without hatch

--- .evergreen/hatch.sh
+++ .evergreen/hatch.sh
@@ -8,6 +8,18 @@ if [ -z "$PYTHON_BINARY" ]; then
     PYTHON_BINARY=$(find_python3)
 fi
 
+# Check if we should run pytest directly.
+if [ -n "$USE_PYTEST" ]; then
+    ENV_NAME=testenv-$RANDOM
+    createvirtualenv "$PYTHON_BINARY" $ENV_NAME
+    # shellcheck disable=SC2064
+    trap "deactivate; rm -rf $ENV_NAME" EXIT HUP
+    python -m pip install -e ".[test]"
+    pytest
+    return
+fi
+
+
 if $PYTHON_BINARY -m hatch --version; then
     run_hatch() {
       $PYTHON_BINARY -m hatch run "$@"
