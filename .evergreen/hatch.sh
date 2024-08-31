844635660b0b386d5f5c6a8c5eda71a7495f303f
Steven Silvester <steve.silvester@mongodb.com>
2024-08-31 10:28:56 -0500

    fix test setup

--- .evergreen/hatch.sh
+++ .evergreen/hatch.sh
@@ -8,19 +8,17 @@ if [ -z "$PYTHON_BINARY" ]; then
     PYTHON_BINARY=$(find_python3)
 fi
 
-# Check if we should run pytest directly.
-if [ -n "$USE_PYTEST" ]; then
+# Check if we should skip hatch and run the tests directly.
+if [ -n "$SKIP_HATCH" ]; then
     ENV_NAME=testenv-$RANDOM
     createvirtualenv "$PYTHON_BINARY" $ENV_NAME
     # shellcheck disable=SC2064
     trap "deactivate; rm -rf $ENV_NAME" EXIT HUP
     python -m pip install -e ".[test]"
-    pytest -v --capture=tee-sys --durations=5 --maxfail=10
-    exit 0
-fi
-
-
-if $PYTHON_BINARY -m hatch --version; then
+    run_hatch() {
+      bash ./.evergreen/run-tests.sh "$@"
+    }
+elif $PYTHON_BINARY -m hatch --version; then
     run_hatch() {
       $PYTHON_BINARY -m hatch run "$@"
     }
