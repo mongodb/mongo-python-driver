6193beb768492e4373451106a35c04c8093a3b8c
Steven Silvester <steve.silvester@mongodb.com>
2024-09-05 20:14:22 -0500

    undo test change

--- test/asynchronous/test_client_bulk_write.py
+++ test/asynchronous/test_client_bulk_write.py
@@ -616,8 +616,6 @@ class TestClientBulkWriteCSOT(AsyncIntegrationTest):
             await client.admin.command("ping")  # Init the client first.
             with self.assertRaises(ClientBulkWriteException) as context:
                 await client.bulk_write(models=models)
-            if not isinstance(context.exception.error, NetworkTimeout):
-                raise context.exception.error
             self.assertIsInstance(context.exception.error, NetworkTimeout)
 
         bulk_write_events = []
