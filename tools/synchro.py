95f894e250e2a6a78025e0f847ffc1e5497cbc2c
Iris <itsirisho@gmail.com>
2024-09-30 11:33:29 -0700

    forgot to add the new helpers file

--- tools/synchro.py
+++ tools/synchro.py
@@ -181,9 +181,9 @@ converted_tests = [
     "test_logger.py",
     "test_monitoring.py",
     "test_raw_bson.py",
+    "test_retryable_writes.py",
     "test_session.py",
     "test_transactions.py",
-    "test_retryable_writes.py",
 ]
 
 sync_test_files = [
