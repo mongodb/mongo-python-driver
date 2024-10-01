b631c5a3838478bf9d544a9d4b09fd1d8a71b6d8
Iris <itsirisho@gmail.com>
2024-10-01 13:35:50 -0700

    Migrate test_connections_survive_primary_stepdown_spec.py to async

--- tools/synchro.py
+++ tools/synchro.py
@@ -186,6 +186,7 @@ converted_tests = [
     "test_client_bulk_write.py",
     "test_client_context.py",
     "test_collection.py",
+    "test_connections_survive_primary_stepdown_spec.py",
     "test_cursor.py",
     "test_database.py",
     "test_encryption.py",
