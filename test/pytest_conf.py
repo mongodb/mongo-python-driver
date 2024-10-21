0b23fc53b7283f3472d478ac4af0eba10d64fb48
Steven Silvester <steve.silvester@mongodb.com>
2024-10-21 10:42:05 -0500

    spelling

--- test/pytest_conf.py
+++ test/pytest_conf.py
@@ -10,6 +10,6 @@ def pytest_collection_modifyitems(items, config):
             default_marker = "default_async"
         else:
             default_marker = "default"
-        markers = [m for m in item.iter_markes() if m not in overlap_markers]
+        markers = [m for m in item.iter_markers() if m not in overlap_markers]
         if not markers:
             item.add_marker(default_marker)
