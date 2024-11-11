40a99843b6e32b3d23b11e6f7865783f3c3158fc
Noah Stapp <noah.stapp@mongodb.com>
2024-11-11 15:43:18 -0500

    Add setup-tests.sh

--- .evergreen/scripts/run-with-env.sh
+++ .evergreen/scripts/run-with-env.sh
@@ -1,12 +1,18 @@
-#!/bin/bash
+#!/bin/bash -eu
 
 # Example use: bash run-with-env.sh run-tests.sh {args...}
 
 # Parameter expansion to get just the current directory's name
 if [ "${PWD##*/}" == "src" ]; then
   . .evergreen/scripts/env.sh
+  if [ -f ".evergreen/scripts/test-env.sh" ]; then
+    . .evergreen/scripts/test-env.sh
+  fi
 else
   . src/.evergreen/scripts/env.sh
+  if [ -f "src/.evergreen/scripts/test-env.sh" ]; then
+    . src/.evergreen/scripts/test-env.sh
+  fi
 fi
 
 set -eu
---
+++ .evergreen/scripts/setup-tests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -eux
+
+PROJECT_DIRECTORY="$(pwd)"
+SCRIPT_DIR="$PROJECT_DIRECTORY/.evergreen/scripts"
+
+if [ -f "$SCRIPT_DIR/test-env.sh" ]; then
+  echo "Reading $SCRIPT_DIR/test-env.sh file"
+  . "$SCRIPT_DIR/test-env.sh"
+  exit 0
+fi
+
+cat <<EOT > "$SCRIPT_DIR"/test-env.sh
+echo "Sourcing test-env.sh now, ORCHESTRATION_FILE: $ORCHESTRATION_FILE"
+export test_encryption="${test_encryption:-}"
+export test_encryption_pyopenssl="${test_encryption_pyopenssl:-}"
+export test_crypt_shared="${test_crypt_shared:-}"
+export test_pyopenssl="${test_pyopenssl:-}"
+export test_loadbalancer="${test_loadbalancer:-}"
+export test_serverless="${test_serverless:-}"
+export TEST_INDEX_MANAGEMENT="${TEST_INDEX_MANAGEMENT:-}"
+export TEST_DATA_LAKE="${TEST_DATA_LAKE:-}"
+export ORCHESTRATION_FILE="${ORCHESTRATION_FILE:-}"
+EOT
+
+chmod +x "$SCRIPT_DIR"/test-env.sh
