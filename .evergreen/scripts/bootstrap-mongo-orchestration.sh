7617ac656aee74e35cc0bf445e01713169225d83
Noah Stapp <noah.stapp@mongodb.com>
2024-11-12 12:32:47 -0500

    Fix unbound variables

--- .evergreen/scripts/bootstrap-mongo-orchestration.sh
+++ .evergreen/scripts/bootstrap-mongo-orchestration.sh
@@ -33,14 +33,14 @@ if [ -n "${skip_crypt_shared}" ]; then
     export SKIP_CRYPT_SHARED=1
 fi
 
-MONGODB_VERSION=${VERSION} \
-    TOPOLOGY=${TOPOLOGY} \
-    AUTH=${AUTH} \
-    SSL=${SSL} \
-    STORAGE_ENGINE=${STORAGE_ENGINE} \
-    DISABLE_TEST_COMMANDS=${DISABLE_TEST_COMMANDS} \
+MONGODB_VERSION=${VERSION:-} \
+    TOPOLOGY=${TOPOLOGY:-} \
+    AUTH=${AUTH:-noauth} \
+    SSL=${SSL:-nossl} \
+    STORAGE_ENGINE=${STORAGE_ENGINE:-} \
+    DISABLE_TEST_COMMANDS=${DISABLE_TEST_COMMANDS:-} \
     ORCHESTRATION_FILE=${ORCHESTRATION_FILE:-} \
-    REQUIRE_API_VERSION=${REQUIRE_API_VERSION} \
-    LOAD_BALANCER=${LOAD_BALANCER} \
+    REQUIRE_API_VERSION=${REQUIRE_API_VERSION:-} \
+    LOAD_BALANCER=${LOAD_BALANCER:-} \
     bash ${DRIVERS_TOOLS}/.evergreen/run-orchestration.sh
 # run-orchestration generates expansion file with the MONGODB_URI for the cluster
