6f74878a6db34a939d5e7034a01f6010e5b7e013
Noah Stapp <noah.stapp@mongodb.com>
2024-11-13 15:13:18 -0500

    Fix archive-logs

--- .evergreen/scripts/archive-mongodb-logs.sh
+++ .evergreen/scripts/archive-mongodb-logs.sh
@@ -2,6 +2,7 @@
 
 set -o xtrace
 mkdir out_dir
-find "$MONGO_ORCHESTRATION_HOME" -name \*.log -exec sh -c 'x=$1; mv $x $PWD/out_dir/$(basename $(dirname $x))_$(basename $x)' shell {} \;
+# shellcheck disable=SC2156
+find "$MONGO_ORCHESTRATION_HOME" -name \*.log -exec sh -c 'x="{}"; mv $x $PWD/out_dir/$(basename $(dirname $x))_$(basename $x)' \;
 tar zcvf mongodb-logs.tar.gz -C out_dir/ .
 rm -rf out_dir
