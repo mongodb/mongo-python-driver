ce1c49a668a2dce46c8faafaa3f4be9adfaf4b90
Steven Silvester <steven.silvester@ieee.org>
2024-12-03 14:29:23 -0600

    PYTHON-4646 Improve usage of hatch in evergreen (#2025)

--- .evergreen/hatch.sh
+++ .evergreen/hatch.sh
@@ -1,45 +1,5 @@
 #!/bin/bash
-set -o errexit  # Exit the script with error if any of the commands fail
-set -x
+set -eu
 
-. .evergreen/utils.sh
-
-if [ -z "$PYTHON_BINARY" ]; then
-    PYTHON_BINARY=$(find_python3)
-fi
-
-# Check if we should skip hatch and run the tests directly.
-if [ -n "$SKIP_HATCH" ]; then
-    ENV_NAME=testenv-$RANDOM
-    createvirtualenv "$PYTHON_BINARY" $ENV_NAME
-    # shellcheck disable=SC2064
-    trap "deactivate; rm -rf $ENV_NAME" EXIT HUP
-    python -m pip install -e ".[test]"
-    run_hatch() {
-      bash ./.evergreen/run-tests.sh
-    }
-else # Set up virtualenv before installing hatch
-    # Use a random venv name because the encryption tasks run this script multiple times in the same run.
-    ENV_NAME=hatchenv-$RANDOM
-    createvirtualenv "$PYTHON_BINARY" $ENV_NAME
-    # shellcheck disable=SC2064
-    trap "deactivate; rm -rf $ENV_NAME" EXIT HUP
-    python -m pip install -q hatch
-
-    # Ensure hatch does not write to user or global locations.
-    touch hatch_config.toml
-    HATCH_CONFIG=$(pwd)/hatch_config.toml
-    if [ "Windows_NT" = "${OS:-}" ]; then # Magic variable in cygwin
-        HATCH_CONFIG=$(cygpath -m "$HATCH_CONFIG")
-    fi
-    export HATCH_CONFIG
-    hatch config restore
-    hatch config set dirs.data "$(pwd)/.hatch/data"
-    hatch config set dirs.cache "$(pwd)/.hatch/cache"
-
-    run_hatch() {
-      python -m hatch run "$@"
-    }
-fi
-
-run_hatch "${@:1}"
+. .evergreen/scripts/ensure-hatch.sh
+hatch run "$@"
--- .evergreen/scripts/generate_config.py
+++ .evergreen/scripts/generate_config.py
@@ -744,7 +744,6 @@ def create_aws_auth_variants():
 
 
 def create_alternative_hosts_variants():
-    expansions = dict(SKIP_HATCH="true")
     batchtime = BATCHTIME_WEEK
     variants = []
 
@@ -752,11 +751,10 @@ def create_alternative_hosts_variants():
     variants.append(
         create_variant(
             [".5.0 .standalone !.sync_async"],
-            get_display_name("OpenSSL 1.0.2", host, python=CPYTHONS[0], **expansions),
+            get_display_name("OpenSSL 1.0.2", host, python=CPYTHONS[0]),
             host=host,
             python=CPYTHONS[0],
             batchtime=batchtime,
-            expansions=expansions,
         )
     )
 
@@ -765,8 +763,7 @@ def create_alternative_hosts_variants():
         variants.append(
             create_variant(
                 [".6.0 .standalone !.sync_async"],
-                display_name=get_display_name("Other hosts", host, **expansions),
-                expansions=expansions,
+                display_name=get_display_name("Other hosts", host),
                 batchtime=batchtime,
                 host=host,
             )
