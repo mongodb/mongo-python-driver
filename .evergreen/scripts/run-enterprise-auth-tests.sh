b74725ef3e64e334fc067bcb1ee46093829e0482
Steven Silvester <steve.silvester@mongodb.com>
2024-12-31 09:00:23 -0600

    clean up script

--- .evergreen/scripts/run-enterprise-auth-tests.sh
+++ .evergreen/scripts/run-enterprise-auth-tests.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -eu
 
 # Disable xtrace for security reasons (just in case it was accidentally set).
 set +x
 # Use the default python to bootstrap secrets.
-PYTHON_BINARY="" bash "${DRIVERS_TOOLS}"/.evergreen/auth_aws/setup_secrets.sh drivers/enterprise_auth
+bash "${DRIVERS_TOOLS}"/.evergreen/auth_aws/setup-secrets.sh drivers/enterprise_auth
 TEST_ENTERPRISE_AUTH=1 AUTH=auth bash "${PROJECT_DIRECTORY}"/.evergreen/hatch.sh test:test-eg
