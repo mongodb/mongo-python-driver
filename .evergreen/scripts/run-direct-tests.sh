4a30c05f5042df701e15682cd43f93d129fc4013
Steven Silvester <steve.silvester@mongodb.com>
2025-01-03 21:23:10 -0600

    more debug

--- .evergreen/scripts/run-direct-tests.sh
+++ .evergreen/scripts/run-direct-tests.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-
+set -x
 . .evergreen/utils.sh
 
 # . .evergreen/scripts/env.sh
-
+ls pymongo
 createvirtualenv "$PYTHON_BINARY" .venv
-
+ls pymongo
 # pip install -e ".[test]"
-set -x
+
 python --version
 # pip install pytest
 pip install -v -e .
+ls pymongo
 python -c "import pymongo"
 python -c "import bson"
 # pytest --version
