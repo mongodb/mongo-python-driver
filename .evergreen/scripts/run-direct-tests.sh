291c369cc81bf58f1a886af6bb41f9099925cc83
Steven Silvester <steve.silvester@mongodb.com>
2025-01-04 17:53:10 -0600

    more debug

--- .evergreen/scripts/run-direct-tests.sh
+++ .evergreen/scripts/run-direct-tests.sh
@@ -12,6 +12,7 @@ ulimit -c unlimited
 python --version
 # pip install pytest
 export PYMONGO_C_EXT_MUST_BUILD=1
+pip install setuptools
 python _setup.py build_ext -i
 pip install -v -e .
 ls pymongo
