8c67ca5058b1c03d6cf5564208752f84621614b0
Steven Silvester <steve.silvester@mongodb.com>
2025-01-06 07:19:15 -0600

    cleanup

--- .evergreen/scripts/run-direct-tests.sh
+++ .evergreen/scripts/run-direct-tests.sh
@@ -2,22 +2,9 @@
 set -x
 . .evergreen/utils.sh
 
-env
-# . .evergreen/scripts/env.sh
-ls pymongo
+. .evergreen/scripts/env.sh
 createvirtualenv "$PYTHON_BINARY" .venv
-ls pymongo
-# pip install -e ".[test]"
-ulimit -c unlimited
-python --version
-# pip install pytest
-export PYMONGO_C_EXT_MUST_BUILD=1
-pip install setuptools
-python _setup.py build_ext -i
-pip install -v -e .
-ls pymongo
-# python -c "import pymongo"
-python -c "import bson"
-# pytest --version
 
+export PYMONGO_C_EXT_MUST_BUILD=1
+pip install -e ".[test]"
 pytest -v
