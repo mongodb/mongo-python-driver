cbc2db80db94c2afbc9a51936a2d9260e5b69361
Steven Silvester <steve.silvester@mongodb.com>
2025-01-10 13:56:27 -0600

    PYTHON-4975 Use justfile as the task runner

--- .evergreen/scripts/ensure-hatch.sh
+++ .evergreen/scripts/ensure-hatch.sh
@@ -5,55 +5,57 @@ set -eu
 HERE=$(dirname ${BASH_SOURCE:-$0})
 pushd "$(dirname "$(dirname $HERE)")" > /dev/null
 
-# Ensure hatch is available.
-if [ ! -x "$(command -v hatch)" ]; then
-  # Install a virtual env with "hatch"
-  # Ensure there is a python venv.
-  . .evergreen/utils.sh
-
-  if [ -z "${PYTHON_BINARY:-}" ]; then
-      PYTHON_BINARY=$(find_python3)
-  fi
-  VENV_DIR=.venv
-  if [ ! -d $VENV_DIR ]; then
-    echo "Creating virtual environment..."
-    createvirtualenv "$PYTHON_BINARY" .venv
-    echo "Creating virtual environment... done."
-  fi
-  if [ -f $VENV_DIR/Scripts/activate ]; then
-    . $VENV_DIR/Scripts/activate
-  else
-    . $VENV_DIR/bin/activate
-  fi
-
-  python --version
-
-  echo "Installing hatch..."
-  python -m pip install -U pip
-  python -m pip install hatch || {
-    # Install rust and try again.
-    CARGO_HOME=${CARGO_HOME:-${DRIVERS_TOOLS}/.cargo}
-    # Handle paths on Windows.
-    if [ "Windows_NT" = "${OS:-}" ]; then # Magic variable in cygwin
-      CARGO_HOME=$(cygpath -m $CARGO_HOME)
-    fi
-    export RUSTUP_HOME="${CARGO_HOME}/.rustup"
-    ${DRIVERS_TOOLS}/.evergreen/install-rust.sh
-    source "${CARGO_HOME}/env"
-    python -m pip install hatch
-  }
-  # Ensure hatch does not write to user or global locations.
-  touch hatch_config.toml
-  HATCH_CONFIG=$(pwd)/hatch_config.toml
-  if [ "Windows_NT" = "${OS:-}" ]; then # Magic variable in cygwin
-      HATCH_CONFIG=$(cygpath -m "$HATCH_CONFIG")
-  fi
-  export HATCH_CONFIG
-  hatch config restore
-  hatch config set dirs.data "$(pwd)/.hatch/data"
-  hatch config set dirs.cache "$(pwd)/.hatch/cache"
-
-  echo "Installing hatch... done."
+# Ensure hatch is available locally.
+VENV_DIR=.venv
+if [ -f $VENV_DIR/Scripts/hatch ]; then
+  return 0
 fi
-hatch --version
-popd > /dev/null
+if [ -f $VENV_DIR/bin/hatch ]; then
+  return 0
+fi
+
+# Install a virtual env with "hatch"
+# Ensure there is a python venv.
+. .evergreen/utils.sh
+
+if [ -z "${PYTHON_BINARY:-}" ]; then
+    PYTHON_BINARY=$(find_python3)
+fi
+
+if [ ! -d $VENV_DIR ]; then
+  echo "Creating virtual environment..."
+  createvirtualenv "$PYTHON_BINARY" .venv
+  echo "Creating virtual environment... done."
+fi
+if [ -f $VENV_DIR/Scripts/activate ]; then
+  . $VENV_DIR/Scripts/activate
+else
+  . $VENV_DIR/bin/activate
+fi
+
+echo "Installing hatch..."
+python -m pip install -U pip
+python -m pip install hatch || {
+  # Install rust and try again.
+  CARGO_HOME=${CARGO_HOME:-${DRIVERS_TOOLS}/.cargo}
+  # Handle paths on Windows.
+  if [ "Windows_NT" = "${OS:-}" ]; then # Magic variable in cygwin
+    CARGO_HOME=$(cygpath -m $CARGO_HOME)
+  fi
+  export RUSTUP_HOME="${CARGO_HOME}/.rustup"
+  ${DRIVERS_TOOLS}/.evergreen/install-rust.sh
+  source "${CARGO_HOME}/env"
+  python -m pip install hatch
+}
+# Ensure hatch does not write to user or global locations.
+touch hatch_config.toml
+HATCH_CONFIG=$(pwd)/hatch_config.toml
+if [ "Windows_NT" = "${OS:-}" ]; then # Magic variable in cygwin
+    HATCH_CONFIG=$(cygpath -m "$HATCH_CONFIG")
+fi
+export HATCH_CONFIG
+hatch config restore
+hatch config set dirs.data "$(pwd)/.hatch/data"
+hatch config set dirs.cache "$(pwd)/.hatch/cache"
+
+echo "Installing hatch... done."
---
+++ justfile
@@ -0,0 +1,64 @@
+# See https://just.systems/man/en/ for instructions
+set shell := ["bash", "-c"]
+set dotenv-load
+set dotenv-filename := "./.evergreen/scripts/env.sh"
+
+# Make the default recipe private so it doesn't show up in the list.
+[private]
+default:
+  @just --list
+
+install:
+    bash .evergreen/scripts/ensure-hatch.sh
+
+[group('docs')]
+docs-build:
+    source .evergreen/scripts/ensure-hatch.sh && hatch run doc:build
+
+[group('docs')]
+docs-serve:
+    source .evergreen/scripts/ensure-hatch.sh && hatch run doc:serve
+
+[group('docs')]
+docs-linkcheck:
+    hatch run doc:linkcheck
+
+[group('docs')]
+docs-test:
+    source .evergreen/scripts/ensure-hatch.sh && hatch run doctest:test
+
+[group('typing')]
+typing:
+    source .evergreen/scripts/ensure-hatch.sh && hatch run typing:check
+
+[group('typing')]
+typing-mypy:
+    source .evergreen/scripts/ensure-hatch.sh && hatch run typing:mypy
+
+[group('lint')]
+lint:
+    pre-commit run --all-files
+
+[group('lint')]
+lint-manual:
+    pre-commit run --all-files --hook-stage manual
+
+[group('test')]
+test *args:
+    source .evergreen/scripts/ensure-hatch.sh && hatch run test:test {{args}}
+
+[group('test')]
+test-mockupdb:
+    source .evergreen/scripts/ensure-hatch.sh && hatch run test:test-mockupdb
+
+[group('test')]
+test-eg *args:
+    source .evergreen/scripts/ensure-hatch.sh && hatch run test:test-eg {{args}}
+
+[group('encryption')]
+setup-encryption:
+    bash .evergreen/setup-encryption.sh
+
+[group('encryption')]
+teardown-encryption:
+    bash .evergreen/teardown-encryption.sh
