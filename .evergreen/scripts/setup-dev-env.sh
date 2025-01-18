aee4e49311d512116c07250b93597ea7d91c6a0b
Steven Silvester <steve.silvester@mongodb.com>
2025-01-17 23:22:46 -0600

    clean up setup dev script

--- .evergreen/scripts/setup-dev-env.sh
+++ .evergreen/scripts/setup-dev-env.sh
@@ -6,19 +6,19 @@ HERE=$(dirname ${BASH_SOURCE:-$0})
 pushd "$(dirname "$(dirname $HERE)")" > /dev/null
 
 # Source the env file to pick up common variables.
-if [ -f $HERE/scripts/env.sh ]; then
-  source $HERE/scripts/env.sh
+if [ -f $HERE/env.sh ]; then
+  source $HERE/env.sh
 fi
 
 # Ensure that we have the correct base python binary.
-if [ -z "${UV_PYTHON}" ]; then
+if [ -z "${UV_PYTHON:-}" ]; then
   . .evergreen/utils.sh
 
   if [ -z "${PYTHON_BINARY:-}" ]; then
       PYTHON_BINARY=$(find_python3)
   fi
   export UV_PYTHON=${PYTHON_BINARY}
-  echo "export UV_PYTHON=$UV_PYTHON" >> $HERE/scripts/env.sh
+  echo "export UV_PYTHON=$UV_PYTHON" >> $HERE/env.sh
 fi
 
 # Set up the python environment.
