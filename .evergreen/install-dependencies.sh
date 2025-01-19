056d02c0680a0a47dc1ea367128bf98c91774f8f
Steven Silvester <steve.silvester@mongodb.com>
2025-01-19 10:50:01 -0600

    add files

--- .evergreen/install-dependencies.sh
+++ .evergreen/install-dependencies.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 set -eu
 
-# Copy PyMongo's test certificates over driver-evergreen-tools'
-cp ${PROJECT_DIRECTORY}/test/certificates/* ${DRIVERS_TOOLS}/.evergreen/x509gen/
-
-# Replace MongoOrchestration's client certificate.
-cp ${PROJECT_DIRECTORY}/test/certificates/client.pem ${MONGO_ORCHESTRATION_HOME}/lib/client.pem
-
-if [ -w /etc/hosts ]; then
-  SUDO=""
-else
-  SUDO="sudo"
-fi
-
 # Set where binaries are expected to be.
 # On Evergreen jobs, "CI" will be set, and we don't want to write to $HOME.
 if [ "${CI:-}" == "true" ]; then
@@ -64,7 +52,3 @@ if [ ! -f $BIN_DIR/uv ]; then
     fi
   fi
 fi
-
-# Add 'server' and 'hostname_not_in_cert' as a hostnames
-echo "127.0.0.1 server" | $SUDO tee -a /etc/hosts
-echo "127.0.0.1 hostname_not_in_cert" | $SUDO tee -a /etc/hosts
