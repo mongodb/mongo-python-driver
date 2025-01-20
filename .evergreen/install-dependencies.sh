8a621f228a28940efe8e868bcc661dc7dedf6add
Steven Silvester <steve.silvester@mongodb.com>
2025-01-20 08:19:54 -0600

    clean up install

--- .evergreen/install-dependencies.sh
+++ .evergreen/install-dependencies.sh
@@ -4,60 +4,50 @@ set -eu
 # Set where binaries are expected to be.
 # On Evergreen jobs, "CI" will be set, and we don't want to write to $HOME.
 if [ "${CI:-}" == "true" ]; then
-  BIN_DIR=$DRIVERS_TOOLS_BINARIES
+  _BIN_DIR=$DRIVERS_TOOLS_BINARIES
+# On Windows spawn hosts, $HOME/cli_bin is on the PATH.
+elif [ "Windows_NT" = "${OS:-}" ]; then
+  _BIN_DIR=$HOME/cli_bin
+# On local machines and Linux spawn hosts, we expect $HOME/.local/bin to be on the PATH.
 else
-  BIN_DIR=$HOME/.local/bin
+  _BIN_DIR=$HOME/.local/bin
 fi
+mkdir -p $_BIN_DIR 2>/dev/null || true
 
+function _pip_install() {
+  _HERE=$(dirname ${BASH_SOURCE:-$0})
+  . $_HERE/utils.sh
+  _VENV_PATH=$(mktemp -d)
+  createvirtualenv $(find_python3) $_VENV_PATH
+  python -m pip install $1
+  ln -s $(which $2) $_BIN_DIR/$2
+}
 
 _CURL_ARGS="--tlsv1.2 -LsSf"
-
-function _cargo_install() {
-  # CARGO_HOME is defined in configure-env.sh
-  export CARGO_HOME=${CARGO_HOME:-$HOME/.cargo/}
-  export RUSTUP_HOME="${CARGO_HOME}/.rustup"
-  . ${DRIVERS_TOOLS}/.evergreen/install-rust.sh
-  cargo install --force --quiet "$@"
-}
 
 # Install just.
 if ! command -v just 2>/dev/null; then
   if [ "Windows_NT" = "${OS:-}" ]; then
-    TARGET="--target x86_64-pc-windows-msvc"
+    _TARGET="--target x86_64-pc-windows-msvc"
   else
-    TARGET=""
+    _TARGET=""
   fi
-  curl $_CURL_ARGS https://just.systems/install.sh | bash -s -- $TARGET --to "$BIN_DIR" || {
-    _cargo_install just
-    if [ "Windows_NT" = "${OS:-}" ]; then
-      mv $CARGO_HOME/bin/just.exe $BIN_DIR/just
-    else
-      mv $CARGO_HOME/bin/just $BIN_DIR
-    fi
+  # On most systems we can install directly.
+  curl $_CURL_ARGS https://just.systems/install.sh | bash -s -- $_TARGET --to "$_BIN_DIR" || {
+    _pip_install rust-just just
   }
   if ! command -v just 2>/dev/null; then
-    export PATH="$PATH:$BIN_DIR"
+    export PATH="$PATH:$_BIN_DIR"
   fi
 fi
 
 # Install uv.
 if ! command -v uv 2>/dev/null; then
-  # On most non-Windows systems we can install directly.
-  if [ "Windows_NT" != "${OS:-}" ]; then
-    curl $_CURL_ARGS https://astral.sh/uv/install.sh | env UV_INSTALL_DIR="$BIN_DIR" INSTALLER_NO_MODIFY_PATH=1 sh || true
-  fi
-  # On Windows or unsupported systems, fall back to installing from cargo.
-  if [ ! -f $BIN_DIR/uv ]; then
-    _cargo_install --git https://github.com/astral-sh/uv uv
-    if [ "Windows_NT" = "${OS:-}" ]; then
-      mv $CARGO_HOME/bin/uv.exe $BIN_DIR/uv
-      mv $CARGO_HOME/bin/uvx.exe $BIN_DIR/uvx
-    else
-      mv $CARGO_HOME/bin/uv $BIN_DIR
-      mv $CARGO_HOME/bin/uvx $BIN_DIR
-    fi
-  fi
+  # On most systems we can install directly.
+  curl $_CURL_ARGS https://astral.sh/uv/install.sh | env UV_INSTALL_DIR="$_BIN_DIR" INSTALLER_NO_MODIFY_PATH=1 sh || {
+     _pip_install uv uv
+  }
   if ! command -v uv 2>/dev/null; then
-    export PATH="$PATH:$BIN_DIR"
+    export PATH="$PATH:$_BIN_DIR"
   fi
 fi
