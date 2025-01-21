bae10df99ac1f39ae5fcb97d6d5e1d1ff8ee29a4
Steven Silvester <steve.silvester@mongodb.com>
2025-01-21 09:46:25 -0600

    Address review

--- .evergreen/scripts/prepare-resources.sh
+++ .evergreen/scripts/prepare-resources.sh
@@ -12,10 +12,10 @@ echo "{ \"releases\": { \"default\": \"$MONGODB_BINARIES\" }}" >$MONGO_ORCHESTRA
 popd
 
 # # Copy PyMongo's test certificates over driver-evergreen-tools'
-# cp ${PROJECT_DIRECTORY}/test/certificates/* ${DRIVERS_TOOLS}/.evergreen/x509gen/
+cp ${PROJECT_DIRECTORY}/test/certificates/* ${DRIVERS_TOOLS}/.evergreen/x509gen/
 
 # # Replace MongoOrchestration's client certificate.
-# cp ${PROJECT_DIRECTORY}/test/certificates/client.pem ${MONGO_ORCHESTRATION_HOME}/lib/client.pem
+cp ${PROJECT_DIRECTORY}/test/certificates/client.pem ${MONGO_ORCHESTRATION_HOME}/lib/client.pem
 
 if [ -w /etc/hosts ]; then
   SUDO=""
