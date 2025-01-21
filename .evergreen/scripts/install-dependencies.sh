6b4fc2fcd89c82775adbbd3c1878b9f9389b8e66
Steven Silvester <steve.silvester@mongodb.com>
2025-01-21 12:44:06 -0600

    fix install-deps

--- .evergreen/scripts/install-dependencies.sh
+++ .evergreen/scripts/install-dependencies.sh
@@ -9,23 +9,29 @@ if [ "${CI:-}" == "true" ]; then
 else
   _BIN_DIR=$HOME/.local/bin
 fi
-if [ ! -f $_BIN_DIR/just ]; then
-  if [ "Windows_NT" = "${OS:-}" ]; then
-    _TARGET="--target x86_64-pc-windows-msvc"
-  else
-    _TARGET=""
-  fi
+
+
+function _pip_install() {
+  _HERE=$(dirname ${BASH_SOURCE:-$0})
+  . $_HERE/utils.sh
+  _VENV_PATH=$(mktemp -d)
+  echo "Installing $2 using pip..."
+  createvirtualenv "$(find_python3)" $_VENV_PATH
+  python -m pip install $1
+  ln -s "$(which $2)" $_BIN_DIR/$2
+  echo "Installing $2 using pip... done."
+}
+
+
+# Ensure just is installed.
+if ! command -v just 2>/dev/null; then
+  # On most systems we can install directly.
+  echo "Installing just..."
   curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- $_TARGET --to "$_BIN_DIR" || {
-    # CARGO_HOME is defined in configure-env.sh
-    export CARGO_HOME=${CARGO_HOME:-$HOME/.cargo/}
-    export RUSTUP_HOME="${CARGO_HOME}/.rustup"
-    . ${DRIVERS_TOOLS}/.evergreen/install-rust.sh
-    cargo install just
-    if [ "Windows_NT" = "${OS:-}" ]; then
-      mv $CARGO_HOME/just.exe $_BIN_DIR/just
-    else
-      mv $CARGO_HOME/just $_BIN_DIR
-    fi
-    export PATH="$_BIN_DIR:$PATH"
+    _pip_install rust-just just
   }
+  if ! command -v just 2>/dev/null; then
+    export PATH="$PATH:$_BIN_DIR"
+  fi
+  echo "Installing just... done."
 fi
