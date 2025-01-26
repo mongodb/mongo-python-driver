d8ecde98d2a7f9642611e1530f2c3abe620df020
Steven Silvester <steve.silvester@mongodb.com>
2025-01-26 06:08:51 -0600

    fix kms logic

--- .evergreen/scripts/run-gcpkms-fail-test.sh
+++ .evergreen/scripts/run-gcpkms-fail-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 HERE=$(dirname ${BASH_SOURCE:-$0})
-. .$HERE/env.sh
+. $HERE/env.sh
 export PYTHON_BINARY=/opt/mongodbtoolchain/v4/bin/python3
 export LIBMONGOCRYPT_URL=https://s3.amazonaws.com/mciuploads/libmongocrypt/debian11/master/latest/libmongocrypt.tar.gz
 SUCCESS=false TEST_FLE_GCP_AUTO=1 bash $HERE/scripts/setup-tests.sh
