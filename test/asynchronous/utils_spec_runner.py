751ccae769d6a951e2f7b8511f0068ab7b8a5911
Shane Harvey <shnhrv@gmail.com>
2025-01-31 10:08:28 -0800

    PYTHON-5120 Reduce configureFailPoint duplication in tests

--- test/asynchronous/utils_spec_runner.py
+++ test/asynchronous/utils_spec_runner.py
@@ -265,15 +265,10 @@ class AsyncSpecRunner(AsyncIntegrationTest):
     async def asyncTearDown(self) -> None:
         self.knobs.disable()
 
-    async def _set_fail_point(self, client, command_args):
-        cmd = SON([("configureFailPoint", "failCommand")])
-        cmd.update(command_args)
-        await client.admin.command(cmd)
-
     async def set_fail_point(self, command_args):
         clients = self.mongos_clients if self.mongos_clients else [self.client]
         for client in clients:
-            await self._set_fail_point(client, command_args)
+            await self.configure_fail_point(client, command_args)
 
     async def targeted_fail_point(self, session, fail_point):
         """Run the targetedFailPoint test operation.
@@ -282,7 +277,7 @@ class AsyncSpecRunner(AsyncIntegrationTest):
         """
         clients = {c.address: c for c in self.mongos_clients}
         client = clients[session._pinned_address]
-        await self._set_fail_point(client, fail_point)
+        await self.configure_fail_point(client, fail_point)
         self.addAsyncCleanup(self.set_fail_point, {"mode": "off"})
 
     def assert_session_pinned(self, session):
