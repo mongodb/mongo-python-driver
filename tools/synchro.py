6b141d1f5bb2fb9a301932ce45870b21ddd8ea21
Noah Stapp <noah.stapp@mongodb.com>
2025-01-31 15:51:00 -0500

    PYTHON-5096 - Convert test.test_retryable_reads_unified to async (#2112)

--- tools/synchro.py
+++ tools/synchro.py
@@ -221,6 +221,7 @@ converted_tests = [
     "test_raw_bson.py",
     "test_read_concern.py",
     "test_retryable_reads.py",
+    "test_retryable_reads_unified.py",
     "test_retryable_writes.py",
     "test_retryable_writes_unified.py",
     "test_session.py",
