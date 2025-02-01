3bc040b89c8f6bf83b516b90ccf913d290205a29
Iris Ho <iris.ho@mongodb.com>
2025-01-31 20:03:01 -0800

    Convert test.test_sessions_unified to async

--- tools/synchro.py
+++ tools/synchro.py
@@ -225,6 +225,7 @@ converted_tests = [
     "test_retryable_writes.py",
     "test_retryable_writes_unified.py",
     "test_session.py",
+    "test_session_unified.py",
     "test_transactions.py",
     "unified_format.py",
 ]
