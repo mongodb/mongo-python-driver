c71aca47ad82d67dc7adf877e7fbe80104990a10
Iris Ho <iris.ho@mongodb.com>
2025-01-31 18:38:49 -0800

    Convert test.test_sdam_monitoring_spec to async

--- tools/synchro.py
+++ tools/synchro.py
@@ -224,6 +224,7 @@ converted_tests = [
     "test_retryable_reads_unified.py",
     "test_retryable_writes.py",
     "test_retryable_writes_unified.py",
+    "test_sdam_monitoring_spec.py",
     "test_session.py",
     "test_transactions.py",
     "unified_format.py",
