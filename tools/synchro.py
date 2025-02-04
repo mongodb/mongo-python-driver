68237f78ecda3399518c3700b3593b028eb9eeef
Noah Stapp <noah.stapp@mongodb.com>
2025-02-04 09:42:21 -0500

    PYTHON-5098 - Convert test.test_run_command to async (#2114)

--- tools/synchro.py
+++ tools/synchro.py
@@ -226,6 +226,7 @@ converted_tests = [
     "test_retryable_reads_unified.py",
     "test_retryable_writes.py",
     "test_retryable_writes_unified.py",
+    "test_run_command.py",
     "test_session.py",
     "test_srv_polling.py",
     "test_transactions.py",
