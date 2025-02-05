ac8fa2d645eaa22dd6346320bfee039294139dbe
Noah Stapp <noah.stapp@mongodb.com>
2025-02-05 12:13:56 -0500

    PYTHON-5094 - Convert test.test_read_preferences to async (#2110)

--- tools/synchro.py
+++ tools/synchro.py
@@ -222,6 +222,7 @@ converted_tests = [
     "test_on_demand_csfle.py",
     "test_raw_bson.py",
     "test_read_concern.py",
+    "test_read_preferences.py",
     "test_read_write_concern_spec.py",
     "test_retryable_reads.py",
     "test_retryable_reads_unified.py",
