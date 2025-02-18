6e76bd093c08286f2433c6459db7ad2d80487ecc
Steven Silvester <steve.silvester@mongodb.com>
2025-02-18 10:45:20 -0600

    do not run again for ecs

--- .evergreen/scripts/run-mongodb-aws-test.sh
+++ .evergreen/scripts/run-mongodb-aws-test.sh
@@ -23,5 +23,8 @@ echo "Running MONGODB-AWS authentication tests for $1"
 
 # Handle credentials and environment setup.
 . "$DRIVERS_TOOLS"/.evergreen/auth_aws/aws_setup.sh "$1"
-TEST_AUTH_AWS=1 AUTH="auth" bash ./.evergreen/just.sh setup-test
-bash ./.evergreen/just.sh test-eg
+
+if [ "$1" != "ecs" ]; then
+   TEST_AUTH_AWS=1 AUTH="auth" bash ./.evergreen/just.sh setup-test
+   bash ./.evergreen/just.sh test-eg
+fi
