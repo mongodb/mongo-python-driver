60c6745b57946e84b9294229e703f0964c3aa667
Steven Silvester <steve.silvester@mongodb.com>
2025-02-25 07:47:07 -0600

    fix typo

--- .evergreen/scripts/run-atlas-tests.sh
+++ .evergreen/scripts/run-atlas-tests.sh
@@ -5,4 +5,4 @@ set +x
 set -o errexit
 bash "${DRIVERS_TOOLS}"/.evergreen/auth_aws/setup_secrets.sh drivers/atlas_connect
 bash "${PROJECT_DIRECTORY}"/.evergreen/just.sh setup-tests atlas
-bash "${PROJECT_DIRECTORY}"/.evergreen/just.sh reun-tests
+bash "${PROJECT_DIRECTORY}"/.evergreen/just.sh run-tests
