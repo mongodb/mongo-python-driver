fc9db37491a8f02c9babe7c5454b1900dd0a4c6b
Steven Silvester <steve.silvester@mongodb.com>
2025-02-25 16:08:39 -0600

    try with normalized paths

--- .evergreen/scripts/bootstrap-mongo-orchestration.sh
+++ .evergreen/scripts/bootstrap-mongo-orchestration.sh
@@ -6,10 +6,6 @@ HERE=$(dirname ${BASH_SOURCE:-$0})
 HERE="$( cd -- "$HERE" > /dev/null 2>&1 && pwd )"
 ROOT=$(dirname "$(dirname $HERE)")
 
-if [ "Windows_NT" = "${OS:-}" ]; then
-    ROOT=$(cygpath -m $ROOT)
-fi
-
 if [ -z "${TEST_CRYPT_SHARED:-}" ]; then
     export SKIP_CRYPT_SHARED=1
 fi
