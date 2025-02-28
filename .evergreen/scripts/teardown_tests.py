ab1772ffd859ad49a16ab2b9790cd66851da4f36
Steven Silvester <steve.silvester@mongodb.com>
2025-02-28 05:52:05 -0600

    handle macos

--- .evergreen/scripts/teardown_tests.py
+++ .evergreen/scripts/teardown_tests.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import os
+import sys
 
 from utils import DRIVERS_TOOLS, LOGGER, run_command
 
@@ -24,7 +25,9 @@ elif TEST_NAME == "kms" and SUB_TEST_NAME in ["azure", "gcp"]:
     teardown_kms(SUB_TEST_NAME)
 
 # Tear down auth_aws if applicable.
-elif TEST_NAME == "auth_aws":
+# We do not run web-identity hosts on macos, because the hosts lack permissions,
+# so there is no reason to run the teardown, which would error with a 401.
+elif TEST_NAME == "auth_aws" and sys.platform != "darwin":
     run_command(f"bash {DRIVERS_TOOLS}/.evergreen/auth_aws/teardown.sh")
 
 LOGGER.info(f"Tearing down tests of type '{TEST_NAME}'... done.")
