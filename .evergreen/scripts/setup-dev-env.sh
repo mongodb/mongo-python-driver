4de9385e9069e4ed699edf5d8e39a201529e47b8
Steven Silvester <steve.silvester@mongodb.com>
2025-03-04 07:50:01 -0600

    PYTHON-5183 Fix C Extension building for Windows spawn hosts

--- .evergreen/scripts/setup-dev-env.sh
+++ .evergreen/scripts/setup-dev-env.sh
@@ -43,8 +43,18 @@ if [ -z "${PYMONGO_BIN_DIR:-}" ]; then
   export PATH="$PATH:$HOME/.local/bin"
 fi
 
+# Set up venv, making sure c extensions build unless disabled.
+if [ -z "${NO_EXT:-}" ]; then
+  export PYMONGO_C_EXT_MUST_BUILD=1
+fi
+# Set up visual studio env on Windows spawn hosts.
+if [ -f $HOME/.visualStudioEnv.sh ]; then
+  set +u
+  SSH_TTY=1 source $HOME/.visualStudioEnv.sh
+  set -u
+fi
 uv sync --frozen
-uv run --frozen --with pip pip install -e .
+
 echo "Setting up python environment... done."
 
 # Ensure there is a pre-commit hook if there is a git checkout.
