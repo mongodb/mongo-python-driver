08abb8b439af195db9ab43c6eea9532d1c17de25
Steven Silvester <steve.silvester@mongodb.com>
2025-03-11 11:13:58 -0500

    add files

--- .evergreen/run-mongodb-oidc-test.sh
+++ .evergreen/run-mongodb-oidc-test.sh
@@ -5,7 +5,7 @@ set -eu
 
 echo "Running MONGODB-OIDC authentication tests on ${OIDC_ENV}..."
 
-bash ./.evergreen/just.sh setup-tests auth_oidc ${OIDC_ENV}-remote
+bash ./.evergreen/just.sh setup-tests auth_oidc $K8S_VARIANT-remote
 bash ./.evergreen/just.sh run-tests "${@:1}"
 
 echo "Running MONGODB-OIDC authentication tests on ${OIDC_ENV}... done."
