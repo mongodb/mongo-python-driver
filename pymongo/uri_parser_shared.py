d616135b243b0851745ec858b396339a6cb18185
Iris Ho <iris.ho@mongodb.com>
2025-03-18 08:45:26 -0700

    update link to use https based on prev commit on main

--- pymongo/uri_parser_shared.py
+++ pymongo/uri_parser_shared.py
@@ -4,7 +4,7 @@
 # may not use this file except in compliance with the License.  You
 # may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+# https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -163,7 +163,7 @@ def parse_host(entity: str, default_port: Optional[int] = DEFAULT_PORT) -> _Addr
         port = int(port)
 
     # Normalize hostname to lowercase, since DNS is case-insensitive:
-    # http://tools.ietf.org/html/rfc4343
+    # https://tools.ietf.org/html/rfc4343
     # This prevents useless rediscovery if "foo.com" is in the seed list but
     # "FOO.com" is in the hello response.
     return host.lower(), port
