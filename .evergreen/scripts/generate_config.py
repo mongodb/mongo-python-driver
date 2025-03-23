c26cc66415f9e5d549aafb1ec3aba07e19e409f7
Steven Silvester <steve.silvester@mongodb.com>
2025-03-23 10:59:46 -0500

    test amazon2

--- .evergreen/scripts/generate_config.py
+++ .evergreen/scripts/generate_config.py
@@ -70,7 +70,7 @@ HOSTS["ubuntu20"] = Host("ubuntu20", "ubuntu2004-small", "Ubuntu-20", dict())
 HOSTS["ubuntu22"] = Host("ubuntu22", "ubuntu2204-small", "Ubuntu-22", dict())
 HOSTS["rhel7"] = Host("rhel7", "rhel79-small", "RHEL7", dict())
 HOSTS["perf"] = Host("perf", "rhel90-dbx-perf-large", "", dict())
-HOSTS["amazon2"] = Host("amazon2", "amazon2-arm64-latest-large-m8g", dict())
+HOSTS["amazon2"] = Host("amazon2", "amazon2-arm64-latest-large-m8g", "Amazon2", dict())
 DEFAULT_HOST = HOSTS["rhel8"]
 DEFAULT_HOST = HOSTS["amazon2"]
 
@@ -143,6 +143,8 @@ def create_variant(
 def get_python_binary(python: str, host: Host) -> str:
     """Get the appropriate python binary given a python version and host."""
     name = host.name
+    if name == "amazon2":
+        return "/opt/mongodbtoolchain/v4/bin/python3"
     if name in ["win64", "win32"]:
         if name == "win32":
             base = "C:/python/32"
