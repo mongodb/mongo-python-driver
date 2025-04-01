d482538f47d8495d81b1a283b8fbfda2df7f10f4
Steven Silvester <steve.silvester@mongodb.com>
2025-04-01 14:49:03 -0500

    increase timeout

--- test/ocsp/test_ocsp.py
+++ test/ocsp/test_ocsp.py
@@ -41,7 +41,7 @@ logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 
 
 def _connect(options):
-    uri = f"mongodb://localhost:27017/?serverSelectionTimeoutMS=5000&tlsCAFile={Path(CA_FILE).as_posix()}&{options}"
+    uri = f"mongodb://localhost:27017/?serverSelectionTimeoutMS=10000&tlsCAFile={Path(CA_FILE).as_posix()}&{options}"
     print(uri)
     try:
         client = pymongo.MongoClient(uri)
