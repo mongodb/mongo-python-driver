04ebbcde875e5fab4b39a31e1d7a0a6daf6c3027
Noah Stapp <noah.stapp@mongodb.com>
2025-04-02 08:22:25 -0400

    PYTHON-5259 - Better test assertions for error substrings (#2253)

--- test/asynchronous/test_pooling.py
+++ test/asynchronous/test_pooling.py
@@ -450,7 +450,7 @@ class TestPooling(_TestPoolingBase):
                 with timeout(0.5):
                     await client.db.t.find_one({"$where": delay(2)})
 
-        self.assertTrue("(configured timeouts: timeoutMS: 500.0ms" in str(error.exception))
+        self.assertIn("(configured timeouts: timeoutMS: 500.0ms", str(error.exception))
 
     @async_client_context.require_failCommand_appName
     async def test_socket_timeout_message(self):
@@ -475,9 +475,9 @@ class TestPooling(_TestPoolingBase):
             with self.assertRaises(Exception) as error:
                 await client.db.t.find_one({"$where": delay(2)})
 
-        self.assertTrue(
-            "(configured timeouts: socketTimeoutMS: 500.0ms, connectTimeoutMS: 20000.0ms)"
-            in str(error.exception)
+        self.assertIn(
+            "(configured timeouts: socketTimeoutMS: 500.0ms, connectTimeoutMS: 20000.0ms)",
+            str(error.exception),
         )
 
     @async_client_context.require_failCommand_appName
@@ -507,9 +507,9 @@ class TestPooling(_TestPoolingBase):
             with self.assertRaises(Exception) as error:
                 await client.admin.command("ping")
 
-        self.assertTrue(
-            "(configured timeouts: socketTimeoutMS: 500.0ms, connectTimeoutMS: 500.0ms)"
-            in str(error.exception)
+        self.assertIn(
+            "(configured timeouts: socketTimeoutMS: 500.0ms, connectTimeoutMS: 500.0ms)",
+            str(error.exception),
         )
 
 
