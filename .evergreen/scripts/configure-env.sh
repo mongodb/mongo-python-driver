3ea40d310214887d864b07333afbc63bfeb7f1ac
Steven Silvester <steve.silvester@mongodb.com>
2025-04-02 08:24:14 -0500

    use upstream

--- .evergreen/scripts/configure-env.sh
+++ .evergreen/scripts/configure-env.sh
@@ -76,8 +76,6 @@ EOT
 rm -rf $DRIVERS_TOOLS
 BRANCH=master
 ORG=mongodb-labs
-BRANCH=ocsp-cleanup
-ORG=blink1073
 git clone --branch $BRANCH https://github.com/$ORG/drivers-evergreen-tools.git $DRIVERS_TOOLS
 
 cat <<EOT > ${DRIVERS_TOOLS}/.env
