c2bc931d5ddd5aa1ad51c22b14dfdf458f1cc0e2
Jeffrey A. Clark <aclark@aclark.net>
2025-04-09 13:16:22 -0400

    Review fixes

--- test/test_dns.py
+++ test/test_dns.py
@@ -218,12 +218,15 @@ class TestInitialDnsSeedlistDiscovery(PyMongoTestCase):
                 mock_resolver.side_effect = mock_resolve
                 domain = case["query"].split("._tcp.")[1]
                 connection_string = f"mongodb+srv://{domain}"
-                try:
+                if "expected_error" not in case:
                     parse_uri(connection_string)
-                except ConfigurationError as e:
-                    self.assertIn(case["expected_error"], str(e))
                 else:
-                    self.fail(f"ConfigurationError was not raised for query: {case['query']}")
+                    try:
+                        parse_uri(connection_string)
+                    except ConfigurationError as e:
+                        self.assertIn(case["expected_error"], str(e))
+                    else:
+                        self.fail(f"ConfigurationError was not raised for query: {case['query']}")
 
     def test_1_allow_srv_hosts_with_fewer_than_three_dot_separated_parts(self):
         with patch("dns.resolver.resolve"):
@@ -261,6 +264,12 @@ class TestInitialDnsSeedlistDiscovery(PyMongoTestCase):
                 "query": "_mongodb._tcp.mongo.local",
                 "mock_target": "mongo.local",
                 "expected_error": "Invalid SRV host",
+            },
+            # When the SRV hostname has three or more dot-separated parts
+            # it is valid for the returned hostnames to be identical.
+            {
+                "query": "_mongodb._tcp.blogs.mongodb.com",
+                "mock_target": "blogs.mongodb.com",
             },
         ]
         self.run_initial_dns_seedlist_discovery_prose_tests(test_cases)
