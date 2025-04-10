5b42ed8cacc1f5af6cc2e4d99364256e7a2df51b
Steven Silvester <steven.silvester@ieee.org>
2025-04-10 09:31:50 -0500

    PYTHON-5286 Create server version variants (#2270)

--- .evergreen/scripts/generate_config_utils.py
+++ .evergreen/scripts/generate_config_utils.py
@@ -40,8 +40,11 @@ SYNCS = ["sync", "async", "sync_async"]
 DISPLAY_LOOKUP = dict(
     ssl=dict(ssl="SSL", nossl="NoSSL"),
     auth=dict(auth="Auth", noauth="NoAuth"),
+    topology=dict(
+        standalone="Standalone", replica_set="Replica Set", sharded_cluster="Sharded Cluster"
+    ),
     test_suites=dict(default="Sync", default_async="Async"),
-    coverage=dict(coverage="cov"),
+    coverage={"1": "cov"},
     no_ext={"1": "No C"},
 )
 HOSTS = dict()
