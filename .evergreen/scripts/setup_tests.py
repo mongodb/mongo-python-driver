27bffc9f8d2f38d14a1833947125d001518ae75d
Noah Stapp <noah.stapp@mongodb.com>
2025-04-16 11:27:50 -0400

    PYTHON-5324 - Fix Windows encryption test secrets path

--- .evergreen/scripts/setup_tests.py
+++ .evergreen/scripts/setup_tests.py
@@ -387,9 +387,9 @@ def handle_test_env() -> None:
         if not DRIVERS_TOOLS:
             raise RuntimeError("Missing DRIVERS_TOOLS")
         csfle_dir = Path(f"{DRIVERS_TOOLS}/.evergreen/csfle")
-        run_command(f"bash {csfle_dir}/setup-secrets.sh", cwd=csfle_dir)
+        run_command(f"bash {csfle_dir.as_posix()}/setup-secrets.sh", cwd=csfle_dir)
         load_config_from_file(csfle_dir / "secrets-export.sh")
-        run_command(f"bash {csfle_dir}/start-servers.sh")
+        run_command(f"bash {csfle_dir.as_posix()}/start-servers.sh")
 
         if sub_test_name == "pyopenssl":
             UV_ARGS.append("--extra ocsp")
