5c575b4c2402bf6c5f3c998c29ff5432fada850a
Steven Silvester <steve.silvester@mongodb.com>
2025-04-21 12:23:39 -0500

    test with fix-arch-handling

--- .evergreen/scripts/configure-env.sh
+++ .evergreen/scripts/configure-env.sh
@@ -61,8 +61,8 @@ EOT
 
 # Write the .env file for drivers-tools.
 rm -rf $DRIVERS_TOOLS
-BRANCH=master
-ORG=mongodb-labs
+BRANCH=fix-arch-handling
+ORG=blink1073
 git clone --branch $BRANCH https://github.com/$ORG/drivers-evergreen-tools.git $DRIVERS_TOOLS
 
 cat <<EOT > ${DRIVERS_TOOLS}/.env
--- .evergreen/scripts/run-server.sh
+++ .evergreen/scripts/run-server.sh
@@ -10,16 +10,5 @@ if [ -f $HERE/env.sh ]; then
   source $HERE/env.sh
 fi
 
-set -x
-ls /Library/Frameworks/Python.Framework
-ls /Library/Frameworks/Python.Framework/Versions
-/Library/Frameworks/Python.Framework/Versions/3.9/bin/python3 -c "import platform;print(platform.machine())"
-/Library/Frameworks/Python.Framework/Versions/3.9/bin/python3 -c "import platform;print(platform.processor())"
-exit 1
-${DRIVERS_TOOLS}/.evergreen/mongodl --out mongodb-bin --strip-path-components 2
-
-./mongodb-bin/mongod --version
-exit 1
-
 UV=${UV_BINARY:-uv}
 $UV run $HERE/run_server.py "$@"
