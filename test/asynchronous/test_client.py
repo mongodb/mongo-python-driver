42317c7cf89cd3d95a8576260efc4f04d8238593
Noah Stapp <noah.stapp@mongodb.com>
2025-04-23 16:32:39 -0400

    PYTHON-5346 - test_init_disconnected_with_srv cannot run against shar… (#2304)

--- test/asynchronous/test_client.py
+++ test/asynchronous/test_client.py
@@ -849,6 +849,58 @@ class TestClient(AsyncIntegrationTest):
         with self.assertRaises(ConnectionFailure):
             await c.pymongo_test.test.find_one()
 
+    @async_client_context.require_replica_set
+    @async_client_context.require_no_load_balancer
+    @async_client_context.require_tls
+    async def test_init_disconnected_with_srv(self):
+        c = await self.async_rs_or_single_client(
+            "mongodb+srv://test1.test.build.10gen.cc", connect=False, tlsInsecure=True
+        )
+        # nodes returns an empty set if not connected
+        self.assertEqual(c.nodes, frozenset())
+        # topology_description returns the initial seed description if not connected
+        topology_description = c.topology_description
+        self.assertEqual(topology_description.topology_type, TOPOLOGY_TYPE.Unknown)
+        self.assertEqual(
+            {
+                ("test1.test.build.10gen.cc", None): ServerDescription(
+                    ("test1.test.build.10gen.cc", None)
+                )
+            },
+            topology_description.server_descriptions(),
+        )
+
+        # address causes client to block until connected
+        self.assertIsNotNone(await c.address)
+        # Initial seed topology and connected topology have the same ID
+        self.assertEqual(
+            c._topology._topology_id, topology_description._topology_settings._topology_id
+        )
+        await c.close()
+
+        c = await self.async_rs_or_single_client(
+            "mongodb+srv://test1.test.build.10gen.cc", connect=False, tlsInsecure=True
+        )
+        # primary causes client to block until connected
+        await c.primary
+        self.assertIsNotNone(c._topology)
+        await c.close()
+
+        c = await self.async_rs_or_single_client(
+            "mongodb+srv://test1.test.build.10gen.cc", connect=False, tlsInsecure=True
+        )
+        # secondaries causes client to block until connected
+        await c.secondaries
+        self.assertIsNotNone(c._topology)
+        await c.close()
+
+        c = await self.async_rs_or_single_client(
+            "mongodb+srv://test1.test.build.10gen.cc", connect=False, tlsInsecure=True
+        )
+        # arbiters causes client to block until connected
+        await c.arbiters
+        self.assertIsNotNone(c._topology)
+
     async def test_equality(self):
         seed = "{}:{}".format(*list(self.client._topology_settings.seeds)[0])
         c = await self.async_rs_or_single_client(seed, connect=False)
