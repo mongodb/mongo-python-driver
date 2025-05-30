3e24168ca18be9d30d8f09f4b34a49e717c2acc3
Steven Silvester <steve.silvester@mongodb.com>
2025-05-30 16:25:04 -0500

    use python 3.11

--- .evergreen/scripts/run-aws-container-test.sh
+++ .evergreen/scripts/run-aws-container-test.sh
@@ -20,7 +20,8 @@ fi
 set -o xtrace
 
 # Install python with pip.
-PYTHON_VER="python3.9"
+PYTHON_VER="python3.11"
+add-apt-repository -qq ppa:deadsnakes/ppa -y
 apt-get -qq update  < /dev/null > /dev/null
 apt-get -qq install $PYTHON_VER $PYTHON_VER-venv build-essential $PYTHON_VER-dev -y  < /dev/null > /dev/null
 
