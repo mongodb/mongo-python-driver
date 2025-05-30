bac627717b411f5a0eec07edf2703a3c6fb623ce
Steven Silvester <steve.silvester@mongodb.com>
2025-05-30 16:50:03 -0500

    cleanup

--- .evergreen/scripts/generate_config.py
+++ .evergreen/scripts/generate_config.py
@@ -743,39 +743,34 @@ def create_aws_tasks():
         "env-creds",
         "session-creds",
         "web-identity",
+        "web-identity-session-name",
         "ecs",
-        "eks",
     ]
+    assume_func = FunctionCall(func="assume ec2 role")
     for version, test_type, python in zip_cycle(get_versions_from("4.4"), aws_test_types, CPYTHONS):
         base_name = f"test-auth-aws-{version}"
         base_tags = ["auth-aws"]
         server_vars = dict(AUTH_AWS="1", VERSION=version)
         server_func = FunctionCall(func="run server", vars=server_vars)
-        assume_func = FunctionCall(func="assume ec2 role")
-        tags = [*base_tags, f"auth-aws-{test_type}"]
-        if test_type in ["eks", "ecs"]:
+        if test_type == "ecs":
             python = None  # noqa:PLW2901
-            base_name = "test-auth-aws"
         name = get_task_name(f"{base_name}-{test_type}", python=python)
         test_vars = dict(TEST_NAME="auth_aws", SUB_TEST_NAME=test_type, PYTHON_VERSION=python)
+        if test_type == "web-identity-session-name":
+            test_type = "web-identity"  # noqa:PLW2901
+            test_vars["AWS_ROLE_SESSION_NAME"] = "test"
+        tags = [*base_tags, f"auth-aws-{test_type}"]
         test_func = FunctionCall(func="run tests", vars=test_vars)
         funcs = [server_func, assume_func, test_func]
-        if test_type == "eks":
-            funcs = [assume_func, test_func]
         tasks.append(EvgTask(name=name, tags=tags, commands=funcs))
 
-        if test_type == "web-identity":
-            tags = [*base_tags, "auth-aws-web-identity"]
-            name = get_task_name(f"{base_name}-web-identity-session-name", python=python)
-            test_vars = dict(
-                TEST_NAME="auth_aws",
-                SUB_TEST_NAME="web-identity",
-                AWS_ROLE_SESSION_NAME="test",
-                PYTHON_VERSION=python,
-            )
-            test_func = FunctionCall(func="run tests", vars=test_vars)
-            funcs = [server_func, assume_func, test_func]
-            tasks.append(EvgTask(name=name, tags=tags, commands=funcs))
+    # The EKS test does not start a server
+    tags = ["auth-aws", "auth-aws-eks"]
+    name = get_task_name("test-auth-aws-eks")
+    test_vars = dict(TEST_NAME="auth_aws", SUB_TEST_NAME="eks")
+    test_func = FunctionCall(func="run tests", vars=test_vars)
+    funcs = [assume_func, test_func]
+    tasks.append(EvgTask(name=name, tags=tags, commands=funcs))
 
     return tasks
 
