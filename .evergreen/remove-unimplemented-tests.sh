83fcf7cd084f6095c0be394b4f464ffa817fe321
Iris <58442094+sleepyStick@users.noreply.github.com>
2025-07-15 12:15:05 -0700

    PYTHON-4931 Add spec tests for GridFS rename (#2431)

--- .evergreen/remove-unimplemented-tests.sh
+++ .evergreen/remove-unimplemented-tests.sh
@@ -8,7 +8,6 @@ rm $PYMONGO/test/client-side-encryption/spec/unified/fle2v2-EncryptedFields-vs-E
 rm $PYMONGO/test/client-side-encryption/spec/unified/localSchema.json  # PYTHON-5143
 rm $PYMONGO/test/client-side-encryption/spec/unified/maxWireVersion.json  # PYTHON-5143
 rm $PYMONGO/test/unified-test-format/valid-pass/poc-queryable-encryption.json  # PYTHON-5143
-rm $PYMONGO/test/gridfs/rename.json  # PYTHON-4931
 rm $PYMONGO/test/discovery_and_monitoring/unified/pool-clear-application-error.json  # PYTHON-4918
 rm $PYMONGO/test/discovery_and_monitoring/unified/pool-clear-checkout-error.json  # PYTHON-4918
 rm $PYMONGO/test/discovery_and_monitoring/unified/pool-clear-min-pool-size-error.json  # PYTHON-4918
