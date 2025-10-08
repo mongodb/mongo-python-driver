39517b1efb7739c70a0c5b4aa657d6d668bd1ec5
Steven Silvester <steve.silvester@mongodb.com>
2025-10-08 07:41:28 -0500

    cleanup

--- .evergreen/just.sh
+++ .evergreen/just.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 set -eux
 
-bash .evergreen/scripts/setup-uv-python.sh
-. .evergreen/scripts/setup-dev-env.sh
+HERE=$(dirname ${BASH_SOURCE:-$0})
+HERE="$( cd -- "$HERE" > /dev/null 2>&1 && pwd )"
+
+# Run the setup scripts.
+bash $HERE/scripts/setup-uv-python.sh
+bash $HERE/scripts/setup-dev-env.sh
+
+# Source the env files to pick up common variables.
+if [ -f $HERE/scripts.env.sh ]; then
+  . $HERE/scripts/env.sh
+fi
+
+# Get variables defined in test-env.sh.
+if [ -f $HERE/scripts.test-env.sh ]; then
+  . $HERE/scriptstest-env.sh
+fi
 
 just "$@"
