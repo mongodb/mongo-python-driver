d28ea9cf87ba654f296bf58f27564665519a11d7
Steven Silvester <steve.silvester@mongodb.com>
2025-10-09 10:06:10 -0500

    set default

--- .evergreen/scripts/setup-uv-python.sh
+++ .evergreen/scripts/setup-uv-python.sh
@@ -5,6 +5,9 @@ set -eu
 HERE=$(dirname ${BASH_SOURCE:-$0})
 HERE="$( cd -- "$HERE" > /dev/null 2>&1 && pwd )"
 
+# Use min supported version by default.
+_python="3.10"
+
 # Source the env files to pick up common variables.
 if [ -f $HERE/env.sh ]; then
   . $HERE/env.sh
@@ -18,7 +21,8 @@ fi
 # Translate PYTHON_BINARY/PYTHON_VERSION to UV_PYTHON.
 if [ -z "${UV_PYTHON:-}" ]; then
   if [ -n "${PYTHON_BINARY:-}" ]; then
-    echo "export UV_PYTHON=$PYTHON_BINARY" >> $HERE/env.sh
+    _python=$PYTHON_BINARY
+
   elif [ -n "${PYTHON_VERSION:-}" ]; then
     version=$PYTHON_VERSION
     if [ "$(uname -s)" = "Darwin" ]; then
@@ -30,19 +34,19 @@ if [ -z "${UV_PYTHON:-}" ]; then
             framework_dir="Python"
         fi
         version=$(echo "$version" | sed 's/t//g')
-        UV_PYTHON="/Library/Frameworks/$framework_dir.Framework/Versions/$version/bin/$binary_name"
+        _python="/Library/Frameworks/$framework_dir.Framework/Versions/$version/bin/$binary_name"
     elif [ "Windows_NT" = "${OS:-}" ]; then
         version=$(echo $version | cut -d. -f1,2 | sed 's/\.//g; s/t//g')
         if [ -n "${IS_WIN32:-}" ]; then
-            UV_PYTHON="C:/python/32/Python$version/python.exe"
+            _python="C:/python/32/Python$version/python.exe"
         else
-            UV_PYTHON="C:/python/Python$version/python.exe"
+            _python="C:/python/Python$version/python.exe"
         fi
     elif [ -d "/opt/python/$version/bin" ]; then
-        UV_PYTHON="/opt/python/$version/bin/python3"
+        _python="/opt/python/$version/bin/python3"
     else
-        UV_PYTHON="${PYTHON_VERSION}"
+        _python="${PYTHON_VERSION}"
     fi
-    echo "export UV_PYTHON=$UV_PYTHON" >> $HERE/env.sh
   fi
+  echo "export UV_PYTHON=$_python" >> $HERE/env.sh
 fi
