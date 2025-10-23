381fd8f7967f4122aaecba6c8785c303957e3f7c
Shane Harvey <shnhrv@gmail.com>
2025-10-23 08:38:38 -0700

    PYTHON-5629 Increase max overload retries from 3 to 5 and initial delay from 50ms to 100ms

--- pymongo/asynchronous/helpers.py
+++ pymongo/asynchronous/helpers.py
@@ -77,8 +77,8 @@ def _handle_reauth(func: F) -> F:
     return cast(F, inner)
 
 
-_MAX_RETRIES = 3
-_BACKOFF_INITIAL = 0.05
+_MAX_RETRIES = 5
+_BACKOFF_INITIAL = 0.1
 _BACKOFF_MAX = 10
 # DRIVERS-3240 will determine these defaults.
 DEFAULT_RETRY_TOKEN_CAPACITY = 1000.0
