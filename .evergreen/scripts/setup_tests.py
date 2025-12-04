6fa0356ce5eb0d5a072099e4de7ef34ac5d26d03
Kevin Albertson <kevin.albertson@mongodb.com>
2025-12-04 09:19:13 -0500

    extract using `tar` command
    Avoid using `tarfile` module to workaround DEVPROD-24663.

--- .evergreen/scripts/setup_tests.py
+++ .evergreen/scripts/setup_tests.py
@@ -117,9 +117,10 @@ def setup_libmongocrypt():
     LOGGER.info(f"Fetching {url}...")
     with request.urlopen(request.Request(url), timeout=15.0) as response:  # noqa: S310
         if response.status == 200:
-            fileobj = io.BytesIO(response.read())
-        with tarfile.open("libmongocrypt.tar.gz", fileobj=fileobj) as fid:
-            fid.extractall(Path.cwd() / "libmongocrypt")
+            with Path("libmongocrypt.tar.gz").open("wb") as f:
+                f.write(response.read())
+            Path("libmongocrypt").mkdir()
+            run_command("tar -xzf libmongocrypt.tar.gz -C libmongocrypt")
     LOGGER.info(f"Fetching {url}... done.")
 
     run_command("ls -la libmongocrypt")
