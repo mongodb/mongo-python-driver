18c1f142b5b3fd179a677b2736332dc17770b7c2
Steven Silvester <steven.silvester@ieee.org>
2025-12-23 06:43:32 -0600

    PYTHON-5529 Introduce optin setting to await for MinPoolSize population (#2664)

--- test/asynchronous/utils.py
+++ test/asynchronous/utils.py
@@ -28,25 +28,25 @@ from inspect import iscoroutinefunction
 
 from bson.son import SON
 from pymongo import AsyncMongoClient
+from pymongo.asynchronous.pool import Pool, _CancellationContext, _PoolGeneration
 from pymongo.errors import ConfigurationError
 from pymongo.hello import HelloCompat
 from pymongo.lock import _async_create_lock
 from pymongo.operations import _Op
 from pymongo.read_preferences import ReadPreference
 from pymongo.server_selectors import any_server_selector, writable_server_selector
-from pymongo.synchronous.pool import _CancellationContext, _PoolGeneration
 
 _IS_SYNC = False
 
 
-async def async_get_pool(client):
+async def async_get_pool(client: AsyncMongoClient) -> Pool:
     """Get the standalone, primary, or mongos pool."""
     topology = await client._get_topology()
     server = await topology._select_server(writable_server_selector, _Op.TEST)
     return server.pool
 
 
-async def async_get_pools(client):
+async def async_get_pools(client: AsyncMongoClient) -> list[Pool]:
     """Get all pools."""
     return [
         server.pool
