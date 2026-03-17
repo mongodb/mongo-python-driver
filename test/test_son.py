554904deef547d78163acd9b5cbc9d6dab4ec2a3
Jeffrey 'Alex' Clark <aclark@aclark.net>
2026-03-17 15:08:50 -0400

    Address review feedback

--- test/test_son.py
+++ test/test_son.py
@@ -145,13 +145,11 @@ class TestSON(unittest.TestCase):
             self.assertEqual(ele * 100, test_son[ele])
 
     def test_contains_has(self):
-        """key in SON and __contains__"""
+        """Test key membership via 'in' and __contains__."""
         test_son = SON([(1, 100), (2, 200), (3, 300)])
         self.assertIn(1, test_son)
         self.assertIn(2, test_son, "in failed")
         self.assertNotIn(22, test_son, "in succeeded when it shouldn't")
-        self.assertTrue(2 in test_son, "'in' operator failed for key 2")
-        self.assertFalse(22 in test_son, "'in' operator succeeded when it shouldn't")
 
     def test_clears(self):
         """Test clear()"""
