23f74d8fe815956c8bc4081c7729a7d4bb37e1f8
Jeffrey 'Alex' Clark <aclark@aclark.net>
2026-05-21 13:11:10 -0400

    PYTHON-4741 Stop using rsSyncApplyStop in tests (#2809)

--- test/asynchronous/test_bulk.py
+++ test/asynchronous/test_bulk.py
@@ -947,17 +947,18 @@ class AsyncTestBulkWriteConcern(AsyncBulkTestBase):
         if not async_client_context.test_commands_enabled:
             self.skipTest("Test commands must be enabled.")
 
-        # Use the rsSyncApplyStop failpoint to pause replication on a
+        # Use the stopReplProducer failpoint to pause replication on a
         # secondary which will cause a wtimeout error.
-        await self.secondary.admin.command("configureFailPoint", "rsSyncApplyStop", mode="alwaysOn")
+        await self.secondary.admin.command(
+            "configureFailPoint", "stopReplProducer", mode="alwaysOn"
+        )
 
         try:
             coll = self.coll.with_options(write_concern=WriteConcern(w=self.w, wtimeout=1))
             return await coll.bulk_write(requests, ordered=ordered)
         finally:
-            await self.secondary.admin.command("configureFailPoint", "rsSyncApplyStop", mode="off")
+            await self.secondary.admin.command("configureFailPoint", "stopReplProducer", mode="off")
 
-    @async_client_context.require_version_max(7, 1)  # PYTHON-4560
     @async_client_context.require_replica_set
     @async_client_context.require_secondaries_count(1)
     async def test_write_concern_failure_ordered(self):
@@ -1039,7 +1040,6 @@ class AsyncTestBulkWriteConcern(AsyncBulkTestBase):
         failed = details["writeErrors"][0]
         self.assertIn("duplicate", failed["errmsg"])
 
-    @async_client_context.require_version_max(7, 1)  # PYTHON-4560
     @async_client_context.require_replica_set
     @async_client_context.require_secondaries_count(1)
     async def test_write_concern_failure_unordered(self):
