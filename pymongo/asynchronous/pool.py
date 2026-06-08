7d4ef9ca69f8fdeb5c417fa0ac29193680b903a0
Sophia Yang <sophia.yang@mongodb.com>
2026-06-08 11:52:26 -0400

    typo2

--- pymongo/asynchronous/pool.py
+++ pymongo/asynchronous/pool.py
@@ -401,7 +401,7 @@ class AsyncConnection:
         if self.op_msg_enabled:
             self._raise_if_not_writable(unacknowledged)
         try:
-            result = result = cast(
+            result = cast(
                 dict[str, Any],
                 await command(
                     self,
--- pymongo/synchronous/pool.py
+++ pymongo/synchronous/pool.py
@@ -401,7 +401,7 @@ class Connection:
         if self.op_msg_enabled:
             self._raise_if_not_writable(unacknowledged)
         try:
-            result = result = cast(
+            result = cast(
                 dict[str, Any],
                 command(
                     self,
