a44fe53d345e3426e4ae79ca3acdaa061ae672ce
Steven Silvester <steve.silvester@mongodb.com>
2026-07-08 21:26:19 -0500

    PYTHON-5040 Revert drivers-evergreen-tools checkout to mongodb-labs/master
    The dedicated failpoint TLS env var change merged upstream as
    mongodb-labs/drivers-evergreen-tools PR 802, so the temporary fork/branch
    override is no longer needed.

--- .evergreen/scripts/configure-env.sh
+++ .evergreen/scripts/configure-env.sh
@@ -74,10 +74,8 @@ EOT
 
 # Write the .env file for drivers-tools.
 rm -rf $DRIVERS_TOOLS
-# TEMPORARY: testing PYTHON-5040 against unmerged drivers-evergreen-tools branch.
-# Revert to BRANCH=master ORG=mongodb-labs once that PR is merged.
-BRANCH=PYTHON-5040
-ORG=blink1073
+BRANCH=master
+ORG=mongodb-labs
 git clone --branch $BRANCH https://github.com/$ORG/drivers-evergreen-tools.git $DRIVERS_TOOLS
 
 cat <<EOT > ${DRIVERS_TOOLS}/.env
