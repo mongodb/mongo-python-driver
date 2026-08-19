a817f992c4a23074762802bbc850efb8974bf26b
Steven Silvester <steve.silvester@mongodb.com>
2026-08-19 14:41:25 -0500

    PYTHON-5808 Fix _Query.get_message() call after merge
    Upstream removed the use_cmd parameter from _Query.get_message().
    Update the call site in cursor_base.py to match the new signature.

--- pymongo/synchronous/cursor_base.py
+++ pymongo/synchronous/cursor_base.py
@@ -116,7 +116,7 @@ class _CursorBase(_AgnosticCursorBase[_DocumentType]):
         if more_to_come:
             request_id, data, max_doc_size = 0, b"", 0
         else:
-            message = operation.get_message(read_preference, conn, use_cmd)
+            message = operation.get_message(read_preference, conn)
             request_id, data, max_doc_size = _split_message(message)
         user_fields = _CURSOR_DOC_FIELDS if use_cmd else None
         docs, reply, duration = run_cursor_command(
