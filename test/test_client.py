54997792ef0f6a3cf8486b9ba88984eee946d288
Noah Stapp <noah.stapp@mongodb.com>
2026-08-31 15:57:41 -0400

    PYTHON-6015 Consolidate topology module-level helpers into shared class (#3021)

--- test/test_client.py
+++ test/test_client.py
@@ -95,8 +95,8 @@ from pymongo.synchronous.mongo_client import MongoClient, _ClientCheckout
 from pymongo.synchronous.pool import (
     Connection,
 )
-from pymongo.synchronous.topology import _ErrorContext
 from pymongo.topology_description import TOPOLOGY_TYPE, TopologyDescription
+from pymongo.topology_shared import _ErrorContext
 from pymongo.write_concern import WriteConcern
 from test import (
     HAVE_IPADDRESS,
